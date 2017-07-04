9d8cd353fa0b090bbe4f9c8a7d21335073059fda
Dominic Wong <dominic.wong.617@gmail.com>
2017-07-04 22:23:46 +0800

    Get Gatecoin's tickers

--- lib/cryptoexchange.rb
+++ lib/cryptoexchange.rb
@@ -3,11 +3,19 @@ require "cryptoexchange/models/ticker"
 require "cryptoexchange/models/market_pair"
 require "cryptoexchange/services/market"
 require "cryptoexchange/services/pairs"
+
 require "cryptoexchange/exchanges/cryptopia/market"
 require "cryptoexchange/exchanges/cryptopia/models/ticker"
 require "cryptoexchange/exchanges/cryptopia/models/market_pair"
 require "cryptoexchange/exchanges/cryptopia/services/market"
 require "cryptoexchange/exchanges/cryptopia/services/pairs"
+
+require "cryptoexchange/exchanges/gatecoin/market"
+require "cryptoexchange/exchanges/gatecoin/models/ticker"
+require "cryptoexchange/exchanges/gatecoin/models/market_pair"
+require "cryptoexchange/exchanges/gatecoin/services/market"
+require "cryptoexchange/exchanges/gatecoin/services/pairs"
+
 require "http"
 
 module Cryptoexchange
---
+++ lib/cryptoexchange/exchanges/gatecoin/services/market.rb
@@ -0,0 +1,50 @@
+module Cryptoexchange::Exchanges
+  module Gatecoin
+    module Services
+      class Market < Cryptoexchange::Services::Market
+
+        def fetch(market_pair)
+          output = super(market_url)
+          adapt(output, market_pair)
+        end
+
+        def market_url
+          "#{Cryptoexchange::Exchanges::Gatecoin::Market::API_URL}/Public/LiveTickers"
+        end
+
+        def adapt(output, market_pair)
+          # base
+          # target
+          # market (exchange_str)
+          # last
+          # bid
+          # ask
+          # high
+          # low
+          # avg
+          # volume
+          # timestamp
+
+          data = output['tickers']
+          ticker_match = data.find do |datum|
+            datum['currencyPair'] == market_pair.base + market_pair.target
+          end
+
+          ticker = Gatecoin::Models::Ticker.new
+          ticker.base = market_pair.base
+          ticker.target = market_pair.target
+          ticker.market = 'gatecoin'
+          ticker.last = ticker_match['last']
+          ticker.bid = ticker_match['bid']
+          ticker.ask = ticker_match['ask']
+          ticker.high = ticker_match['high']
+          ticker.low = ticker_match['low']
+          ticker.volume = ticker_match['volume'] # TODO: Check if it is base denominated?
+          ticker.avg = ticker_match['vwap']
+          ticker.timestamp = ticker_match['createDateTime'].to_i
+          ticker
+        end
+      end
+    end
+  end
+end
