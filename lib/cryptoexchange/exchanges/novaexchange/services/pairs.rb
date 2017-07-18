e832d424187feac642d8d36062654bfbb2d73648
Yihang Ho <hoyihang5@gmail.com>
2017-07-18 16:08:12 +0800

    Fix base/target pair for Nova
    The base/target pairs coming the their API endpoints are buggy. For
    e.g., consider https://novaexchange.com/remote/v2/market/info/BTC_LTC/.
    
    As of now, the base currency is BTC, target currency is LTC, but the
    price is ~ 0.0189. This value is closer to the rate for LTC/BTC, not
    BTC/LTC. Hence, the correct base should be LTC, and target should be
    BTC. As a result, the market name should be target_base.

--- lib/cryptoexchange/exchanges/novaexchange/services/pairs.rb
+++ lib/cryptoexchange/exchanges/novaexchange/services/pairs.rb
@@ -13,8 +13,12 @@ module Cryptoexchange::Exchanges
           market_pairs = []
           output['markets'].each do |ticker|
             market_pair = Novaexchange::Models::MarketPair.new
-            market_pair.base = ticker['basecurrency']
-            market_pair.target = ticker['currency']
+
+            # Note: The base/target are swapped intentionally. It appears that
+            # the API endpoint is returning in the wrong order.
+            market_pair.base = ticker['currency']
+            market_pair.target = ticker['basecurrency']
+
             market_pair.market = Novaexchange::Market::NAME
             market_pairs << market_pair
           end
