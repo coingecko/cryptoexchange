c9d54756531791c79d51f4f057f902ad87a9e89a
Sam Lin <greenlin111@gmail.com>
2017-07-19 10:23:29 +0800

    implement Bithumb

---
+++ spec/exchanges/bithumb/integration/market_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+RSpec.describe 'Bithumb integration specs' do
+  let(:client) { Cryptoexchange::Client.new }
+
+  it 'fetch pairs' do
+    pairs = client.pairs('bithumb')
+    expect(pairs).not_to be_empty
+
+    pair = pairs.first
+    expect(pair.base).to_not be nil
+    expect(pair.target).to_not be nil
+    expect(pair.market).to eq 'bithumb'
+  end
+
+  it 'fetch ticker' do
+    btc_krw_pair = Cryptoexchange::Models::MarketPair.new(base: 'btc', target: 'krw', market: 'bithumb')
+    ticker = client.ticker(btc_krw_pair)
+
+    expect(ticker.base).to eq 'BTC'
+    expect(ticker.target).to eq 'KRW'
+    expect(ticker.market).to eq 'bithumb'
+    expect(ticker.last).to_not be nil
+    expect(ticker.high).to_not be nil
+    expect(ticker.low).to_not be nil
+    expect(ticker.volume).to_not be nil
+    expect(ticker.timestamp).to be_a Numeric
+    expect(ticker.payload).to_not be nil
+  end
+end
