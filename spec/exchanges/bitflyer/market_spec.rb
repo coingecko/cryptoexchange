f7bd26b9385e3abb0df9bb982654290800f35f9d
Dominic Wong <dominic.wong.617@gmail.com>
2017-08-10 15:03:01 +0800

    Fix failing bitflyer spec

--- spec/exchanges/bitflyer/market_spec.rb
+++ spec/exchanges/bitflyer/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Bitflyer::Market do
   it { expect(described_class::NAME).to eq 'bitflyer' }
-  it { expect(described_class::API_URL).to eq 'https://api.bitflyer.jp/v1/' }
+  it { expect(described_class::API_URL).to eq 'https://api.bitflyer.jp/v1' }
 end
