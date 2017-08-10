3e25eb9aa840b9cb12ed521255711dc622f5b295
Dominic Wong <dominic.wong.617@gmail.com>
2017-08-10 15:02:41 +0800

    Fix failing coincheck spec

--- spec/exchanges/coincheck/market_spec.rb
+++ spec/exchanges/coincheck/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Coincheck::Market do
   it { expect(described_class::NAME).to eq 'coincheck' }
-  it { expect(described_class::API_URL).to eq 'https://coincheck.com/api/' }
+  it { expect(described_class::API_URL).to eq 'https://coincheck.com/api' }
 end
