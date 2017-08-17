0c6692f4240db2f91a92c82c026a70f160bafb6b
TM Lee <tm89lee@gmail.com>
2017-08-17 20:05:11 +0800

    [#hotfix] Bleutrade, EtherDelta base/target swap fix

--- spec/exchanges/bleutrade/integration/market_spec.rb
+++ spec/exchanges/bleutrade/integration/market_spec.rb
@@ -13,12 +13,22 @@ RSpec.describe 'Bleutrade integration specs' do
     expect(pair.market).to eq 'bleutrade'
   end
 
+  it 'fetch pairs and assign the correct base/target' do
+    pairs = client.pairs('bleutrade')
+    expect(pairs).not_to be_empty
+
+    pair = pairs.first
+    expect(pair.base).to eq 'ADC'
+    expect(pair.target).to eq 'BTC'
+    expect(pair.market).to eq 'bleutrade'
+  end
+
   it 'fetch ticker' do
-    btc_adc_pair = Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'adc', market: 'bleutrade')
+    btc_adc_pair = Cryptoexchange::Models::MarketPair.new(base: 'ADC', target: 'BTC', market: 'bleutrade')
     ticker = client.ticker(btc_adc_pair)
 
-    expect(ticker.base).to eq 'BTC'
-    expect(ticker.target).to eq 'ADC'
+    expect(ticker.base).to eq 'ADC'
+    expect(ticker.target).to eq 'BTC'
     expect(ticker.market).to eq 'bleutrade'
     expect(ticker.last).to be_a Numeric
     expect(ticker.bid).to be_a Numeric
