9c302ec255ec59f15abf2f52ca8ea06832db2743
Yihang Ho <hoyihang5@gmail.com>
2017-08-23 08:07:04 +0000

    Implement Huobi

---
+++ lib/cryptoexchange/exchanges/huobi/market.rb
@@ -0,0 +1,9 @@
+module Cryptoexchange::Exchanges
+  module Huobi
+    class Market
+      NAME = 'huobi'
+      DOT_COM_API_URL = 'https://be.huobi.com'
+      DOT_PRO_API_URL = 'https://api.huobi.pro'
+    end
+  end
+end
