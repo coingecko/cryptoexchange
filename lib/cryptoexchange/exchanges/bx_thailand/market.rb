ac6f839eaf3f8de0586dbd3d165294ee2a2fe1c0
Dominic Wong <dominic.wong.617@gmail.com>
2017-08-26 14:35:00 +0800

    BX Thailand Ticker

---
+++ lib/cryptoexchange/exchanges/bx_thailand/market.rb
@@ -0,0 +1,8 @@
+module Cryptoexchange::Exchanges
+  module BxThailand
+    class Market
+      NAME = 'bx_thailand'
+      API_URL = 'https://bx.in.th/api/'
+    end
+  end
+end
---
+++ lib/cryptoexchange/exchanges/bx_thailand/services/market.rb
@@ -0,0 +1,49 @@
+module Cryptoexchange::Exchanges
+  module BxThailand
+    module Services
+      class Market < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            false
+          end
+        end
+
+        def fetch
+          output = super(ticker_url)
+          adapt_all(output)
+        end
+
+        def ticker_url
+          Cryptoexchange::Exchanges::BxThailand::Market::API_URL
+        end
+
+        def adapt_all(output)
+          output.values.map do |ticker|
+            base = ticker['secondary_currency']
+            target = ticker['primary_currency']
+            market_pair = Cryptoexchange::Models::MarketPair.new(
+                            base: base,
+                            target: target,
+                            market: BxThailand::Market::NAME
+                          )
+            adapt(ticker, market_pair)
+          end
+        end
+
+        def adapt(output, market_pair)
+          ticker           = Cryptoexchange::Models::Ticker.new
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
+          ticker.market    = BxThailand::Market::NAME
+          ticker.ask       = NumericHelper.to_d(HashHelper.dig(output, 'orderbook', 'asks', 'highbid'))
+          ticker.bid       = NumericHelper.to_d(HashHelper.dig(output, 'orderbook', 'bids', 'highbid'))
+          ticker.last      = NumericHelper.to_d(output['last_price'])
+          ticker.volume    = NumericHelper.to_d(output['volume_24hours'])
+          ticker.timestamp = Time.now.to_i
+          ticker.payload   = output
+          ticker
+        end
+      end
+    end
+  end
+end
