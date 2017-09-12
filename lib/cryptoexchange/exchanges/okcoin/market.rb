86751f6c356ccf07b4275ff133924f80721f7435
TM Lee <tm89lee@gmail.com>
2017-09-12 13:45:28 +0800

    Okcoin change API to omit V1, this can support CNY pair

--- lib/cryptoexchange/exchanges/okcoin/market.rb
+++ lib/cryptoexchange/exchanges/okcoin/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Okcoin
     class Market
       NAME = 'okcoin'
-      API_URL = 'https://www.okcoin.com/api/v1'
+      API_URL = 'https://www.okcoin.com/api'
     end
   end
 end
--- spec/exchanges/okcoin/market_spec.rb
+++ spec/exchanges/okcoin/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Okcoin::Market do
   it { expect(described_class::NAME).to eq 'okcoin' }
-  it { expect(described_class::API_URL).to eq 'https://www.okcoin.com/api/v1' }
+  it { expect(described_class::API_URL).to eq 'https://www.okcoin.com/api' }
 end
