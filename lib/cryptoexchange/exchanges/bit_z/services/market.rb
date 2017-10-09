12498418cdbab26fb3c29e876f28044cdd54d2bb
Sam Lin <greenlin111@gmail.com>
2017-10-09 15:11:25 +0800

    change ticker form specific endpoint to all pairs

--- lib/cryptoexchange/exchanges/bit_z/services/market.rb
+++ lib/cryptoexchange/exchanges/bit_z/services/market.rb
@@ -4,35 +4,45 @@ module Cryptoexchange::Exchanges
       class Market < Cryptoexchange::Services::Market
         class << self
           def supports_individual_ticker_query?
-            true
+            false
           end
         end
 
-        def fetch(market_pair)
-          output = super(ticker_url(market_pair))
-          adapt(output, market_pair)
+        def fetch
+          output = super(ticker_url)
+          adapt_all(output)
         end
 
-        def ticker_url(market_pair)
-          base = market_pair.base
-          target = market_pair.target
-          "#{Cryptoexchange::Exchanges::BitZ::Market::API_URL}/ticker?coin=#{base}_#{target}"
+        def ticker_url
+          "#{Cryptoexchange::Exchanges::BitZ::Market::API_URL}/tickerall"
+        end
+
+        def adapt_all(output)
+          output["data"].map do |key, value|
+            base = key.split("_").first
+            target = key.split("_").last
+            market_pair = Cryptoexchange::Models::MarketPair.new(
+                            base: base,
+                            target: target,
+                            market: BitZ::Market::NAME
+                          )
+            adapt(value,market_pair)
+          end
         end
 
         def adapt(output, market_pair)
-          data = output["data"]
           ticker = Cryptoexchange::Models::Ticker.new
           ticker.base = market_pair.base
           ticker.target = market_pair.target
           ticker.market = BitZ::Market::NAME
-          ticker.ask = NumericHelper.to_d(data['sell'])
-          ticker.bid = NumericHelper.to_d(data['buy'])
-          ticker.last = NumericHelper.to_d(data['last'])
-          ticker.high = NumericHelper.to_d(data['high'])
-          ticker.low = NumericHelper.to_d(data['low'])
-          ticker.volume = NumericHelper.to_d(data['vol'])
+          ticker.ask = NumericHelper.to_d(output['sell'])
+          ticker.bid = NumericHelper.to_d(output['buy'])
+          ticker.last = NumericHelper.to_d(output['last'])
+          ticker.high = NumericHelper.to_d(output['high'])
+          ticker.low = NumericHelper.to_d(output['low'])
+          ticker.volume = NumericHelper.to_d(output['vol'])
           ticker.timestamp = DateTime.now.to_time.to_i
-          ticker.payload = data
+          ticker.payload = output
           ticker
         end
       end
