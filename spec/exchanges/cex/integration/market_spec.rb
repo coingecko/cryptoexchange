0b50ff472fad9cc5f4bfef02ba0c2c185cd8f800
Raymond Usbal <raymond@philippinedev.com>
2017-11-22 00:46:04 +0800

    Implemented CEX.io OrderBook

--- spec/exchanges/cex/integration/market_spec.rb
+++ spec/exchanges/cex/integration/market_spec.rb
@@ -32,23 +32,24 @@ RSpec.describe 'Cex integration specs' do
     expect(ticker.payload).to_not be nil
   end
 
-  # it 'fetch order book' do
-  #   eth_btc_pair = Cryptoexchange::Models::MarketPair.new(base: 'ETH', target: 'BTC', market: 'cex')
-  #   order_book = client.order_book(eth_btc_pair)
-  #
-  #   expect(order_book.base).to eq 'ETH'
-  #   expect(order_book.target).to eq 'BTC'
-  #   expect(order_book.market).to eq 'cex'
-  #   expect(order_book.asks).to_not be_empty
-  #   expect(order_book.bids).to_not be_empty
-  #   expect(order_book.asks.first.price).to_not be_nil
-  #   expect(order_book.bids.first.amount).to_not be_nil
-  #   expect(order_book.bids.first.timestamp).to be_nil
-  #   expect(order_book.asks.count).to be > 10
-  #   expect(order_book.bids.count).to be > 10
-  #   expect(order_book.timestamp).to be_a Numeric
-  #   expect(order_book.payload).to_not be nil
-  # end
+  it 'fetch order book' do
+    eth_btc_pair = Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'USD', market: 'cex')
+    order_book = client.order_book(eth_btc_pair)
+
+    expect(order_book.payload['error']).to_not eq 'Invalid Symbols Pair'
+    expect(order_book.base).to eq 'BTC'
+    expect(order_book.target).to eq 'USD'
+    expect(order_book.market).to eq 'cex'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to be_nil
+    expect(order_book.asks.count).to be > 10
+    expect(order_book.bids.count).to be > 10
+    expect(order_book.timestamp).to be_a Numeric
+    expect(order_book.payload).to_not be nil
+  end
 
   # it 'fetch trade' do
   #   eth_btc_pair = Cryptoexchange::Models::MarketPair.new(base: 'ETH', target: 'BTC', market: 'cex')
