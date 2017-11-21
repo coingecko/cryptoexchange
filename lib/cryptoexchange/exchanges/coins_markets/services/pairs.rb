f5203075bf9d0698c76eca594294f8b1d8e9790d
Raymond Usbal <raymond@philippinedev.com>
2017-11-21 16:06:10 +0800

    Fix Base-Target position and volume

--- lib/cryptoexchange/exchanges/coins_markets/services/pairs.rb
+++ lib/cryptoexchange/exchanges/coins_markets/services/pairs.rb
@@ -11,7 +11,7 @@ module Cryptoexchange::Exchanges
 
         def adapt(output)
           output.map do |key, _|
-            base, target = key.split('_')
+            target, base = key.split('_')
             Cryptoexchange::Models::MarketPair.new(
               base: base,
               target: target,
