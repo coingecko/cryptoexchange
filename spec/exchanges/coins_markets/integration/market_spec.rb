53f9d12578d6e796b5c3de9c9394b0e12d643217
Raymond Usbal <raymond@philippinedev.com>
2017-11-23 09:47:47 +0800

    CoinsMarkets base/target correction

--- spec/exchanges/coins_markets/integration/market_spec.rb
+++ spec/exchanges/coins_markets/integration/market_spec.rb
@@ -14,11 +14,11 @@ RSpec.describe 'CoinsMarkets integration specs' do
   end
 
   it 'fetch ticker' do
-    pair = Cryptoexchange::Models::MarketPair.new(base: 'DOG', target: 'XGTC', market: 'coins_markets')
+    pair = Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'DOG', market: 'coins_markets')
     ticker = client.ticker(pair)
 
-    expect(ticker.base).to eq 'DOG'
-    expect(ticker.target).to eq 'XGTC'
+    expect(ticker.base).to eq 'BTC'
+    expect(ticker.target).to eq 'DOG'
     expect(ticker.market).to eq 'coins_markets'
     expect(ticker.last).to be_a Numeric
     expect(ticker.ask).to be_a Numeric
@@ -26,8 +26,7 @@ RSpec.describe 'CoinsMarkets integration specs' do
     expect(ticker.high).to be_a Numeric
     expect(ticker.low).to be_a Numeric
     expect(ticker.volume).to be_a Numeric
-  #   expect(ticker.change).to be_a Numeric
-  #   expect(ticker.timestamp).to be_a Numeric
+    expect(ticker.timestamp).to be_a Numeric
     expect(2000..Date.today.year).to include(Time.at(ticker.timestamp).year)
     expect(ticker.payload).to_not be nil
   end
