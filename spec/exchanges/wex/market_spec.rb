900d4ce731588c6d702033c88c386c9d6720beec
Ranhiru Cooray <ranhiru@gmail.com>
2017-11-30 20:31:54 +0800

    WIP - Fixed market URL

--- spec/exchanges/wex/market_spec.rb
+++ spec/exchanges/wex/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Wex::Market do
   it { expect(described_class::NAME).to eq 'wex' }
-  it { expect(described_class::API_URL).to eq 'https://wex.nz/api/3/' }
+  it { expect(described_class::API_URL).to eq 'https://wex.nz/api/3' }
 end
