c61591c7abf177000dbd1d837d810c61048f988e
Ranhiru Cooray <ranhiru@gmail.com>
2017-12-02 14:53:08 +0800

    WIP - Added order book

--- lib/cryptoexchange/exchanges/okex/services/order_book.rb
+++ lib/cryptoexchange/exchanges/okex/services/order_book.rb
@@ -1,5 +1,5 @@
 module Cryptoexchange::Exchanges
-  module Binance
+  module Okex
     module Services
       class OrderBook < Cryptoexchange::Services::Market
         class << self
@@ -9,14 +9,12 @@ module Cryptoexchange::Exchanges
         end
 
         def fetch(market_pair)
-          output = super(order_book_url(market_pair))
+          output = super(ticker_url(market_pair))
           adapt(output, market_pair)
         end
 
-        def order_book_url(market_pair)
-          base = market_pair.base
-          target = market_pair.target
-          "#{Cryptoexchange::Exchanges::Binance::Market::API_URL}/depth?symbol=#{base}#{target}"
+        def ticker_url(market_pair)
+          "#{Cryptoexchange::Exchanges::Okex::Market::API_URL}/depth.do?symbol=#{market_pair.base.downcase}_#{market_pair.target.downcase}"
         end
 
         def adapt(output, market_pair)
@@ -25,18 +23,20 @@ module Cryptoexchange::Exchanges
 
           order_book.base      = market_pair.base
           order_book.target    = market_pair.target
-          order_book.market    = Binance::Market::NAME
+          order_book.market    = Okex::Market::NAME
           order_book.asks      = adapt_orders(output['asks'], timestamp)
           order_book.bids      = adapt_orders(output['bids'], timestamp)
-          order_book.timestamp = timestamp
+          order_book.timestamp = Time.now.to_i
           order_book.payload   = output
           order_book
         end
 
         def adapt_orders(orders, timestamp)
           orders.collect do |order_entry|
-            Cryptoexchange::Models::Order.new(price: NumericHelper.to_d(order_entry[0]),
-                                              amount: NumericHelper.to_d(order_entry[1]),
+            price = order_entry[0]
+            amount = order_entry[1]
+            Cryptoexchange::Models::Order.new(price: price,
+                                              amount: amount,
                                               timestamp: timestamp)
           end
         end
--- spec/exchanges/okex/integration/market_spec.rb
+++ spec/exchanges/okex/integration/market_spec.rb
@@ -30,4 +30,20 @@ RSpec.describe 'Okex integration specs' do
     expect(2000..Date.today.year).to include(Time.at(ticker.timestamp).year)
     expect(ticker.payload).to_not be nil
   end
+
+  it 'fetch order book' do
+    btc_usd_pair = Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'USDT', market: 'okex')
+    order_book = client.order_book(btc_usd_pair)
+
+    expect(order_book.base).to eq 'BTC'
+    expect(order_book.target).to eq 'USDT'
+    expect(order_book.market).to eq 'okex'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to_not be_nil
+    expect(order_book.timestamp).to be_a Numeric
+    expect(order_book.payload).to_not be nil
+  end
 end
