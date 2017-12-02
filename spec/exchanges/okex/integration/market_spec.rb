09e1ba9584993e5a92d651655f9f3f6e3b7d420b
Ranhiru Cooray <ranhiru@gmail.com>
2017-12-02 14:29:18 +0800

    WIP - Added ticker and specs

--- spec/exchanges/okex/integration/market_spec.rb
+++ spec/exchanges/okex/integration/market_spec.rb
@@ -12,4 +12,22 @@ RSpec.describe 'Okex integration specs' do
     expect(pair.target).not_to be_nil
     expect(pair.market).to eq 'okex'
   end
+
+  it 'fetch ticker' do
+    pair = Cryptoexchange::Models::MarketPair.new(base: 'ETH', target: 'USDT', market: 'okex')
+    ticker = client.ticker(pair)
+
+    expect(ticker.base).to eq 'ETH'
+    expect(ticker.target).to eq 'USDT'
+    expect(ticker.market).to eq 'okex'
+    expect(ticker.last).to be_a Numeric
+    expect(ticker.bid).to be_a Numeric
+    expect(ticker.ask).to be_a Numeric
+    expect(ticker.high).to be_a Numeric
+    expect(ticker.low).to be_a Numeric
+    expect(ticker.volume).to be_a Numeric
+    expect(ticker.timestamp).to be_a Numeric
+    expect(2000..Date.today.year).to include(Time.at(ticker.timestamp).year)
+    expect(ticker.payload).to_not be nil
+  end
 end
