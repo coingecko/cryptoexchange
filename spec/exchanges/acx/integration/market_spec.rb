1775a65b45a0323487bb5f03a148a0d3dfce6303
Ranhiru Cooray <ranhiru@gmail.com>
2017-12-02 18:04:34 +0800

    WIP - Added market and order book

--- spec/exchanges/acx/integration/market_spec.rb
+++ spec/exchanges/acx/integration/market_spec.rb
@@ -12,4 +12,38 @@ RSpec.describe 'ACX integration specs' do
     expect(pair.target).to_not be nil
     expect(pair.market).to eq 'acx'
   end
+
+  it 'fetch ticker' do
+    eth_aud_pair = Cryptoexchange::Models::MarketPair.new(base: 'eth', target: 'aud', market: 'acx')
+    ticker = client.ticker(eth_aud_pair)
+
+    expect(ticker.base).to eq 'ETH'
+    expect(ticker.target).to eq 'AUD'
+    expect(ticker.market).to eq 'acx'
+    expect(ticker.last).to be_a Numeric
+    expect(ticker.high).to be_a Numeric
+    expect(ticker.low).to be_a Numeric
+    expect(ticker.ask).to be_a Numeric
+    expect(ticker.bid).to be_a Numeric
+    expect(ticker.volume).to be_a Numeric
+    expect(ticker.timestamp).to be_a Numeric
+    expect(2000..Date.today.year).to include(Time.at(ticker.timestamp).year)
+    expect(ticker.payload).to_not be nil
+  end
+
+  it 'fetch order book' do
+    eth_aud_pair = Cryptoexchange::Models::MarketPair.new(base: 'ETH', target: 'AUD', market: 'acx')
+    order_book = client.order_book(eth_aud_pair)
+
+    expect(order_book.base).to eq 'ETH'
+    expect(order_book.target).to eq 'AUD'
+    expect(order_book.market).to eq 'acx'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to_not be_nil
+    expect(order_book.timestamp).to be_a Numeric
+    expect(order_book.payload).to_not be nil
+  end
 end
