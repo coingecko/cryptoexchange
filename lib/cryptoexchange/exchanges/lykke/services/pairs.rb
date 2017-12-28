bae8b6964085afaef133eb4d431b66ae3cf768e3
Esther Leong Yuet Mei <estherleongym@gmail.com>
2017-12-29 02:29:33 +0800

    Implemented suggested refactored codes for pair.rb and removed troubleshooting history

--- lib/cryptoexchange/exchanges/lykke/services/pairs.rb
+++ lib/cryptoexchange/exchanges/lykke/services/pairs.rb
@@ -2,28 +2,24 @@ module Cryptoexchange::Exchanges
   module Lykke
     module Services
       class Pairs < Cryptoexchange::Services::Pairs
-        TARGETS_URL = "#{Cryptoexchange::Exchanges::Lykke::Market::API_URL}/AssetPairs/dictionary"
+        PAIRS_URL = "#{Cryptoexchange::Exchanges::Lykke::Market::API_URL}/AssetPairs/dictionary"
 
         def fetch
-          pairs_output = HTTP.timeout(:write => 2, :connect => 5, :read => 8).get(TARGETS_URL)
-          pairs_dictionary = JSON.parse(pairs_output.to_s)
-          adapt(pairs_dictionary)
+          output = super
+          adapt(output)
         end
 
-        def adapt(pairs_dictionary)
-          market_pairs = []
-          pairs_dictionary.each do |pair|
+        def adapt(output)
+          output.map do |pair|
             base = pair["baseAssetId"]
             target = pair["quotingAssetId"]
-            next unless base && target
-            market_pairs << Cryptoexchange::Models::MarketPair.new(
+            Cryptoexchange::Models::MarketPair.new(
                               base: base,
                               target: target,
                               market: Lykke::Market::NAME
                             )
 
           end
-          market_pairs
         end
       end
     end
