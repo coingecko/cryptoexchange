22187c847a9f7a0966c54d0052bd9d8fb7128e33
Esther Leong Yuet Mei <estherleongym@gmail.com>
2018-01-29 23:20:12 +0800

    Added OrderBook and Tradebook

--- spec/exchanges/coinrail/integration/market_spec.rb
+++ spec/exchanges/coinrail/integration/market_spec.rb
@@ -21,47 +21,40 @@ RSpec.describe 'Coinrail integration specs' do
     expect(ticker.target).to eq 'KRW'
     expect(ticker.market).to eq 'coinrail'
     expect(ticker.last).to be_a Numeric
-    expect(ticker.ask).to be_a Numeric
-    expect(ticker.bid).to be_a Numeric
-    expect(ticker.low).to be_a Numeric
-    expect(ticker.high).to be_a Numeric
     expect(ticker.volume).to be_a Numeric
     expect(ticker.timestamp).to be_a Numeric
     expect(2000..Date.today.year).to include(Time.at(ticker.timestamp).year)
     expect(ticker.payload).to_not be nil
   end
-  #
-  # it 'fetch order book' do
-  #   order_book = client.order_book(btc_krw_pair)
-  #
-  #   expect(order_book.base).to eq 'BTC'
-  #   expect(order_book.target).to eq 'KRW'
-  #   expect(order_book.market).to eq 'coinnest'
-  #   expect(order_book.asks).to_not be_empty
-  #   expect(order_book.bids).to_not be_empty
-  #   expect(order_book.asks.first.price).to_not be_nil
-  #   expect(order_book.bids.first.amount).to_not be_nil
-  #   expect(order_book.bids.first.timestamp).to be_nil
-  #   expect(order_book.asks.count).to be > 10
-  #   expect(order_book.bids.count).to be > 10
-  #   expect(order_book.timestamp).to be_a Numeric
-  #   expect(order_book.payload).to_not be nil
-  # end
-  #
-  # it 'fetch trade' do
-  #   trades = client.trades(btc_krw_pair)
-  #   trade = trades.sample
-  #
-  #   expect(trades).to_not be_empty
-  #   expect(trade.base).to eq 'BTC'
-  #   expect(trade.target).to eq 'KRW'
-  #   expect(trade.market).to eq 'coinnest'
-  #   expect(trade.trade_id).to_not be_nil
-  #   expect(['buy', 'sell']).to include trade.type
-  #   expect(trade.price).to_not be_nil
-  #   expect(trade.amount).to_not be_nil
-  #   expect(trade.timestamp).to be_a Numeric
-  #   expect(trade.payload).to_not be nil
-  # end
 
+  it 'fetch order book' do
+    order_book = client.order_book(btc_krw_pair)
+
+    expect(order_book.base).to eq 'BTC'
+    expect(order_book.target).to eq 'KRW'
+    expect(order_book.market).to eq 'coinrail'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to be_nil
+    expect(order_book.asks.count).to be > 10
+    expect(order_book.bids.count).to be > 10
+    expect(order_book.timestamp).to be_a Numeric
+    expect(order_book.payload).to_not be nil
+  end
+
+  it 'fetch trade' do
+    trades = client.trades(btc_krw_pair)
+    trade = trades.sample
+
+    expect(trades).to_not be_empty
+    expect(trade.base).to eq 'BTC'
+    expect(trade.target).to eq 'KRW'
+    expect(trade.market).to eq 'coinrail'
+    expect(trade.price).to_not be_nil
+    expect(trade.amount).to_not be_nil
+    expect(trade.timestamp).to be_a Numeric
+    expect(trade.payload).to_not be nil
+  end
 end
