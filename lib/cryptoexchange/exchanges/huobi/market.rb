376ae5bf88a5bf2caa033d32e781f8f1ec132cbc
TM Lee <tm89lee@gmail.com>
2018-03-05 10:53:49 +0800

    Drop huobi.com, only huobi.pro

--- lib/cryptoexchange/exchanges/huobi/market.rb
+++ lib/cryptoexchange/exchanges/huobi/market.rb
@@ -2,7 +2,6 @@ module Cryptoexchange::Exchanges
   module Huobi
     class Market
       NAME = 'huobi'
-      DOT_COM_API_URL = 'https://be.huobi.com'
       DOT_PRO_API_URL = 'https://api.huobi.pro'
     end
   end
