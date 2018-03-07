445b479be2896f710a2224b44eea3407cf70aae3
Jack Wong <jack@myfave.com>
2018-03-07 10:38:24 +0800

    append market_spec - base and integration

---
+++ spec/exchanges/neraex/market_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+RSpec.describe Cryptoexchange::Exchanges::Neraex::Market do
+  it { expect(described_class::NAME).to eq 'neraex' }
+  it { expect(described_class::API_URL).to eq 'https://neraex.pro' }
+end
