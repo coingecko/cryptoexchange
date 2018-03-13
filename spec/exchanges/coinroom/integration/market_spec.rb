31e0cf66fc8aa0662dfd4b647caf139b19d8f280
cySheng <cyusheng93@gmail.com>
2018-03-13 15:45:15 +0800

    Removed orderbook and trades

--- spec/exchanges/coinroom/integration/market_spec.rb
+++ spec/exchanges/coinroom/integration/market_spec.rb
@@ -31,37 +31,4 @@ RSpec.describe 'Coinroom integration specs' do
     expect(ticker.payload).to_not be nil
   end
 
-  it 'fetch order book' do
-    order_book = client.order_book(btc_usd_pair)
-
-    expect(order_book.base).to eq 'USD'
-    expect(order_book.target).to eq 'BTC'
-    expect(order_book.market).to eq 'coinroom'
-    expect(order_book.asks).to_not be_empty
-    expect(order_book.bids).to_not be_empty
-    expect(order_book.asks.first.price).to_not be_nil
-    expect(order_book.bids.first.amount).to_not be_nil
-    expect(order_book.bids.first.timestamp).to be_nil
-    expect(order_book.asks.count).to be > 10
-    expect(order_book.bids.count).to be > 10
-    expect(order_book.timestamp).to be_a Numeric
-    expect(order_book.payload).to_not be nil
-  end
-
-  it 'fetch trade' do
-    trades = client.trades(btc_usd_pair)
-    trade = trades.sample
-
-    expect(trades).to_not be_empty
-    expect(trade.base).to eq 'USD'
-    expect(trade.target).to eq 'BTC'
-    expect(trade.market).to eq 'coinroom'
-    expect(trade.trade_id).to_not be_nil
-    expect(['buy', 'sell']).to include trade.type
-    expect(trade.price).to_not be_nil
-    expect(trade.amount).to_not be_nil
-    expect(trade.timestamp).to be_a Numeric
-    expect(trade.payload).to_not be nil
-  end
-
 end
