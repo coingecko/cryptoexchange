11c0c4e64ef4586efb3857bad878bc6706046ebe
Esther Leong Yuet Mei <estherleongym@gmail.com>
2018-03-22 22:36:19 +0800

    Implemented Pairs, Tickers, OrderBook, and updated README for Bits Blockchain

--- lib/cryptoexchange/exchanges/bits_blockchain/services/pairs.rb
+++ lib/cryptoexchange/exchanges/bits_blockchain/services/pairs.rb
@@ -2,21 +2,20 @@ module Cryptoexchange::Exchanges
   module BitsBlockchain
     module Services
       class Pairs < Cryptoexchange::Services::Pairs
+        PAIRS_URL = "https://www.bitsblockchain.net/moon/v1/market/currencyStatic"
 
         def fetch
           output = super
           market_pairs = []
-          output.each do |pair|
+          output['currencyStatic']['currencyPairs'].each do |pair|
             market_pairs << Cryptoexchange::Models::MarketPair.new(
-                              base: pair[:base],
-                              target: pair[:target],
+                              base: pair[1]["tradedCcy"],
+                              target: pair[1]["settlementCcy"],
                               market: BitsBlockchain::Market::NAME
                             )
           end
-
           market_pairs
         end
-
       end
     end
   end
--- spec/exchanges/bits_blockchain/integration/market_spec.rb
+++ spec/exchanges/bits_blockchain/integration/market_spec.rb
@@ -47,20 +47,4 @@ RSpec.describe 'Bits Blockchain integration specs' do
     expect(order_book.timestamp).to be_a Numeric
     expect(order_book.payload).to_not be nil
   end
-
-  it 'fetch trade' do
-    trades = client.trades(atmc_usd_pair)
-    trade = trades.sample
-
-    expect(trades).to_not be_empty
-    expect(trade.base).to eq 'ATMC'
-    expect(trade.target).to eq 'USD'
-    expect(trade.market).to eq 'bits_blockchain'
-    expect(trade.trade_id).to_not be_nil
-    expect(['buy', 'sell']).to include trade.type
-    expect(trade.price).to_not be_nil
-    expect(trade.amount).to_not be_nil
-    expect(trade.timestamp).to be_a Numeric
-    expect(trade.payload).to_not be nil
-  end
 end
