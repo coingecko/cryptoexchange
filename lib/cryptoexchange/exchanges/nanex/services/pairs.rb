0833b54baae5c148753928a272e9fce0b5321a8d
Sam Lin <greenlin111@gmail.com>
2018-03-27 20:51:26 +0800

    add nanex pairs

---
+++ lib/cryptoexchange/exchanges/nanex/services/pairs.rb
@@ -0,0 +1,26 @@
+module Cryptoexchange::Exchanges
+  module Nanex
+    module Services
+      class Pairs < Cryptoexchange::Services::Pairs
+        PAIRS_URL = "#{Cryptoexchange::Exchanges::Nanex::Market::API_URL}/public/tickers"
+
+        def fetch
+          output = super
+          adapt(output)
+        end
+
+        def adapt(output)
+          output.collect do |_pair, value|
+            base = value['base']
+            target = value['quote']
+            market_pair = Cryptoexchange::Models::MarketPair.new(
+              base: base,
+              target: target,
+              market: Nanex::Market::NAME
+            )
+          end
+        end
+      end
+    end
+  end
+end
