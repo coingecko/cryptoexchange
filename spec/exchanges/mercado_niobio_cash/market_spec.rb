1f36e3cec06cf96496713b2b147b8f3e5ccfa8d5
Vitor Araujo <vitorgamer58@outlook.com>
2018-04-12 15:41:32 -0300

    Mercado Niobio cash Added and Minor Fixes
    refer to https://github.com/coingecko/cryptoexchange/issues/420

--- spec/exchanges/mercado_niobio_cash/market_spec.rb
+++ spec/exchanges/mercado_niobio_cash/market_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::MercadoNiobioCash::Market do
-  it { expect(described_class::NAME).to eq 'mercado_bitcoin' }
+  it { expect(described_class::NAME).to eq 'mercado_niobio_cash' }
   it { expect(described_class::API_URL).to eq 'https://www.mercadoniobiocash.com.br/public_api/' }
 end
