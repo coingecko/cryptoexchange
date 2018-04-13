0a2055bbb8ec93b7278346d2959e184fb247ed0a
Esther Leong Yuet Mei <estherleongym@gmail.com>
2018-04-13 10:09:01 +0800

    Stored 1E8 in variable

--- lib/cryptoexchange/exchanges/rightbtc/services/market.rb
+++ lib/cryptoexchange/exchanges/rightbtc/services/market.rb
@@ -36,16 +36,17 @@ module Cryptoexchange::Exchanges
         end
 
         def adapt(output, market_pair)
+          i = 10**8
           ticker = Cryptoexchange::Models::Ticker.new
           ticker.base      = market_pair.base
           ticker.target    = market_pair.target
           ticker.market    = Rightbtc::Market::NAME
-          ticker.ask       = NumericHelper.to_d(output['sell'].to_f/(10**8))
-          ticker.bid       = NumericHelper.to_d(output['buy'].to_f/(10**8))
-          ticker.last      = NumericHelper.to_d(output['last'].to_f/(10**8))
-          ticker.high      = NumericHelper.to_d(output['high'].to_f/(10**8))
-          ticker.low       = NumericHelper.to_d(output['low'].to_f/(10**8))
-          ticker.volume    = NumericHelper.to_d(output['vol24h'].to_f/(10**8))
+          ticker.ask       = NumericHelper.to_d(output['sell'].to_f/(i))
+          ticker.bid       = NumericHelper.to_d(output['buy'].to_f/(i))
+          ticker.last      = NumericHelper.to_d(output['last'].to_f/(i))
+          ticker.high      = NumericHelper.to_d(output['high'].to_f/(i))
+          ticker.low       = NumericHelper.to_d(output['low'].to_f/(i))
+          ticker.volume    = NumericHelper.to_d(output['vol24h'].to_f/(i))
           ticker.timestamp = Time.now.to_i
           ticker.payload   = output
           ticker
