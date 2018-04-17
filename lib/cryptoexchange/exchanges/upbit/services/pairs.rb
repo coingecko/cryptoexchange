8bd23e647d5f370ef78da5ac6dcc3736eb0ae7d9
Sam.Lin <greenlin111@gmail.com>
2018-04-17 12:43:56 +0800

    check market status then list pairs

--- lib/cryptoexchange/exchanges/upbit/services/pairs.rb
+++ lib/cryptoexchange/exchanges/upbit/services/pairs.rb
@@ -6,16 +6,14 @@ module Cryptoexchange::Exchanges
 
         def fetch
           raw_output = HTTP.use(:auto_inflate).headers("Accept-Encoding" => "gzip").get(PAIRS_URL)
-          output = JSON.parse(raw_output)
-          market_pairs = []
-          output.each do |pair|
-            market_pairs << Cryptoexchange::Models::MarketPair.new(
-                              base: pair["baseCurrencyCode"],
-                              target: pair["quoteCurrencyCode"],
-                              market: Upbit::Market::NAME
-                            )
-          end
-          market_pairs
+          output     = JSON.parse(raw_output)
+          output.map do |pair|
+            next unless pair['marketState'] == "ACTIVE"
+            Cryptoexchange::Models::MarketPair.new(
+              base:   pair["baseCurrencyCode"],
+              target: pair["quoteCurrencyCode"],
+              market: Upbit::Market::NAME)
+          end.compact
         end
       end
     end
