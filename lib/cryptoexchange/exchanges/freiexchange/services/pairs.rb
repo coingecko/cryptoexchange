117a6e14f421663c85426442ee98c0c93a5e696d
wenjieyek <dx3152@hotmail.com>
2018-04-19 10:01:59 +0800

    deleted pair yml file and created pair list by returnticker

--- lib/cryptoexchange/exchanges/freiexchange/services/pairs.rb
+++ lib/cryptoexchange/exchanges/freiexchange/services/pairs.rb
@@ -2,21 +2,23 @@ module Cryptoexchange::Exchanges
   module Freiexchange
     module Services
       class Pairs < Cryptoexchange::Services::Pairs
+        PAIRS_URL = "#{Cryptoexchange::Exchanges::Freiexchange::Market::API_URL}/returnTicker"
 
         def fetch
           output = super
-          market_pairs = []
-          output.each do |pair|
-            market_pairs << Cryptoexchange::Models::MarketPair.new(
-                              base: pair[:base],
-                              target: pair[:target],
-                              market: Freiexchange::Market::NAME
-                            )
-          end
-
-          market_pairs
+          adapt(output)
         end
 
+        def adapt(output)
+          output.map do |pair, output|
+             base, target = pair.split("_")
+            Cryptoexchange::Models::MarketPair.new(
+              base: base,
+              target: target,
+              market: Freiexchange::Market::NAME
+            )
+          end
+        end
       end
     end
   end
