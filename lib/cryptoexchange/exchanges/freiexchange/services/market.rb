62c2fa9bf2ef29ffbaf12157f7dc381d8b56f6c5
wenjieyek <dx3152@hotmail.com>
2018-04-20 13:11:47 +0800

    added ticker.first to call data inside array

--- lib/cryptoexchange/exchanges/freiexchange/services/market.rb
+++ lib/cryptoexchange/exchanges/freiexchange/services/market.rb
@@ -19,13 +19,16 @@ module Cryptoexchange::Exchanges
 
         def adapt_all(output)
           output.map do |pair, ticker|
-            base, target = pair.split("_")
-            market_pair = Cryptoexchange::Models::MarketPair.new(
-                            base: base,
-                            target: target,
-                            market: Freiexchange::Market::NAME
-                          )
-            adapt(market_pair, ticker)
+            ticker = ticker.first
+            if pair && ticker
+              base, target = pair.split("_")
+              market_pair = Cryptoexchange::Models::MarketPair.new(
+                              base: base,
+                              target: target,
+                              market: Freiexchange::Market::NAME
+                            )
+              adapt(market_pair, ticker)
+            end
           end
         end
 
@@ -35,10 +38,10 @@ module Cryptoexchange::Exchanges
           ticker.base = market_pair.base
           ticker.target = market_pair.target
           ticker.market = Freiexchange::Market::NAME
-          ticker.volume = NumericHelper.to_d(market.first['volume24h'])
-          ticker.high = NumericHelper.to_d(market.first['high'])
-          ticker.low = NumericHelper.to_d(market.first['low'])
-          ticker.last = NumericHelper.to_d(market.first['last'])
+          ticker.volume = NumericHelper.to_d(market['volume24h'])
+          ticker.high = NumericHelper.to_d(market['high'])
+          ticker.low = NumericHelper.to_d(market['low'])
+          ticker.last = NumericHelper.to_d(market['last'])
           ticker.timestamp = Time.now.to_i
           ticker.payload = market
           ticker
