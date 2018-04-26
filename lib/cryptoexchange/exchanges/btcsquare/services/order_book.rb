8f5f4e5a257351eaf503e67d170fab9fc524bd2e
Sam Lin <greenlin111@gmail.com>
2018-04-26 17:35:40 +0800

    implement btcsquare order book

---
+++ lib/cryptoexchange/exchanges/btcsquare/services/order_book.rb
@@ -0,0 +1,45 @@
+module Cryptoexchange::Exchanges
+  module Btcsquare
+    module Services
+      class OrderBook < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            true
+          end
+        end
+
+        def fetch(market_pair)
+          output = super(ticker_url(market_pair))
+          adapt(output, market_pair)
+        end
+
+        def ticker_url(market_pair)
+          base   = market_pair.base
+          target = market_pair.target
+          "#{Cryptoexchange::Exchanges::Btcsquare::Market::API_URL}/orders/#{target}-#{base}"
+        end
+
+        def adapt(output, market_pair)
+          order_book = Cryptoexchange::Models::OrderBook.new
+
+          order_book.base      = market_pair.base
+          order_book.target    = market_pair.target
+          order_book.market    = Btcsquare::Market::NAME
+          order_book.asks      = adapt_orders output['sell']
+          order_book.bids      = adapt_orders output['buy']
+          order_book.timestamp = Time.now.to_i
+          order_book.payload   = output
+          order_book
+        end
+
+        def adapt_orders(orders)
+          orders.collect do |price, amount|
+            Cryptoexchange::Models::Order.new(price: price,
+                                              amount: amount,
+                                              timestamp: nil)
+          end
+        end
+      end
+    end
+  end
+end
