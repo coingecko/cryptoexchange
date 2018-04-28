c3750feace457e193111d3ec3cc37a22b581bd7e
Sam Lin <greenlin111@gmail.com>
2018-04-28 13:41:30 +0800

    fetch cryptex order book info

---
+++ lib/cryptoexchange/exchanges/cryptex/services/order_book.rb
@@ -0,0 +1,48 @@
+module Cryptoexchange::Exchanges
+  module Cryptex
+    module Services
+      class OrderBook < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            true
+          end
+        end
+
+        def fetch(market_pair)
+          output = super(ticker_url(market_pair))
+          adapt(output, market_pair)
+        end
+
+        def ticker_url(market_pair)
+          base   = market_pair.base.downcase
+          target = market_pair.target.downcase
+          "#{Cryptoexchange::Exchanges::Cryptex::Market::API_URL}/#{base}_#{target}/orderbook"
+        end
+
+        def adapt(output, market_pair)
+          order_book = Cryptoexchange::Models::OrderBook.new
+          timestamp  = Time.now.to_i
+          data       = output['data']
+
+          order_book.base      = market_pair.base
+          order_book.target    = market_pair.target
+          order_book.market    = Cryptex::Market::NAME
+          order_book.asks      = adapt_orders(data['asks'])
+          order_book.bids      = adapt_orders(data['bids'])
+          order_book.timestamp = timestamp
+          order_book.payload   = output
+          order_book
+        end
+
+        def adapt_orders(orders)
+          orders.collect do |order_entry|
+            price, amount = order_entry
+            Cryptoexchange::Models::Order.new(price:     price,
+                                              amount:    amount,
+                                              timestamp: nil)
+          end
+        end
+      end
+    end
+  end
+end
