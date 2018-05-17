050cf23b0c60e5cad02f952fe1a4f83eae8eea97
Sam.Lin <greenlin111@gmail.com>
2018-05-17 12:42:28 +0800

    add rspec test case on bitpaction

---
+++ spec/exchanges/bitpaction/integration/market_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+RSpec.describe 'Bitpaction integration specs' do
+  let(:client) { Cryptoexchange::Client.new }
+  let(:bps_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'BPS', target: 'BTC', market: 'bitpaction') }
+
+  it 'fetch pairs' do
+    pairs = client.pairs('bitpaction')
+    expect(pairs).not_to be_empty
+
+    pair = pairs.first
+    expect(pair.base).to_not be nil
+    expect(pair.target).to_not be nil
+    expect(pair.market).to eq 'bitpaction'
+  end
+
+  it 'fetch ticker' do
+    ticker = client.ticker(bps_btc_pair)
+
+    expect(ticker.base).to eq 'BPS'
+    expect(ticker.target).to eq 'BTC'
+    expect(ticker.market).to eq 'bitpaction'
+    expect(ticker.last).to be_a Numeric
+    expect(ticker.low).to be_a Numeric
+    expect(ticker.high).to be_a Numeric
+    expect(ticker.volume).to be_a Numeric
+    expect(ticker.change).to be_a Numeric
+    expect(ticker.timestamp).to be_a Numeric
+    expect(2000..Date.today.year).to include(Time.at(ticker.timestamp).year)
+    expect(ticker.payload).to_not be nil
+  end
+
+  it 'fetch order book' do
+    order_book = client.order_book(bps_btc_pair)
+
+    expect(order_book.base).to eq 'BPS'
+    expect(order_book.target).to eq 'BTC'
+    expect(order_book.market).to eq 'bitpaction'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to be_nil
+    expect(order_book.asks.count).to be > 0
+    expect(order_book.bids.count).to be > 0
+    expect(order_book.timestamp).to be_a Numeric
+    expect(order_book.payload).to_not be nil
+  end
+
+  it 'fetch trade' do
+    trades = client.trades(bps_btc_pair)
+    trade = trades.sample
+
+    expect(trades).to_not be_empty
+    expect(trade.base).to eq 'BPS'
+    expect(trade.target).to eq 'BTC'
+    expect(trade.market).to eq 'bitpaction'
+    expect(trade.trade_id).to be_nil
+    expect(['buy', 'sell']).to include trade.type
+    expect(trade.price).to_not be_nil
+    expect(trade.amount).to_not be_nil
+    expect(trade.timestamp).to be_a Numeric
+    expect(trade.payload).to_not be nil
+  end
+end
