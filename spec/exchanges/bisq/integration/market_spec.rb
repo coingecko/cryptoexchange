a964fdc69c13ba608a6adf6d3a9675480cecc2cb
Wenjie Yek <dx3152@hotmail.com>
2018-05-18 14:21:02 +0800

    implemented bisq pairs, tickers and trades

---
+++ spec/exchanges/bisq/integration/market_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+RSpec.describe 'Bisq integration specs' do
+  let(:client) { Cryptoexchange::Client.new }
+  let(:bch_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'BCH', target: 'BTC', market: 'bisq') }
+
+  it 'fetch pairs' do
+    pairs = client.pairs('bisq')
+    expect(pairs).not_to be_empty
+
+    pair = pairs.first
+    expect(pair.base).to_not be nil
+    expect(pair.target).to_not be nil
+    expect(pair.market).to eq 'bisq'
+  end
+
+  it 'fetch ticker' do
+    ticker = client.ticker(bch_btc_pair)
+
+    expect(ticker.base).to eq 'BCH'
+    expect(ticker.target).to eq 'BTC'
+    expect(ticker.market).to eq 'bisq'
+
+    expect(ticker.last).to be_a Numeric
+    expect(ticker.high).to be_a Numeric
+    expect(ticker.low).to be_a Numeric
+    expect(ticker.volume).to be_a Numeric
+
+    expect(ticker.timestamp).to be_a Numeric
+    expect(2000..Date.today.year).to include(Time.at(ticker.timestamp).year)
+    expect(ticker.payload).to_not be nil
+  end
+
+  it 'fetch trade' do
+    trades = client.trades(bch_btc_pair)
+    trade = trades.sample
+
+    expect(trades).to_not be_empty
+    expect(trade.base).to eq 'BCH'
+    expect(trade.target).to eq 'BTC'
+    expect(trade.market).to eq 'bisq'
+
+    expect(trade.trade_id).to_not be_nil
+    expect(['BUY', 'SELL']).to include trade.type
+    expect(trade.price).to_not be_nil
+    expect(trade.amount).to_not be_nil
+    expect(trade.timestamp).to be_a Numeric
+    expect(trade.payload).to_not be nil
+  end
+end
