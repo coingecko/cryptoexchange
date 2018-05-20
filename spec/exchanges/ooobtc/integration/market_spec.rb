81eeec5a422608fc683104733fb926c2e1d6d852
Sam Lin <greenlin111@gmail.com>
2018-05-20 19:12:52 +0800

    Add test case on ooobtc

---
+++ spec/exchanges/ooobtc/integration/market_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+require 'pry'
+
+RSpec.describe 'Ooobtc integration specs' do
+  let(:client) { Cryptoexchange::Client.new }
+  let(:eth_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'ETH', target: 'BTC', market: 'ooobtc') }
+
+  it 'fetch pairs and assign the correct base/target' do
+    pairs = client.pairs('ooobtc')
+    expect(pairs).not_to be_empty
+
+    pair = pairs.first
+    expect(pair.base).to_not be nil
+    expect(pair.target).to_not be nil
+    expect(pair.market).to eq 'ooobtc'
+  end
+
+  it 'fetch ticker' do
+    ticker = client.ticker(eth_btc_pair)
+
+    expect(ticker.base).to eq 'ETH'
+    expect(ticker.target).to eq 'BTC'
+    expect(ticker.market).to eq 'ooobtc'
+    expect(ticker.last).to be_a Numeric
+    expect(ticker.bid).to be_a Numeric
+    expect(ticker.ask).to be_a Numeric
+    expect(ticker.volume).to be_a Numeric
+    expect(ticker.timestamp).to be_a Numeric
+    expect(ticker.payload).to_not be nil
+  end
+
+  it 'fetch order_book' do
+    order_book = client.order_book(eth_btc_pair)
+
+    expect(order_book.base).to eq 'ETH'
+    expect(order_book.target).to eq 'BTC'
+    expect(order_book.market).to eq 'ooobtc'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be nil
+    expect(order_book.bids.first.amount).to_not be nil
+    expect(order_book.bids.first.timestamp).to be nil
+    expect(order_book.asks.count).to_not be nil
+    expect(order_book.bids.count).to_not be nil
+    expect(order_book.timestamp).to be_a Numeric
+    expect(order_book.payload).to_not be nil
+  end
+
+  it 'fetch trades' do
+    trades = client.trades(eth_btc_pair)
+    trade = trades.sample
+
+    expect(trades).to_not be_empty
+    expect(trade.base).to eq 'ETH'
+    expect(trade.target).to eq 'BTC'
+    expect(trade.market).to eq 'ooobtc'
+    expect(trade.price).to_not be nil
+    expect(trade.amount).to_not be nil
+    expect(trade.timestamp).to be_a Numeric
+    expect(trade.payload).to_not be nil
+  end
+end
