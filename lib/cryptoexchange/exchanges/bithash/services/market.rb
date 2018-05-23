c7867c6e59da39a47869e05876972b59ec2b82d1
Sam Lin <sam@swingcloud.app>
2018-05-23 20:24:19 +0800

    Implement Bithash Ticker, Trades and Orderbook  (#503)
    * implement bithash ticker
    
    * implement bithash market
    
    * implement bithash order book
    
    * implement bithash trades
    
    * add rspec test case on bithash
    
    * add Bithash on readme

---
+++ lib/cryptoexchange/exchanges/bithash/services/market.rb
@@ -0,0 +1,50 @@
+module Cryptoexchange::Exchanges
+  module Bithash
+    module Services
+      class Market < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            false
+          end
+        end
+
+        def fetch
+          output = super ticker_url
+          adapt_all(output)
+        end
+
+        def ticker_url
+          "#{Cryptoexchange::Exchanges::Bithash::Market::API_URL}/ticker"
+        end
+
+        def adapt_all(output)
+          output.map do |pair, market|
+            base, target = pair.split("_")
+            market_pair  = Cryptoexchange::Models::MarketPair.new(
+              base:   base,
+              target: target,
+              market: Bithash::Market::NAME
+            )
+            adapt(market, market_pair)
+          end
+        end
+
+        def adapt(output, market_pair)
+          ticker           = Cryptoexchange::Models::Ticker.new
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
+          ticker.market    = Bithash::Market::NAME
+          ticker.ask       = NumericHelper.to_d(output['sell'])
+          ticker.bid       = NumericHelper.to_d(output['buy'])
+          ticker.last      = NumericHelper.to_d(output['last'])
+          ticker.high      = NumericHelper.to_d(output['high'])
+          ticker.low       = NumericHelper.to_d(output['low'])
+          ticker.volume    = NumericHelper.to_d(output['vol'])
+          ticker.timestamp = NumericHelper.to_d(output['updated'])
+          ticker.payload   = output
+          ticker
+        end
+      end
+    end
+  end
+end
---
+++ spec/exchanges/bithash/integration/market_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+
+RSpec.describe 'Bithash integration specs' do
+  let(:client) { Cryptoexchange::Client.new }
+  let(:eth_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'eth', target: 'btc', market: 'bithash') }
+
+  it 'fetch pairs' do
+    pairs = client.pairs('bithash')
+    expect(pairs).not_to be_empty
+
+    pair = pairs.sample
+    expect(pair.base).to_not be nil
+    expect(pair.target).to_not be nil
+    expect(pair.market).to eq 'bithash'
+  end
+
+  it 'fetch ticker' do
+    ticker = client.ticker(eth_btc_pair)
+
+    expect(ticker.base).to eq 'ETH'
+    expect(ticker.target).to eq 'BTC'
+    expect(ticker.market).to eq 'bithash'
+    expect(ticker.ask).to be_a Numeric
+    expect(ticker.bid).to be_a Numeric
+    expect(ticker.last).to be_a Numeric
+    expect(ticker.high).to be_a Numeric
+    expect(ticker.low).to be_a Numeric
+    expect(ticker.volume).to be_a Numeric
+    expect(ticker.timestamp).to be_a Numeric
+    expect(2000..Date.today.year).to include(Time.at(ticker.timestamp).year)
+    expect(ticker.payload).to_not be nil
+  end
+
+  it 'fetch order book' do
+    order_book = client.order_book(eth_btc_pair)
+
+    expect(order_book.base).to eq 'ETH'
+    expect(order_book.target).to eq 'BTC'
+    expect(order_book.market).to eq 'bithash'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to be_nil
+    expect(order_book.timestamp).to be_a Numeric
+    expect(order_book.payload).to_not be nil
+  end
+
+  it 'fetch trade' do
+    trades = client.trades(eth_btc_pair)
+    trade = trades.sample
+
+    expect(trades).to_not be_empty
+    expect(trade.trade_id).to_not be_nil
+    expect(trade.base).to eq 'ETH'
+    expect(trade.target).to eq 'BTC'
+    expect(trade.price).to_not be_nil
+    expect(trade.amount).to_not be_nil
+    expect(trade.timestamp).to be_a Numeric
+    expect(trade.payload).to_not be nil
+    expect(trade.market).to eq 'bithash'
+  end
+end
