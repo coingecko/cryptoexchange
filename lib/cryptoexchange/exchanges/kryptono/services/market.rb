5906cb78310e3ebfbf95e006a7d1495aac99d37b
Sam Lin <greenlin111@gmail.com>
2018-05-27 13:26:20 +0800

    Implement Kryptono ticker

---
+++ lib/cryptoexchange/exchanges/kryptono/services/market.rb
@@ -0,0 +1,51 @@
+module Cryptoexchange::Exchanges
+  module Kryptono
+    module Services
+      class Market < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            true
+          end
+        end
+
+        def fetch(market_pair)
+          ticker_output = super(ticker_url(market_pair, "1m"))
+          volume_output = super(ticker_url(market_pair, "1d"))
+          adapt(ticker_output, volume_output, market_pair)
+        end
+
+        def ticker_url(market_pair, tt)
+          base   = market_pair.base
+          target = market_pair.target
+          "#{Cryptoexchange::Exchanges::Kryptono::Market::API_URL}/cs?symbol=#{base}_#{target}&tt=#{tt}"
+        end
+
+
+        # [
+        #   1527379200000, timestamp
+        #   "0.07971168",  open
+        #   "0.07971168",  high
+        #   "0.07971168",  low
+        #   "0.07971168",  close
+        #   "0.00000000"   volume
+        # ]
+
+        def adapt(ticker_output, volume_output, market_pair)
+          ticker_value     = ticker_output['series'].last
+          volume           = volume_output['series'].last[-1]
+          ticker           = Cryptoexchange::Models::Ticker.new
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
+          ticker.market    = Kryptono::Market::NAME
+          ticker.last      = NumericHelper.to_d(ticker_value[4])
+          ticker.high      = NumericHelper.to_d(ticker_value[2])
+          ticker.low       = NumericHelper.to_d(ticker_value[3])
+          ticker.volume    = NumericHelper.to_d(volume)
+          ticker.timestamp = ticker_value[0] / 1000
+          ticker.payload   = [ticker_output, volume_output]
+          ticker
+        end
+      end
+    end
+  end
+end
