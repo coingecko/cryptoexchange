d3deaa4e6f6304ec26af959b96d6d38a4d60d16c
Daniel Cheng <danielchengml@gmail.com>
2018-05-29 22:57:54 -0700

    Implement Exrates Ticker, Market and update README Issue #419

---
+++ lib/cryptoexchange/exchanges/exrates/market.rb
@@ -0,0 +1,8 @@
+module Cryptoexchange::Exchanges
+  module Exrates
+    class Market
+      NAME='exrates'
+      API_URL = 'https://exrates.me/public/coinmarketcap/ticker'
+    end
+  end
+end
--- lib/cryptoexchange/exchanges/exrates/services/pairs.rb
+++ lib/cryptoexchange/exchanges/exrates/services/pairs.rb
@@ -1,8 +1,8 @@
 module Cryptoexchange::Exchanges
-  module Exmo
+  module Exrates
     module Services
       class Pairs < Cryptoexchange::Services::Pairs
-        PAIRS_URL = "#{Cryptoexchange::Exchanges::Exmo::Market::API_URL}/pair_settings"
+        PAIRS_URL = "#{Cryptoexchange::Exchanges::Exrates::Market::API_URL}"
 
         def fetch
           output = super
@@ -14,10 +14,11 @@ module Cryptoexchange::Exchanges
           market_pairs = []
           pairs.each do |pair|
             base, target = pair.split('_')
-            market_pairs << Cryptoexchange::Models::MarketPair.new(
+            market_pairs << 
+            Cryptoexchange::Models::MarketPair.new(
               base: base,
-              target: target,
-              market: Exmo::Market::NAME
+              target: target, 
+              market: Exrates::Market::NAME
             )
           end
           market_pairs
