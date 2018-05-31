aefd190748d1de4eb1b39fe3c9ab21944f254311
TM Lee <tmlee.ltm@gmail.com>
2018-05-31 13:02:58 +0800

    Fix kryptono specs

--- spec/exchanges/kryptono/integration/market_spec.rb
+++ spec/exchanges/kryptono/integration/market_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe 'Kryptono integration specs' do
   let(:client) { Cryptoexchange::Client.new }
-  let(:eos_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'EOS', target: 'BTC', market: 'kryptono') }
+  let(:eos_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'KNOW', market: 'kryptono') }
 
   it 'fetch pairs' do
     pairs = client.pairs('kryptono')
@@ -17,8 +17,8 @@ RSpec.describe 'Kryptono integration specs' do
   it 'fetch ticker' do
     ticker = client.ticker(eos_btc_pair)
 
-    expect(ticker.base).to eq 'EOS'
-    expect(ticker.target).to eq 'BTC'
+    expect(ticker.base).to eq 'BTC'
+    expect(ticker.target).to eq 'KNOW'
     expect(ticker.market).to eq 'kryptono'
     expect(ticker.last).to be_a Numeric
     expect(ticker.high).to be_a Numeric
@@ -32,8 +32,8 @@ RSpec.describe 'Kryptono integration specs' do
   it 'fetch order book' do
     order_book = client.order_book(eos_btc_pair)
 
-    expect(order_book.base).to eq 'EOS'
-    expect(order_book.target).to eq 'BTC'
+    expect(order_book.base).to eq 'BTC'
+    expect(order_book.target).to eq 'KNOW'
     expect(order_book.market).to eq 'kryptono'
     expect(order_book.asks).to_not be_empty
     expect(order_book.bids).to_not be_empty
@@ -51,8 +51,8 @@ RSpec.describe 'Kryptono integration specs' do
     trade = trades.sample
 
     expect(trades).to_not be_empty
-    expect(trade.base).to eq 'EOS'
-    expect(trade.target).to eq 'BTC'
+    expect(trade.base).to eq 'BTC'
+    expect(trade.target).to eq 'KNOW'
     expect(trade.trade_id).to_not be nil
     expect(trade.price).to_not be nil
     expect(trade.amount).to_not be nil
--- spec/exchanges/kryptono/market_spec.rb
+++ spec/exchanges/kryptono/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Kryptono::Market do
   it { expect(described_class::NAME).to eq 'kryptono' }
-  it { expect(described_class::API_URL).to eq 'https://engine.kryptono.exchange/api/v1' }
+  it { expect(described_class::API_URL).to eq 'https://engines.kryptono.exchange/api/v1' }
 end
