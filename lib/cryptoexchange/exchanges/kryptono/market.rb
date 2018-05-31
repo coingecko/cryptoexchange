088992ea081b1c48f47e0a5e2919cbd045805002
TM Lee <tmlee.ltm@gmail.com>
2018-05-31 10:10:54 +0800

    Kyrptono update root endpoint and pairs

--- lib/cryptoexchange/exchanges/kryptono/market.rb
+++ lib/cryptoexchange/exchanges/kryptono/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Kryptono
     class Market
       NAME = 'kryptono'
-      API_URL = 'https://engine.kryptono.exchange/api/v1'
+      API_URL = 'https://engines.kryptono.exchange/api/v1'
     end
   end
 end
