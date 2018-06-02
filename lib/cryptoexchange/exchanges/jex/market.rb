3b117c45d3ba8297cd5b794e98593cd79b1a96b3
Daniel Cheng <danielchengml@gmail.com>
2018-06-01 20:08:08 -0700

    Implements JEX Exchange: Ticker and MarketPairs, Update README, #562 (#572)
    * Implements Jex Exchange: Ticker and MarketPairs, Update README, #562
    
    * JEX: Update 'check derivatives(ETF/Call/Put)' Method for both market and Pairs, add Test Specs-(Check derivatives Removed)
    
    * remove cryptoexchange-0.22.2.gem
    
    * Update README by removing JEX
    
    * add JEX intoi README
    
    * Update README. Revert formatting

---
+++ lib/cryptoexchange/exchanges/jex/market.rb
@@ -0,0 +1,8 @@
+module Cryptoexchange::Exchanges
+  module Jex
+    class Market
+      NAME = 'jex'
+      API_URL = 'https://www.jex.com/api/v2/pub/jexcap'
+    end
+  end
+end
---
+++ lib/cryptoexchange/exchanges/jex/services/market.rb
@@ -0,0 +1,70 @@
+module Cryptoexchange::Exchanges
+  module Jex
+    module Services
+      class Market < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            false
+          end
+        end
+
+        def fetch
+          output = super(self.ticker_url)
+          adapt_all(output)
+        end
+
+        def ticker_url
+          "#{Cryptoexchange::Exchanges::Jex::Market::API_URL}"
+        end
+
+        def adapt_all(output)
+          market_pairs = {}
+          output.each do |pair|
+            if !derivative(pair[0])
+            market_pairs[pair[0]] = pair[1]
+            end
+          end
+          market_pairs.map do |pair, ticker|
+            base, target = pair.split('/')
+            market_pair = Cryptoexchange::Models::MarketPair.new(
+              base: base.upcase,
+              target: target.upcase,
+              market: Jex::Market::NAME
+            )
+            adapt(ticker, market_pair)
+          end
+        end
+
+        def derivative(pair)
+          etf = /(ETF)/ =~ pair
+          option = /( PUT | CALL)/ =~ pair
+          if etf && !pair.include?("/")
+            true  
+          elsif option && pair.count("/") > 1
+            true
+          else
+            false
+          end
+        end
+
+        def adapt(output, market_pair)
+          ticker            = Cryptoexchange::Models::Ticker.new
+          ticker.base       = market_pair.base
+          ticker.target     = market_pair.target
+          ticker.market     = Jex::Market::NAME
+          ticker.last       = NumericHelper.to_d(output['last'])
+          ticker.bid        = NumericHelper.to_d(output['highestBid'])
+          ticker.ask        = NumericHelper.to_d(output['lowestAsk'])
+          ticker.change     = NumericHelper.to_d(output['percentChange'])*100
+          ticker.high       = NumericHelper.to_d(output['high24hr'])
+          ticker.low        = NumericHelper.to_d(output['low24hr'])
+          ticker.volume     = NumericHelper.to_d(output['baseVolume'])
+          ticker.timestamp  = DateTime.now.to_time.to_i
+          ticker.payload    = output
+          ticker
+        end
+
+      end
+    end
+  end
+end
