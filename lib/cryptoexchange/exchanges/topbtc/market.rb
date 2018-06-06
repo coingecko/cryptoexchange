a7fcee7164644ae9d8854d85055146f691d59188
Daniel Cheng <daniel@coingecko.com>
2018-06-05 23:19:02 -0700

    implement TopBtc

---
+++ lib/cryptoexchange/exchanges/topbtc/market.rb
@@ -0,0 +1,8 @@
+module Cryptoexchange::Exchanges
+  module Topbtc
+    class Market
+      NAME = 'topbtc'
+      API_URL = 'http://www.topbtc.one/market/tickerall.php'
+    end
+  end
+end
---
+++ spec/exchanges/topbtc/integration/market_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+RSpec.describe 'Topbtc integration specs' do
+  let(:client) { Cryptoexchange::Client.new }
+  let(:uqc_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'UQC', target: 'BTC', market: 'topbtc') }
+
+  it 'fetch pairs' do
+    pairs = client.pairs('topbtc')
+    expect(pairs).not_to be_empty
+
+    pair = pairs.first
+    expect(pair.base).not_to be_nil
+    expect(pair.target).not_to be_nil
+    expect(pair.market).to eq 'topbtc'
+  end
+
+  it 'fetch ticker' do
+    ticker = client.ticker(uqc_btc_pair)
+
+    expect(ticker.base).to eq 'UQC'
+    expect(ticker.target).to eq 'BTC'
+    expect(ticker.market).to eq 'topbtc'
+
+    expect(ticker.last).to be_a Numeric
+    expect(ticker.bid).to be_a Numeric
+    expect(ticker.ask).to be_a Numeric
+    expect(ticker.high).to be_a Numeric
+    expect(ticker.volume).to be_a Numeric
+    
+    expect(ticker.timestamp).to be_a Numeric
+    expect(2000..Date.today.year).to include(Time.at(ticker.timestamp).year)
+    expect(ticker.payload).to_not be nil
+  end
+end
