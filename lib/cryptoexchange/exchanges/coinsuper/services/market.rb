00004b60aa49674eda6ecbad0015bc5682ca2471
estherleongym <30499981+estherleongym@users.noreply.github.com>
2018-06-06 11:51:44 +1000

    Added isFrozen==0 check before proceeding with parsing (#584)

--- lib/cryptoexchange/exchanges/coinsuper/services/market.rb
+++ lib/cryptoexchange/exchanges/coinsuper/services/market.rb
@@ -19,13 +19,15 @@ module Cryptoexchange::Exchanges
 
         def adapt_all(output)
           output['data'].map do |pair|
-            base, target = pair[0].split('/')
-            market_pair = Cryptoexchange::Models::MarketPair.new(
-                            base: base,
-                            target: target,
-                            market: Coinsuper::Market::NAME
-                          )
-            adapt(market_pair, pair[1])
+            if pair[1]['isFrozen']==0
+              base, target = pair[0].split('/')
+              market_pair = Cryptoexchange::Models::MarketPair.new(
+                              base: base,
+                              target: target,
+                              market: Coinsuper::Market::NAME
+                            )
+              adapt(market_pair, pair[1])
+            end
           end
         end
 
