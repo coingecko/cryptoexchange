66a88e69e6dd10d0be81684eb22d816df13c23b4
Sam Lin <greenlin111@gmail.com>
2018-06-06 11:43:08 +0800

    implement bilaxy ticker

---
+++ lib/cryptoexchange/exchanges/bilaxy/services/market.rb
@@ -0,0 +1,40 @@
+module Cryptoexchange::Exchanges
+  module Bilaxy
+    module Services
+      class Market < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            true
+          end
+        end
+
+        def fetch(market_pair)
+          output = super(ticker_url(market_pair))
+          adapt(output, market_pair)
+        end
+
+        def ticker_url(market_pair)
+          id = market_pair.id
+          "#{Cryptoexchange::Exchanges::Bilaxy::Market::API_URL}/ticker?symbol=#{id}"
+        end
+
+        def adapt(output, market_pair)
+          data = output['data']
+          ticker           = Cryptoexchange::Models::Ticker.new
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
+          ticker.market    = Bilaxy::Market::NAME
+          ticker.last      = NumericHelper.to_d(data['last'])
+          ticker.ask       = NumericHelper.to_d(data['sell'])
+          ticker.bid       = NumericHelper.to_d(data['buy'])
+          ticker.low       = NumericHelper.to_d(data['low'])
+          ticker.high      = NumericHelper.to_d(data['high'])
+          ticker.volume    = NumericHelper.to_d(data['vol'])
+          ticker.timestamp = Time.now.to_i
+          ticker.payload   = output
+          ticker
+        end
+      end
+    end
+  end
+end
