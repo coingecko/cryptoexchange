17823fb89d9dd791aea1819ffeac6824373ad5f0
Sam Lin <greenlin111@gmail.com>
2018-06-06 16:32:32 +0800

    implement cfinex ticker

---
+++ lib/cryptoexchange/exchanges/cfinex/services/market.rb
@@ -0,0 +1,52 @@
+module Cryptoexchange::Exchanges
+  module Cfinex
+    module Services
+      class Market < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            false
+          end
+        end
+
+        def fetch
+          output = super ticker_url
+          adapt_all(output)
+        end
+
+        def ticker_url
+          "#{Cryptoexchange::Exchanges::Cfinex::Market::API_URL}/tickerapi"
+        end
+
+        def adapt_all(output)
+          output.map do |pair, ticker|
+            next if ticker['isFrozen'] == 1
+            base, target = pair.split('_')
+            market_pair  = Cryptoexchange::Models::MarketPair.new(
+              base:   base,
+              target: target,
+              market: Cfinex::Market::NAME
+            )
+            adapt(ticker, market_pair)
+          end.compact
+        end
+
+        def adapt(output, market_pair)
+          ticker           = Cryptoexchange::Models::Ticker.new
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
+          ticker.market    = Cfinex::Market::NAME
+          ticker.bid       = NumericHelper.to_d(output['highestBid'])
+          ticker.ask       = NumericHelper.to_d(output['lowestAsk'])
+          ticker.last      = NumericHelper.to_d(output['last'])
+          ticker.volume    = NumericHelper.to_d(output['quoteVolume'])
+          ticker.high      = NumericHelper.to_d(output['high24hr'])
+          ticker.low       = NumericHelper.to_d(output['low24hr'])
+          ticker.change    = NumericHelper.to_d(output['percentChange'])
+          ticker.timestamp = Time.now.to_i
+          ticker.payload   = output
+          ticker
+        end
+      end
+    end
+  end
+end
