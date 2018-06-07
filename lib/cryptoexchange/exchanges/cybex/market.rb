c2095c3d296fadeef3fdf95e0bdfd99f2a0a53e0
Sam Lin <greenlin111@gmail.com>
2018-06-07 11:22:54 +0800

    use four main target symbol to generate pairs

--- lib/cryptoexchange/exchanges/cybex/market.rb
+++ lib/cryptoexchange/exchanges/cybex/market.rb
@@ -1,8 +1,18 @@
 module Cryptoexchange::Exchanges
   module Cybex
     class Market
-      NAME = 'cybex'
-      API_URL = 'https://beijing.51nebula.com'
+      NAME            = 'cybex'
+      API_URL         = 'https://beijing.51nebula.com'
+      USED_GATEWAY    = 'JADE'
+      OMITTED_GATEWAY = 'JAMES'
+
+      class << self
+        def prepend_symbol_prefix(pair)
+          base   = pair.base == 'CYB' ? pair.base : "#{USED_GATEWAY}.#{pair.base}"
+          target = pair.target == 'CYB' ? pair.target : "#{USED_GATEWAY}.#{pair.target}"
+          [base, target]
+        end
+      end
     end
   end
 end
