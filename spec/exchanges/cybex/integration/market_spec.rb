966959e1b3e16f85f5f40e31cb89a4e6c6a423d6
Sam Lin <greenlin111@gmail.com>
2018-06-07 11:23:53 +0800

    update test case

--- spec/exchanges/cybex/integration/market_spec.rb
+++ spec/exchanges/cybex/integration/market_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 RSpec.describe 'Cybex integration specs' do
   let(:client) { Cryptoexchange::Client.new }
-  let(:btc_usdt_pair) { Cryptoexchange::Models::MarketPair.new(base: 'JADE.BTC', target: 'JADE.USDT', market: 'cybex') }
+  let(:btc_usdt_pair) { Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'USDT', market: 'cybex') }
+  let(:cyb_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'CYB', target: 'BTC', market: 'cybex') }
 
   it 'fetch pairs' do
     pairs = client.pairs('cybex')
@@ -18,8 +19,8 @@ RSpec.describe 'Cybex integration specs' do
   it 'fetch ticker' do
     ticker = client.ticker(btc_usdt_pair)
 
-    expect(ticker.base).to eq 'JADE.BTC'
-    expect(ticker.target).to eq 'JADE.USDT'
+    expect(ticker.base).to eq 'BTC'
+    expect(ticker.target).to eq 'USDT'
     expect(ticker.market).to eq 'cybex'
     expect(ticker.last).to be_a Numeric
     expect(ticker.bid).to be_a Numeric
@@ -33,35 +34,35 @@ RSpec.describe 'Cybex integration specs' do
   end
 
   it 'fetch order book' do
-  order_book = client.order_book(btc_usdt_pair)
+    order_book = client.order_book(cyb_btc_pair)
 
-  expect(order_book.base).to eq 'JADE.BTC'
-  expect(order_book.target).to eq 'JADE.USDT'
-  expect(order_book.market).to eq 'cybex'
-  expect(order_book.asks).to_not be_empty
-  expect(order_book.bids).to_not be_empty
-  expect(order_book.asks.first.price).to_not be_nil
-  expect(order_book.bids.first.amount).to_not be_nil
-  expect(order_book.bids.first.timestamp).to be_nil
-  expect(order_book.asks.count).to be > 0
-  expect(order_book.bids.count).to be > 0
-  expect(order_book.timestamp).to be_a Numeric
-  expect(order_book.payload).to_not be nil
+    expect(order_book.base).to eq 'CYB'
+    expect(order_book.target).to eq 'BTC'
+    expect(order_book.market).to eq 'cybex'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to be_nil
+    expect(order_book.asks.count).to be > 0
+    expect(order_book.bids.count).to be > 0
+    expect(order_book.timestamp).to be_a Numeric
+    expect(order_book.payload).to_not be nil
 
-end
+  end
 
-it 'fetch trades' do
-  trades = client.trades(btc_usdt_pair)
-  trade  = trades.sample
+  it 'fetch trades' do
+    trades = client.trades(btc_usdt_pair)
+    trade  = trades.sample
 
-  expect(trades).to_not be_empty
-  expect(trade.base).to eq 'JADE.BTC'
-  expect(trade.target).to eq 'JADE.USDT'
-  expect(trade.market).to eq 'cybex'
-  expect(trade.trade_id).to be_nil
-  expect(trade.price).to_not be_nil
-  expect(trade.amount).to_not be_nil
-  expect(trade.timestamp).to be_a Numeric
-  expect(trade.payload).to_not be nil
-end
+    expect(trades).to_not be_empty
+    expect(trade.base).to eq 'BTC'
+    expect(trade.target).to eq 'USDT'
+    expect(trade.market).to eq 'cybex'
+    expect(trade.trade_id).to be_nil
+    expect(trade.price).to_not be_nil
+    expect(trade.amount).to_not be_nil
+    expect(trade.timestamp).to be_a Numeric
+    expect(trade.payload).to_not be nil
+  end
 end
