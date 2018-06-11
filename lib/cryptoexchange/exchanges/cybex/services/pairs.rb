a5064a6001ac751a2a9f5591c56b678a73cd1f4f
Sam Lin <sam@swingcloud.app>
2018-06-11 12:23:43 +0800

    [#Hotfix] new way to match cybex pairs (#612)
    * use four main target symbol to generate pairs
    
    * prepend jade if symbol is not cyb
    
    * update test case
    
    * use ruby 2.2 compatible syntax

--- lib/cryptoexchange/exchanges/cybex/services/pairs.rb
+++ lib/cryptoexchange/exchanges/cybex/services/pairs.rb
@@ -2,9 +2,12 @@ module Cryptoexchange::Exchanges
   module Cybex
     module Services
       class Pairs < Cryptoexchange::Services::Pairs
-        PAIRS_URL   = "#{Cryptoexchange::Exchanges::Cybex::Market::API_URL}"
-        HTTP_METHOD = 'POST'
-        POST_PARAMS = { "jsonrpc": "2.0", "method": "list_assets", "params": ["A", 100], "id": 1 }
+        PAIRS_URL           = "#{Cryptoexchange::Exchanges::Cybex::Market::API_URL}"
+        HTTP_METHOD         = 'POST'
+        POST_PARAMS         = { "jsonrpc": "2.0", "method": "list_assets", "params": ["A", 100], "id": 1 }
+        OMITTED_SYMBOLS     = ['ANGEL', 'JADE', 'JAMES']
+        MAIN_TARGET_SYMBOLS = ['USDT', 'BTC', 'ETH', 'CYB']
+
 
         def fetch
           output    = super
@@ -13,8 +16,18 @@ module Cryptoexchange::Exchanges
         end
 
         def list_all_combinations(output)
-          assets = output['result'].map { |asset| asset['symbol'] }
-          assets.combination(2).to_a
+          pairs_list = []
+          symbols    = output['result'].map { |asset| asset['symbol'] }
+          symbols    = symbols - OMITTED_SYMBOLS
+          symbols.delete_if { |sym| !/\A(#{Cryptoexchange::Exchanges::Cybex::Market::OMITTED_GATEWAY}\.)/.match(sym).nil? }
+          symbols      = symbols.map { |a| a.split('.').last }
+          symbol_pairs = symbols.product(MAIN_TARGET_SYMBOLS)
+          symbol_pairs.map do |pair|
+            next if pair[0] == pair[1] || pair[0] == 'USDT'
+            next if /(BTC|ETH|CYB)/.match(pair[0]) && /(BTC|ETH|CYB)/.match(pair[1])
+            pairs_list << pair
+          end
+          pairs_list
         end
 
         def adapt(pair_list)
