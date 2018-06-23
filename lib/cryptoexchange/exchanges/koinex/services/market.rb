72aa733aad6c2cf6c53f9f50ff1572fe1b35b35d
Jack Wong <jack@coingecko.com>
2018-06-24 00:51:30 +0800

    fix koinex api.

--- lib/cryptoexchange/exchanges/koinex/services/market.rb
+++ lib/cryptoexchange/exchanges/koinex/services/market.rb
@@ -14,17 +14,17 @@ module Cryptoexchange::Exchanges
         end
 
         def ticker_url
-          "#{Cryptoexchange::Exchanges::Koinex::Market::API_URL}/ticker"
+            "#{Cryptoexchange::Exchanges::Koinex::Market::API_URL}/ticker"
         end
 
         def adapt_all(output)
-          output["stats"].map do |pair, tickers|
+          output["stats"]["inr"].map do |pair, tickers|
             base = pair
             target = "INR"
             market_pair = Cryptoexchange::Models::MarketPair.new(
                             base: base,
                             target: target,
-                            market: Koinex::Market::NAME
+                            market: "koinex"
                           )
             adapt(market_pair, tickers, [pair, tickers])
           end
@@ -34,7 +34,7 @@ module Cryptoexchange::Exchanges
           ticker = Cryptoexchange::Models::Ticker.new
           ticker.base = market_pair.base
           ticker.target = market_pair.target
-          ticker.market = Koinex::Market::NAME
+          ticker.market = 'koinex'
           ticker.last = NumericHelper.to_d(tickers['last_traded_price'].to_f)
           ticker.bid = NumericHelper.to_d(tickers['highest_bid'].to_f)
           ticker.ask = NumericHelper.to_d(tickers['lowest_ask'].to_f)
