14b8482a3c60c2f61321ee3a561a0d6ff5bca37a
Wenjie Yek <dx3152@hotmail.com>
2018-07-06 15:12:11 +0800

    read pairs form /markets

--- lib/cryptoexchange/exchanges/radar_relay/services/pairs.rb
+++ lib/cryptoexchange/exchanges/radar_relay/services/pairs.rb
@@ -2,16 +2,19 @@ module Cryptoexchange::Exchanges
   module RadarRelay
     module Services
       class Pairs < Cryptoexchange::Services::Pairs
+        PAIRS_URL = "#{Cryptoexchange::Exchanges::RadarRelay::Market::API_URL}/markets"
+
         def fetch
           output = super
           adapt(output)
         end
 
         def adapt(output)
-          output.map do |pair|
+          output.map do |ticker|
+            base, target = ticker['displayName'].split('/')
             Cryptoexchange::Models::MarketPair.new({
-              base: pair[:base],
-              target: pair[:target],
+              base: base,
+              target: target,
               market: RadarRelay::Market::NAME
             })
           end
