a11624a92eb8367bbd36fee387386820fb3add07
Esther Leong Yuet Mei <estherleongym@gmail.com>
2018-07-08 20:17:01 +0800

    Changed id_fetcher to fire a call for each pair

--- lib/cryptoexchange/exchanges/liqnet/services/id_fetcher.rb
+++ lib/cryptoexchange/exchanges/liqnet/services/id_fetcher.rb
@@ -3,15 +3,13 @@ module Cryptoexchange::Exchanges
     module Services
       class IdFetcher
         PAIR_SYMBOLS_URL = "#{Cryptoexchange::Exchanges::Liqnet::Market::API_URL}/markets"
-        PAIR_SYMBOLS = []
-        
+
         def self.get_id(market_pair)
-          if PAIR_SYMBOLS.empty?
+          # To-do: aim to optimize call for pair id
            pairs_response = HTTP.get(PAIR_SYMBOLS_URL)
-           PAIR_SYMBOLS << pairs_response.parse(:json)
-          end
+           pairs_response = pairs_response.parse(:json)
             # filtering through asset dictionary to find market_pair and obtaining the market_pair base and target's ID
-            pair = PAIR_SYMBOLS[0].select{ |s| s['market'] == "#{market_pair.base.upcase}/#{market_pair.target.upcase}" }
+           pair = pairs_response.select{ |s| s['market'] == "#{market_pair.base.upcase}/#{market_pair.target.upcase}" }
           pair[0]['id']
         end
       end
