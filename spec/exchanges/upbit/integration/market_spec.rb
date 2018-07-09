a722250a7c83dbf593a62f7a221ec2459e5a71de
TM Lee <tmlee.ltm@gmail.com>
2018-07-09 09:23:19 +0800

    Update upbit specs

--- spec/exchanges/upbit/integration/market_spec.rb
+++ spec/exchanges/upbit/integration/market_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe 'Upbit integration specs' do
   let(:client) { Cryptoexchange::Client.new }
-  let(:krw_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'krw', target: 'btc', market: 'upbit') }
+  let(:krw_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'btc', target: 'krw', market: 'upbit') }
 
   it 'fetch pairs' do
     pairs = client.pairs('upbit')
@@ -22,8 +22,8 @@ RSpec.describe 'Upbit integration specs' do
   it 'fetch ticker' do
     ticker = client.ticker(krw_btc_pair)
 
-    expect(ticker.base).to eq 'KRW'
-    expect(ticker.target).to eq 'BTC'
+    expect(ticker.base).to eq 'BTC'
+    expect(ticker.target).to eq 'KRW'
     expect(ticker.market).to eq 'upbit'
     expect(ticker.last).to be_a Numeric
     expect(ticker.high).to be_a Numeric
@@ -37,8 +37,8 @@ RSpec.describe 'Upbit integration specs' do
   it 'fetch order book' do
     order_book = client.order_book(krw_btc_pair)
 
-    expect(order_book.base).to eq 'KRW'
-    expect(order_book.target).to eq 'BTC'
+    expect(order_book.base).to eq 'BTC'
+    expect(order_book.target).to eq 'KRW'
     expect(order_book.market).to eq 'upbit'
     expect(order_book.asks).to_not be_empty
     expect(order_book.bids).to_not be_empty
@@ -53,8 +53,8 @@ RSpec.describe 'Upbit integration specs' do
     trade = trades.sample
 
     expect(trades).to_not be_empty
-    expect(trade.base).to eq 'KRW'
-    expect(trade.target).to eq 'BTC'
+    expect(trade.base).to eq 'BTC'
+    expect(trade.target).to eq 'KRW'
     expect(trade.market).to eq 'upbit'
     expect(['buy', 'sell']).to include trade.type
     expect(trade.price).to_not be_nil
