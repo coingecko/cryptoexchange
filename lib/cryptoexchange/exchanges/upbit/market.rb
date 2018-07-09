43015b7259563c4ac1391c1aca02a56dcd5647c2
estherleongym <30499981+estherleongym@users.noreply.github.com>
2018-07-09 08:51:37 +0800

    Updated Pairs and Tickers, and implemented OrderBook and Trades for Upbit (#684)

--- lib/cryptoexchange/exchanges/upbit/market.rb
+++ lib/cryptoexchange/exchanges/upbit/market.rb
@@ -2,8 +2,7 @@ module Cryptoexchange::Exchanges
   module Upbit
     class Market
       NAME = 'upbit'
-      API_URL = 'https://crix-api-endpoint.upbit.com/v1/crix/candles'
-      PAIRS_URL = 'https://s3.ap-northeast-2.amazonaws.com/crix-production/crix_master'
+      API_URL = 'https://api.upbit.com/v1'
     end
   end
 end
