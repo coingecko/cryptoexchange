7a6871358955785b6acf45af228bc4e67aa08818
Sky Chin <sky@avana.asia>
2018-07-14 16:59:14 +0800

    Bitmart integration #671

---
+++ lib/cryptoexchange/exchanges/bitmart/services/market.rb
@@ -0,0 +1,37 @@
+module Cryptoexchange::Exchanges
+  module Bitmart
+    module Services
+      class Market < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            true
+          end
+        end
+
+        def fetch(market_pair)
+          url = ticker_url(market_pair)
+          output = super(url)
+          adapt(output, market_pair)
+        end
+
+        def ticker_url(market_pair)
+          "#{Cryptoexchange::Exchanges::Bitmart::Market::API_URL}/ticker/#{market_pair.base}_#{market_pair.target}"
+        end
+
+        def adapt(output, market_pair)
+          ticker           = Cryptoexchange::Models::Ticker.new
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
+          ticker.market    = Bitmart::Market::NAME
+          ticker.ask       = NumericHelper.to_d(output['ask_1'])
+          ticker.bid       = NumericHelper.to_d(output['bid_1'])
+          ticker.last      = NumericHelper.to_d(output['new_24h'])
+          ticker.volume    = NumericHelper.to_d(output['volume'])
+          ticker.timestamp = output['openTime']
+          ticker.payload   = output
+          ticker
+        end
+      end
+    end
+  end
+end
---
+++ lib/cryptoexchange/exchanges/bitmart/services/pairs.rb
@@ -0,0 +1,28 @@
+module Cryptoexchange::Exchanges
+  module Bitmart
+    module Services
+      class Pairs < Cryptoexchange::Services::Pairs
+        PAIRS_URL = "#{Cryptoexchange::Exchanges::Bitmart::Market::API_URL}/tickers/market_cap"
+
+        def fetch
+          output = super
+          adapt(output)
+        end
+
+        def adapt(output)
+          market_pairs = []
+          output.each do |value|
+            base, target = value['pair'].split('_')
+            market_pairs << Cryptoexchange::Models::MarketPair.new(
+                    base: base,
+                    target: target,
+                    market: Bitmart::Market::NAME
+                  )
+          end
+
+          market_pairs
+        end
+      end
+    end
+  end
+end
