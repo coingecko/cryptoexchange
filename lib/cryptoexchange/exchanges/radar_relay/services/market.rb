c0275ca256499c91f0255082da5d0cc6c00cdf9c
TM Lee <tmlee.ltm@gmail.com>
2018-07-18 15:58:41 +0800

    Hotfix RadarRelay volume

--- lib/cryptoexchange/exchanges/radar_relay/services/market.rb
+++ lib/cryptoexchange/exchanges/radar_relay/services/market.rb
@@ -26,7 +26,7 @@ module Cryptoexchange::Exchanges
           ticker.last      = NumericHelper.to_d(output['last'])
           ticker.high      = output['high'] == "NA" ? nil : NumericHelper.to_d(output['high'])
           ticker.low       = output['low'] == "NA" ? nil : NumericHelper.to_d(output['low'])
-          ticker.volume    = NumericHelper.to_d(output['baseVolume'])
+          ticker.volume    = NumericHelper.to_d(output['quoteVolume'])
           ticker.timestamp = Time.now.to_i
           ticker.payload   = output
           ticker
