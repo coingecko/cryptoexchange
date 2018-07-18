e6ade47d4d9c0ad9735c3346250eb343b58b7270
Jack Wong <jackyhwong91@gmail.com>
2018-07-18 16:20:55 +0800

    fix Bitso (bitso updated API endpoint) (#717)

--- lib/cryptoexchange/exchanges/bitso/market.rb
+++ lib/cryptoexchange/exchanges/bitso/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Bitso
     class Market < Cryptoexchange::Models::Market
       NAME = 'bitso'
-      API_URL = 'https://api-dev.bitso.com/v3'
+      API_URL = 'https://api.bitso.com/v3'
     end
   end
 end
