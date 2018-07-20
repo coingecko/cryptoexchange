c3aa110df4b76daf15ed8dad24c18ae672b72110
Esther Leong Yuet Mei <estherleongym@gmail.com>
2018-07-20 08:27:08 +0800

    Added trading URL for Orderbook.io

--- lib/cryptoexchange/exchanges/orderbook/market.rb
+++ lib/cryptoexchange/exchanges/orderbook/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'orderbook'
       API_URL = 'https://api.orderbook.io'
+
+      def self.trade_page_url(args={})
+        "https://www.orderbook.io/#/trading/#{args[:base]}-#{args[:target]}"
+      end
     end
   end
 end
