8c5a942d02251cd3b76446d96d40c77ebd6dcf23
Sky Chin <sky4just@gmail.com>
2018-07-21 07:10:45 +0800

    Bitmart integration #671 - Implements Order Book and Trades - Remove fail specs

--- spec/exchanges/bitmart/integration/market_spec.rb
+++ spec/exchanges/bitmart/integration/market_spec.rb
@@ -30,4 +30,33 @@ RSpec.describe 'Bitmart integration specs' do
     expect(2000..Date.today.year).to include(Time.at(ticker.timestamp).year)
     expect(ticker.payload).to_not be nil
   end
+
+  it 'fetch order book' do
+    order_book = client.order_book(btc_jpy_pair)
+
+    expect(order_book.base).to eq 'ABT'
+    expect(order_book.target).to eq 'BTC'
+    expect(order_book.market).to eq 'bitmart'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.count).to be > 0
+    expect(order_book.bids.count).to be > 0
+    expect(order_book.timestamp).to be_a Numeric
+    expect(order_book.payload).to_not be nil
+  end
+
+  it 'fetch trade' do
+    trades = client.trades(btc_jpy_pair)
+    trade = trades.sample
+
+    expect(trades).to_not be_empty
+    expect(trade.base).to eq 'ABT'
+    expect(trade.target).to eq 'BTC'
+    expect(trade.market).to eq 'bitmart'
+    expect(['buy', 'sell']).to include trade.type
+    expect(trade.price).to_not be_nil
+    expect(trade.amount).to_not be_nil
+    expect(trade.timestamp).to be_a Numeric
+    expect(trade.payload).to_not be nil
+  end
 end
