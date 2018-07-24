18e6a91270c48bf431f8345604e3ae2d2b89b346
Jack Wong <jackyhwong91@gmail.com>
2018-07-24 14:20:50 +0800

    update api endpoint to https://v1-1.api.token.store (#728)

--- lib/cryptoexchange/exchanges/thetokenstore/market.rb
+++ lib/cryptoexchange/exchanges/thetokenstore/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Thetokenstore
     class Market < Cryptoexchange::Models::Market
       NAME = 'thetokenstore'
-      API_URL = 'https://v1.api.token.store'
+      API_URL = 'https://v1-1.api.token.store'
     end
   end
 end
