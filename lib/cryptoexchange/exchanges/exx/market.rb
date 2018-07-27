5e2787c883c4c29517d391f2da9eb2aba8157488
Esther Leong Yuet Mei <estherleongym@gmail.com>
2018-07-27 23:16:00 +0800

    Added trading URL for EXX

--- lib/cryptoexchange/exchanges/exx/market.rb
+++ lib/cryptoexchange/exchanges/exx/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'exx'
       API_URL = 'https://api.exx.com/data/v1'
+
+      def self.trade_page_url(args={})
+        "https://www.exx.com/tradePros/#{args[:base]}_#{args[:target]}"
+      end
     end
   end
 end
