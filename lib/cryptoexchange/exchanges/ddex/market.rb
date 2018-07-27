842d4fcb7d4afee2a27b8988af0d0d0b437aee3d
Esther Leong Yuet Mei <estherleongym@gmail.com>
2018-07-27 15:31:57 +0800

    Added trading URL for DDEX

--- lib/cryptoexchange/exchanges/ddex/market.rb
+++ lib/cryptoexchange/exchanges/ddex/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'ddex'
       API_URL = 'https://api.ddex.io/v2'
+
+      def self.trade_page_url(args={})
+        "https://ddex.io/trade/#{args[:base]}-#{args[:target]}"
+      end
     end
   end
 end
