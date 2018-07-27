c46f6bbffa87b80949c20af147804486cba28e5a
Esther Leong Yuet Mei <estherleongym@gmail.com>
2018-07-27 17:23:31 +0800

    Added trading URL for CRXZone

--- lib/cryptoexchange/exchanges/crxzone/market.rb
+++ lib/cryptoexchange/exchanges/crxzone/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'crxzone'
       API_URL = 'https://www.crxzone.com/API'
+
+      def self.trade_page_url(args={})
+        "https://www.crxzone.com/services/trade/#{args[:base]}-#{args[:target]}"
+      end
     end
   end
 end
