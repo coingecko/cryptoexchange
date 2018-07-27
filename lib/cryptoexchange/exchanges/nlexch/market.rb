de59d2fad718ae3248c43f6843d70d48fe638c3a
Esther Leong Yuet Mei <estherleongym@gmail.com>
2018-07-27 17:25:45 +0800

    Added trading URL for Nlexch

--- lib/cryptoexchange/exchanges/nlexch/market.rb
+++ lib/cryptoexchange/exchanges/nlexch/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'nlexch'
       API_URL = 'https://www.nlexch.com/api/v2'
+
+      def self.trade_page_url(args={})
+        "https://www.nlexch.com/markets/#{args[:base]}#{args[:target]}"
+      end
     end
   end
 end
