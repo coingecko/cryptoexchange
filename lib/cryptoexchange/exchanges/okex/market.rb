03df6f58f474d65d4eccd0ab711713d5f6cb90ef
Esther Leong Yuet Mei <estherleongym@gmail.com>
2018-07-30 00:10:09 +0800

    Added trading URL for Okex

--- lib/cryptoexchange/exchanges/okex/market.rb
+++ lib/cryptoexchange/exchanges/okex/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'okex'
       API_URL = 'https://www.okex.com/api/v1'
+
+      def self.trade_page_url(args={})
+        "https://www.okex.com/market?product=#{args[:base].downcase}_#{args[:target].downcase}"
+      end
     end
   end
 end
