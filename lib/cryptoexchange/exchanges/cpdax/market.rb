3ec7ac644d4379a98daa84da8ff29689d893d467
Sam Lin <greenlin111@gmail.com>
2018-07-30 23:54:12 +0800

    update cpdax endpoint

--- lib/cryptoexchange/exchanges/cpdax/market.rb
+++ lib/cryptoexchange/exchanges/cpdax/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Cpdax
     class Market < Cryptoexchange::Models::Market
       NAME = 'cpdax'
-      API_URL = 'https://api.cpdax.com/api/v1'
+      API_URL = 'https://api.cpdax.com/v1'
     end
   end
 end
--- spec/exchanges/cpdax/market_spec.rb
+++ spec/exchanges/cpdax/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Cpdax::Market do
   it { expect(described_class::NAME).to eq 'cpdax' }
-  it { expect(described_class::API_URL).to eq 'https://api.cpdax.com/api/v1' }
+  it { expect(described_class::API_URL).to eq 'https://api.cpdax.com/v1' }
 end
