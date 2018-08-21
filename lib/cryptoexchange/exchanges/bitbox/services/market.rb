3bb498793f647adb1e9f31b7c3dcbf884375c52e
TM Lee <tmlee.ltm@gmail.com>
2018-08-22 00:09:21 +0800

    Improve granularity

--- lib/cryptoexchange/exchanges/bitbox/services/market.rb
+++ lib/cryptoexchange/exchanges/bitbox/services/market.rb
@@ -13,11 +13,21 @@ module Cryptoexchange::Exchanges
           adapt(output['responseData'], market_pair)
         end
 
+        def volume_ticker_url(market_pair)
+          "#{Cryptoexchange::Exchanges::Bitbox::Market::API_URL}/v1/outpost/ohlc?currencyPairs=#{market_pair.target}.#{market_pair.base}&timeFrame=1800&limit=100"
+        end
+
         def ticker_url(market_pair)
-          "#{Cryptoexchange::Exchanges::Bitbox::Market::API_URL}/v1/outpost/ohlc?currencyPairs=#{market_pair.target}.#{market_pair.base}&timeFrame=1800&limit=2"
+          "#{Cryptoexchange::Exchanges::Bitbox::Market::API_URL}/v1/outpost/ohlc?currencyPairs=#{market_pair.target}.#{market_pair.base}&timeFrame=60&limit=1"
         end
 
         def adapt(output, market_pair)
+          volume_resp = HTTP.timeout(:write => 2, :connect => 15, :read => 18).follow.get(volume_ticker_url(market_pair))
+          volume_output = JSON.parse volume_resp.body
+          total_volume = volume_output['responseData'].values[0].select do |s| 
+            s["closeTime"]/1000 > (Time.now.to_i - (60 * 60 * 24))
+          end.inject(0) { |sum, s| sum + (s["volume"] * s["close"]) }
+
           output = output.values[0][0]
           ticker = Cryptoexchange::Models::Ticker.new
           ticker.base = market_pair.base
@@ -26,8 +36,8 @@ module Cryptoexchange::Exchanges
           ticker.last = NumericHelper.to_d(output['close'])
           ticker.high = NumericHelper.to_d(output['high'])
           ticker.low = NumericHelper.to_d(output['low'])
-          ticker.volume = NumericHelper.to_d(output['volume'])
-          ticker.timestamp = (output['closeTime']/1000)
+          ticker.volume = total_volume
+          ticker.timestamp = nil
           ticker.payload = output
           ticker
         end
