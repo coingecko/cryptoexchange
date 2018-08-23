14162e2da8c7eb4b9047d2ed9ffbdbcddff712c6
Jack Wong <jackyhwong91@gmail.com>
2018-08-23 16:50:32 +0800

    freeze constants

--- lib/cryptoexchange/exchanges/stellar_term/market.rb
+++ lib/cryptoexchange/exchanges/stellar_term/market.rb
@@ -1,8 +1,8 @@
 module Cryptoexchange::Exchanges
   module StellarTerm
     class Market < Cryptoexchange::Models::Market
-      NAME = 'stellar_term'
-      API_URL = "https://api.stellarterm.com/v1"
+      NAME = "stellar_term".freeze
+      API_URL = "https://api.stellarterm.com/v1".freeze
 
       def self.trade_page_url
         nil
