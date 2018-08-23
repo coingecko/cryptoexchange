e2cbab52b41eb8f3af426f72c7e621a62bdc5bfd
Esther Leong <estherleongym@gmail.com>
2018-08-23 12:53:29 +0100

    Updated API endpoint for Pairs, Tickers, and removed OrderBook

--- lib/cryptoexchange/exchanges/b2bx/services/pairs.rb
+++ lib/cryptoexchange/exchanges/b2bx/services/pairs.rb
@@ -2,18 +2,20 @@ module Cryptoexchange::Exchanges
   module B2bx
     module Services
       class Pairs < Cryptoexchange::Services::Pairs
-        PAIRS_URL = "#{Cryptoexchange::Exchanges::B2bx::Market::API_URL}/public/symbol"
+        PAIRS_URL = "#{Cryptoexchange::Exchanges::B2bx::Market::API_URL}"
 
         def fetch
           market_pairs = []
           raw_output = HTTP.use(:auto_inflate).headers("Accept-Encoding" => "gzip").get(PAIRS_URL)
           output = JSON.parse(raw_output)
           output.each do |pair|
-            market_pairs << Cryptoexchange::Models::MarketPair.new(
-                              base: pair['MarginCurrency'],
-                              target: pair['ProfitCurrency'],
-                              market: B2bx::Market::NAME
-                            )
+            symbol = pair['Instrument']
+            base, target = symbol.split(/(BTC$)+|(ETH$)+(.*)|(USDT$)+(.*)|(USD$)+(.*)|(BCH$)+(.*)|(DASH$)+(.*)|(XRP$)+(.*)|(OMG$)+(.*)|(XMR$)+(.*)|(LTC$)+(.*)/)
+              market_pairs << Cryptoexchange::Models::MarketPair.new(
+                base:   base,
+                target: target,
+                market: B2bx::Market::NAME
+            )
           end
           market_pairs
         end
--- spec/exchanges/b2bx/integration/market_spec.rb
+++ spec/exchanges/b2bx/integration/market_spec.rb
@@ -27,24 +27,23 @@ RSpec.describe 'B2bx integration specs' do
     expect(ticker.timestamp).to be nil
     expect(ticker.payload).to_not be nil
   end
-
-  it 'fetch order book' do
-    pending 'fetch order book disabled due to timestamp in curl'
-    order_book = client.order_book(eth_btc_pair)
-
-    expect(order_book.base).to eq 'ETH'
-    expect(order_book.target).to eq 'BTC'
-    expect(order_book.market).to eq 'b2bx'
-    expect(order_book.asks).to_not be_empty
-    expect(order_book.bids).to_not be_empty
-    expect(order_book.asks.first.price).to_not be_nil
-    expect(order_book.bids.first.amount).to_not be_nil
-    expect(order_book.bids.first.timestamp).to be_nil
-    expect(order_book.asks.count).to be > 10
-    expect(order_book.bids.count).to be > 10
-    expect(order_book.timestamp).to be_a Numeric
-    expect(order_book.payload).to_not be nil
-  end
+  #
+  # it 'fetch order book' do
+  #   order_book = client.order_book(eth_btc_pair)
+  #
+  #   expect(order_book.base).to eq 'ETH'
+  #   expect(order_book.target).to eq 'BTC'
+  #   expect(order_book.market).to eq 'b2bx'
+  #   expect(order_book.asks).to_not be_empty
+  #   expect(order_book.bids).to_not be_empty
+  #   expect(order_book.asks.first.price).to_not be_nil
+  #   expect(order_book.bids.first.amount).to_not be_nil
+  #   expect(order_book.bids.first.timestamp).to be_nil
+  #   expect(order_book.asks.count).to be > 10
+  #   expect(order_book.bids.count).to be > 10
+  #   expect(order_book.timestamp).to be_a Numeric
+  #   expect(order_book.payload).to_not be nil
+  # end
 
   # it 'fetch trade' do
   #   trades = client.trades(eth_btc_pair)
