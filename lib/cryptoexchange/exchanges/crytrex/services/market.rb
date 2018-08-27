d6732852560b0c958e668cdd73ab0ed7ad45870e
Esther Leong <estherleongym@gmail.com>
2018-08-27 12:58:26 +0100

    Set timestamp to nil in Tickers

--- lib/cryptoexchange/exchanges/crytrex/services/market.rb
+++ lib/cryptoexchange/exchanges/crytrex/services/market.rb
@@ -27,7 +27,7 @@ module Cryptoexchange::Exchanges
           ticker.ask       = NumericHelper.to_d(output['ask'])
           ticker.bid       = NumericHelper.to_d(output['bid'])
           ticker.volume    = NumericHelper.to_d(output['24h_volume'])
-          ticker.timestamp = Time.now.to_i
+          ticker.timestamp = nil
           ticker.payload   = output
           ticker
         end
