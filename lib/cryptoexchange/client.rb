76a1d82f9128fee9a1a03296187b7c2219edabfc
Jack Wong <jackyhwong91@gmail.com>
2018-08-29 16:49:47 +0800

    fix missing conditional matcher.

--- lib/cryptoexchange/client.rb
+++ lib/cryptoexchange/client.rb
@@ -30,7 +30,7 @@ module Cryptoexchange
 
         if !market_pair.base_raw.nil? && market_pair.base_raw.casecmp(market_pair.base) != 0
           tickers.find do |t|
-            t.base_raw.casecmp(market_pair.base_raw) &&
+            t.base_raw.casecmp(market_pair.base_raw) == 0 &&
               t.base.casecmp(market_pair.base) == 0 &&
               t.target.casecmp(market_pair.target) == 0
           end
