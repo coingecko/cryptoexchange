f4f3a123fcbb91eba020fed31eeb17f00a14f6e3
Jack Wong <jackyhwong91@gmail.com>
2018-08-30 12:44:10 +0800

    Revert ticker base raw (#890)
    * Revert "checks for string .empty? (#888)"
    
    This reverts commit 039f8588d1c851ef7c1db865df5003d0db06e94b.
    
    * Revert "do not return base_raw and target_raw if it's the same. (#887)"
    
    This reverts commit 213323c8480902f661a84cd283007ee8c63dd641.
    
    * Revert "fix missing conditional matcher. (#886)"
    
    This reverts commit 578f0d53ae3e1713a82ad6f04c581e9a2078f41d.
    
    * Revert "update ticker to support & return base_raw and target_raw (optional) (#882)"
    
    This reverts commit c8704b2d3ea8f37caf84fcd38a26431df55a1c82.

--- lib/cryptoexchange/exchanges/stellar_term/services/pairs.rb
+++ lib/cryptoexchange/exchanges/stellar_term/services/pairs.rb
@@ -14,12 +14,10 @@ module Cryptoexchange::Exchanges
           output["assets"].map do |asset|
             next if asset["id"] == "XLM-native"
 
-            base_raw = asset["id"]
-            base = asset["code"]
+            base = asset["id"]
             target = "XLM"
 
             Cryptoexchange::Models::MarketPair.new(
-              base_raw: base_raw,
               base: base,
               target: target,
               market: StellarTerm::Market::NAME
--- spec/exchanges/stellar_term/integration/market_spec.rb
+++ spec/exchanges/stellar_term/integration/market_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe "StellarTerm integration specs" do
   let(:client) { Cryptoexchange::Client.new }
   let(:rmt_xlm_pair) do
     Cryptoexchange::Models::MarketPair.new(
-      base: "RMT",
-      base_raw: "RMT-GDEGOXPCHXWFYY234D2YZSPEJ24BX42ESJNVHY5H7TWWQSYRN5ZKZE3N",
+      base: "RMT-GDEGOXPCHXWFYY234D2YZSPEJ24BX42ESJNVHY5H7TWWQSYRN5ZKZE3N",
       target: "XLM",
       market: Cryptoexchange::Exchanges::StellarTerm::Market::NAME
     )
@@ -16,22 +15,20 @@ RSpec.describe "StellarTerm integration specs" do
     expect(pairs).not_to be_empty
 
     pair = pairs.first
-    expect(pair.base_raw).not_to be nil
-    expect(pair.base).not_to be nil
-    expect(pair.target).not_to be nil
+    expect(pair.base).to_not be nil
+    expect(pair.target).to_not be nil
     expect(pair.market).to eq Cryptoexchange::Exchanges::StellarTerm::Market::NAME
   end
 
   it "fetch ticker" do
     ticker = client.ticker(rmt_xlm_pair)
 
-    expect(ticker.base).to eq "RMT"
-    expect(ticker.base_raw).to eq "RMT-GDEGOXPCHXWFYY234D2YZSPEJ24BX42ESJNVHY5H7TWWQSYRN5ZKZE3N"
+    expect(ticker.base).to eq "RMT-GDEGOXPCHXWFYY234D2YZSPEJ24BX42ESJNVHY5H7TWWQSYRN5ZKZE3N"
     expect(ticker.target).to eq "XLM"
     expect(ticker.market).to eq Cryptoexchange::Exchanges::StellarTerm::Market::NAME
     expect(ticker.last).to be_a Numeric
     expect(ticker.volume).to be_a Numeric
     expect(ticker.timestamp).to be nil
-    expect(ticker.payload).not_to be nil
+    expect(ticker.payload).to_not be nil
   end
 end
