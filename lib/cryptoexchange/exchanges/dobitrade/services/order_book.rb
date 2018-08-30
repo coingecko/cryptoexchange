2c440ee18173e500f23aaecf27c6d1f05e784916
Sky Chin <sky4just@gmail.com>
2018-08-31 07:29:25 +0800

    Implement DOBI Trade Ticker, Orderbook, Trades #750

---
+++ lib/cryptoexchange/exchanges/dobitrade/services/order_book.rb
@@ -0,0 +1,43 @@
+module Cryptoexchange::Exchanges
+  module Dobitrade
+    module Services
+      class OrderBook < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            true
+          end
+        end
+
+        def fetch(market_pair)
+          output = super(ticker_url(market_pair))
+          adapt(output['data'], market_pair)
+        end
+
+        def ticker_url(market_pair)
+          "#{Cryptoexchange::Exchanges::Dobitrade::Market::API_URL}/market/orderbook?market=#{market_pair.base.downcase}_#{market_pair.target.downcase}&limit=200"
+        end
+
+        def adapt(output, market_pair)
+          order_book = Cryptoexchange::Models::OrderBook.new
+          order_book.base      = market_pair.base
+          order_book.target    = market_pair.target
+          order_book.market    = Dobitrade::Market::NAME
+          order_book.asks      = adapt_orders output['asks']
+          order_book.bids      = adapt_orders output['bids']
+          order_book.timestamp = Time.now.to_i
+          order_book.payload   = output
+          order_book
+        end
+
+        def adapt_orders(orders)
+          orders.collect do |order_entry|
+            price = order_entry['p']
+            amount = order_entry['n']
+            Cryptoexchange::Models::Order.new(price: price,
+                                              amount: amount)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/exchanges/dobitrade/market_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+RSpec.describe Cryptoexchange::Exchanges::Dobitrade::Market do
+  it { expect(described_class::NAME).to eq 'dobitrade' }
+  it { expect(described_class::API_URL).to eq 'https://api.dobitrade.com' }
+end
