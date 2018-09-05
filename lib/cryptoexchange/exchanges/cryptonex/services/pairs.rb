5dd69d7ee0b1fa5a88e343df5f8227f26ec4c3fa
Sky Chin <sky4just@gmail.com>
2018-09-06 06:48:13 +0800

    Implement Cryptonex Ticker, Orderbook, Trades #756

--- lib/cryptoexchange/exchanges/cryptonex/services/pairs.rb
+++ lib/cryptoexchange/exchanges/cryptonex/services/pairs.rb
@@ -3,12 +3,12 @@ module Cryptoexchange::Exchanges
     module Services
       class Pairs < Cryptoexchange::Services::Pairs
         PAIRS_URL           = "#{Cryptoexchange::Exchanges::Cryptonex::Market::API_URL}"
-        HTTP_METHOD         = 'POST'
-        POST_PARAMS         = { "id": 2, "jsonrpc": "2.0", "method": "currency_pair.get_rate_list" }
+        POST_PARAMS         = "{\n  \"id\": 2,\n  \"jsonrpc\": \"2.0\",\n  \"method\": \"currency_pair.get_rate_list\"\n}"
 
         def fetch
-          output = super
-          puts output
+          # byebug
+          puts raw_output = HTTP.post(PAIRS_URL, :body => POST_PARAMS)
+          output = JSON.parse(raw_output)
           adapt(output['result']['rates'])
         end
 
