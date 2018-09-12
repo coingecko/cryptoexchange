72db68ba9a69636b002029ca96d50c5ed393cdbf
Esther Leong <estherleongym@gmail.com>
2018-09-12 22:06:39 +0100

    Updated Allcoin API endpoint and README & added Pairs and Trades

--- lib/cryptoexchange/exchanges/allcoin/services/market.rb
+++ lib/cryptoexchange/exchanges/allcoin/services/market.rb
@@ -10,28 +10,25 @@ module Cryptoexchange::Exchanges
 
         def fetch(market_pair)
           output = super(ticker_url(market_pair))
-          adapt(output, market_pair)
+          adapt(output['data'], market_pair)
         end
 
         def ticker_url(market_pair)
-          "#{Cryptoexchange::Exchanges::Allcoin::Market::API_URL}/ticker?symbol=#{market_pair.base.downcase}_#{market_pair.target.downcase}"
+          "#{Cryptoexchange::Exchanges::Allcoin::Market::API_URL}/Api_Order/ticker?symbol=#{market_pair.base.downcase}2#{market_pair.target.downcase}"
         end
 
         def adapt(output, market_pair)
           ticker = Cryptoexchange::Models::Ticker.new
-          base = market_pair.base
-          target = market_pair.target
-
-          ticker.base      = base
-          ticker.target    = target
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
           ticker.market    = Allcoin::Market::NAME
-          ticker.ask       = NumericHelper.to_d(output['ticker']['sell'])
-          ticker.bid       = NumericHelper.to_d(output['ticker']['buy'])
-          ticker.last      = NumericHelper.to_d(output['ticker']['last'])
-          ticker.high      = NumericHelper.to_d(output['ticker']['high'])
-          ticker.low       = NumericHelper.to_d(output['ticker']['low'])
-          ticker.volume    = NumericHelper.to_d(output['ticker']['vol'])
-          ticker.timestamp = output['date'].to_i
+          ticker.ask       = NumericHelper.to_d(output['sell'].to_f)
+          ticker.bid       = NumericHelper.to_d(output['buy'].to_f)
+          ticker.last      = NumericHelper.to_d(output['last'].to_f)
+          ticker.high      = NumericHelper.to_d(output['high'].to_f)
+          ticker.low       = NumericHelper.to_d(output['low'].to_f)
+          ticker.volume    = NumericHelper.to_d(output['vol'].to_f)
+          ticker.timestamp = nil
           ticker.payload   = output
           ticker
         end
