1b78699b50d4431c0b1560d0650733c368913e93
Jack Wong <jackyhwong91@gmail.com>
2018-09-18 14:04:06 +0800

    Fix therocktrading volume

--- spec/exchanges/therocktrading/integration/market_spec.rb
+++ spec/exchanges/therocktrading/integration/market_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Therocktrading integration specs' do
   let(:pairs) { client.pairs('therocktrading') }
   let(:pair) { pairs.first }
   let(:btc_eur_pair) { Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'EUR', market: 'therocktrading') }
-  let(:usd_xrp_pair) { Cryptoexchange::Models::MarketPair.new(base: 'USD', target: 'XRP', market: 'therocktrading') }
+  let(:usd_xrp_pair) { Cryptoexchange::Models::MarketPair.new(base: 'EUR', target: 'XRP', market: 'therocktrading') }
 
   it 'fetch pairs' do
     expect(pairs).not_to be_empty
@@ -38,7 +38,7 @@ RSpec.describe 'Therocktrading integration specs' do
   it 'fetch ticker (XRP is not treated as Base)' do
     ticker = client.ticker(usd_xrp_pair)
 
-    expect(ticker.base).to eq 'USD'
+    expect(ticker.base).to eq 'EUR'
     expect(ticker.target).to eq 'XRP'
     expect(ticker.market).to eq 'therocktrading'
     expect(ticker.last).to be_a Numeric
@@ -47,7 +47,7 @@ RSpec.describe 'Therocktrading integration specs' do
     expect(ticker.high).to be_a Numeric
     expect(ticker.low).to be_a Numeric
     expect(ticker.volume).to be_a Numeric
-    
+
     expect(ticker.payload).to_not be nil
   end
 
