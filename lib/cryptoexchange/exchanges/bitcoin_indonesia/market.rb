ae2ff7b7aba572c4ad2aa7825f789fe5d64d0881
Yih Yang <yyngo788n88@hotmail.com>
2018-10-03 00:01:59 +0800

    added trade_page_url for bitcoin_indonesia

--- lib/cryptoexchange/exchanges/bitcoin_indonesia/market.rb
+++ lib/cryptoexchange/exchanges/bitcoin_indonesia/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'bitcoin_indonesia'
       API_URL = 'https://vip.bitcoin.co.id/api'
+
+      def self.trade_page_url(args={})
+        "https://indodax.com/market/#{args[:base].upcase}#{args[:target].upcase}"
+      end
     end
   end
 end
--- spec/exchanges/bitcoin_indonesia/market_spec.rb
+++ spec/exchanges/bitcoin_indonesia/market_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::BitcoinIndonesia::Market do
+  let(:client) { Cryptoexchange::Client.new }
+  let(:market) { 'bitcoin_indonesia' }
+  let(:ltc_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'ltc', target: 'btc', market: market) }
+
   it { expect(described_class::NAME).to eq 'bitcoin_indonesia' }
   it { expect(described_class::API_URL).to eq 'https://vip.bitcoin.co.id/api' }
+
+  it 'give trade url' do
+    trade_page_url = client.trade_page_url market, base: ltc_btc_pair.base, target: ltc_btc_pair.target
+    expect(trade_page_url).to eq 'https://indodax.com/market/LTCBTC'
+  end
 end
