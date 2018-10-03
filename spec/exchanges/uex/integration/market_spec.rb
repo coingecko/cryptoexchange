0540fce15c6a14146edd0b69e2695860cb63596b
Yih Yang <yyngo788n88@hotmail.com>
2018-10-04 00:49:10 +0800

    added integration specs for Uex and recorded VCR Cassettes

---
+++ spec/exchanges/uex/integration/market_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+RSpec.describe 'Uex integration specs' do
+  let(:client) { Cryptoexchange::Client.new }
+  let(:eth_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'ETH', target: 'BTC', market: 'uex') }
+
+  it 'fetch pairs' do
+    pairs = client.pairs('uex')
+    expect(pairs).not_to be_empty
+
+    pair = pairs.first
+    expect(pair.base).to_not be_nil
+    expect(pair.target).to_not be_nil
+    expect(pair.market).to eq 'uex'
+  end
+
+  it 'fetch ticker' do
+    ticker = client.ticker(eth_btc_pair)
+
+    expect(ticker.base).to eq 'ETH'
+    expect(ticker.target).to eq 'BTC'
+    expect(ticker.market).to eq 'uex'
+    expect(ticker.last).to be_a Numeric
+    expect(ticker.bid).to be_a Numeric
+    expect(ticker.ask).to be_a Numeric
+    expect(ticker.high).to be_a Numeric
+    expect(ticker.volume).to be_a Numeric
+    expect(ticker.timestamp).to be_a Numeric
+    expect(ticker.payload).to_not be nil
+  end
+end
