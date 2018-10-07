c048ea1735454fed91cea62f146bd8b0a32e778c
Sam Lin <greenlin111@gmail.com>
2018-10-07 11:21:34 +0800

    ✨ implement Coinpark ticker

---
+++ lib/cryptoexchange/exchanges/coinpark/services/market.rb
@@ -0,0 +1,42 @@
+module Cryptoexchange::Exchanges
+  module Coinpark
+    module Services
+      class Market < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            true
+          end
+        end
+
+        def fetch(market_pair)
+          output = super(ticker_url(market_pair))
+          adapt(output['result'], market_pair)
+        end
+
+        def ticker_url(market_pair)
+          base   = market_pair.base
+          target = market_pair.target
+          "#{Cryptoexchange::Exchanges::Coinpark::Market::API_URL}/mdata?cmd=ticker&pair=#{base}_#{target}"
+        end
+
+        def adapt(output, market_pair)
+          ticker           = Cryptoexchange::Models::Ticker.new
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
+          ticker.market    = Coinpark::Market::NAME
+          ticker.ask       = NumericHelper.to_d(output['sell'])
+          ticker.bid       = NumericHelper.to_d(output['buy'])
+          ticker.last      = NumericHelper.to_d(output['last'])
+          ticker.high      = NumericHelper.to_d(output['high'])
+          ticker.low       = NumericHelper.to_d(output['low'])
+          ticker.volume    = NumericHelper.to_d(output['vol'])
+          ticker.change    = NumericHelper.to_d(output['percent'].gsub(/[+-]|(%)/, ''))
+          ticker.timestamp = output['timestamp'] / 1000
+          ticker.payload   = output
+          ticker
+        end
+
+      end
+    end
+  end
+end
