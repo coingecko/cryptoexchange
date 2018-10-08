c81da06904c02bde7b90a9c6ec49274dc89485ed
Pedro Guerrato <pedro.guerrato@epv.network>
2018-10-08 09:26:26 -0300

    Removed Xbit exchange

--- lib/cryptoexchange/exchanges/xbit/market.rb
+++
@@ -1,8 +0,0 @@
-module Cryptoexchange::Exchanges
-  module Xbit
-    class Market < Cryptoexchange::Models::Market
-      NAME = 'xbit'
-      API_URL = 'https://xbit.com.ec/api'
-    end
-  end
-end
--- spec/exchanges/xbit/market_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe Cryptoexchange::Exchanges::Xbit::Market do
-  it { expect(Cryptoexchange::Exchanges::Xbit::Market::NAME).to eq 'xbit' }
-  it { expect(Cryptoexchange::Exchanges::Xbit::Market::API_URL).to eq 'https://xbit.com.ec/api' }
-end
