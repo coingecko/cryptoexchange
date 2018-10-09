b352d8e98d3c093f3ea6bb146fe60de4e8ffa6f0
Matt Kimmel <matt@infinite-jest.net>
2018-10-09 17:49:51 -0400

    Add qTrade integration

---
+++ lib/cryptoexchange/exchanges/qtrade/services/pairs.rb
@@ -0,0 +1,29 @@
+module Cryptoexchange::Exchanges
+  module Qtrade
+    module Services
+      class Pairs < Cryptoexchange::Services::Pairs
+        PAIRS_URL = "#{Cryptoexchange::Exchanges::Qtrade::Market::API_URL}/markets"
+
+        def fetch
+          output = super
+          adapt(output)
+        end
+
+        def adapt(output)
+          pairs = output['data']['markets']
+          market_pairs = []
+          pairs.each do |pair|
+            base = pair['market_currency']
+            target = pair['base_currency'] # This is correct, but confusing
+            market_pair = Cryptoexchange::Models::MarketPair.new
+            market_pair.base = base
+            market_pair.target = target
+            market_pair.market = Qtrade::Market::NAME
+            market_pairs << market_pair
+          end
+          pp market_pairs
+        end
+      end
+    end
+  end
+end
