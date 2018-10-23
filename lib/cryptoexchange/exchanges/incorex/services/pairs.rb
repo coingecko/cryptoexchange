22b312c777e0e0adca9b919b26803b722bb9e615
Esther <30499981+estherleongym@users.noreply.github.com>
2018-10-23 15:55:27 +0100

    Minor Adjustments to IncoreX Integration (#1094)
    * added incorex api
    
    * added incorex api
    
    * Added specs and made minor adjustments

---
+++ lib/cryptoexchange/exchanges/incorex/services/pairs.rb
@@ -0,0 +1,28 @@
+module Cryptoexchange::Exchanges
+  module Incorex
+    module Services
+      class Pairs < Cryptoexchange::Services::Pairs
+        PAIRS_URL = "#{Cryptoexchange::Exchanges::Incorex::Market::API_URL}/pair_settings"
+
+        def fetch
+          output = super
+          adapt(output)
+        end
+
+        def adapt(output)
+          pairs = output.keys
+          market_pairs = []
+          pairs.each do |pair|
+            base, target = pair.split('_')
+            market_pairs << Cryptoexchange::Models::MarketPair.new(
+                              base: base,
+                              target: target,
+                              market: Incorex::Market::NAME
+                            )
+          end
+          market_pairs
+        end
+      end
+    end
+  end
+end
