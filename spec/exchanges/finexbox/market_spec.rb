e1a9bcd6a2192326425c06c02484b025883b33dc
finexbox <44494099+finexbit@users.noreply.github.com>
2018-10-30 16:39:51 +0800

    Modify the trade history

--- spec/exchanges/finexbox/market_spec.rb
+++ spec/exchanges/finexbox/market_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe Cryptoexchange::Exchanges::Bitebtc::Market do
+RSpec.describe Cryptoexchange::Exchanges::Finexbox::Market do
   it { expect(described_class::NAME).to eq 'finexbox' }
   it { expect(described_class::API_URL).to eq 'https://xapi.finexbox.com/v1' }
 end
