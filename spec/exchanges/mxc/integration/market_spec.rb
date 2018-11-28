19e2989c9cd3a2e0e017426c80d4f6f5bdb20df8
Sam Lin <greenlin111@gmail.com>
2018-11-28 15:57:00 +0800

    ✅ add test case and update readme

---
+++ spec/exchanges/mxc/integration/market_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+RSpec.describe 'Mxc integration specs' do
+  let(:client) { Cryptoexchange::Client.new }
+  let(:btc_usdt_pair) { Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'USDT', market: 'mxc') }
+
+  it 'fetch pairs' do
+    pairs = client.pairs('mxc')
+    expect(pairs).not_to be_empty
+
+    pair = pairs.first
+    expect(pair.base).to_not be nil
+    expect(pair.target).to_not be nil
+    expect(pair.market).to eq 'mxc'
+  end
+
+  it 'give trade url' do
+    trade_page_url = client.trade_page_url 'mxc', base: btc_usdt_pair.base, target: btc_usdt_pair.target
+    expect(trade_page_url).to eq 'https://www.mxc.com/trade.html?symbol=BTC_USDT'
+  end
+
+  it 'fetch ticker' do
+    ticker = client.ticker(btc_usdt_pair)
+
+    expect(ticker.base).to eq 'BTC'
+    expect(ticker.target).to eq 'USDT'
+    expect(ticker.market).to eq 'mxc'
+    expect(ticker.last).to be_a Numeric
+    expect(ticker.bid).to be_a Numeric
+    expect(ticker.ask).to be_a Numeric
+    expect(ticker.low).to be_a Numeric
+    expect(ticker.high).to be_a Numeric
+    expect(ticker.volume).to be_a Numeric
+    expect(ticker.timestamp).to be nil
+    expect(ticker.payload).to_not be nil
+  end
+
+  it 'fetch order book' do
+    order_book = client.order_book(btc_usdt_pair)
+
+    expect(order_book.base).to eq 'BTC'
+    expect(order_book.target).to eq 'USDT'
+    expect(order_book.market).to eq 'mxc'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.count).to be > 0
+    expect(order_book.bids.count).to be > 0
+    expect(order_book.timestamp).to be nil
+    expect(order_book.payload).to_not be nil
+  end
+end
