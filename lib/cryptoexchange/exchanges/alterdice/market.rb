2ca895e5ddade26563c76a468d44bb4a9d78e471
Alterdice <43209450+Alterdice@users.noreply.github.com>
2018-12-05 11:11:40 +0200

    alterdice implement (#1161)

---
+++ lib/cryptoexchange/exchanges/alterdice/market.rb
@@ -0,0 +1,8 @@
+module Cryptoexchange::Exchanges
+  module Alterdice
+    class Market < Cryptoexchange::Models::Market
+      NAME = 'Alterdice'
+      API_URL = 'https://api.alterdice.com/v1'
+    end
+  end
+end
