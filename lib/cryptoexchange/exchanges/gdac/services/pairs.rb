43a5df41d5dc631f75bbbfe91db8cb647caf7b3b
Jack Wong <jackyhwong91@gmail.com>
2018-12-06 18:37:21 +0800

    fix GDAC inverted base, target. (#1208)

--- lib/cryptoexchange/exchanges/gdac/services/pairs.rb
+++ lib/cryptoexchange/exchanges/gdac/services/pairs.rb
@@ -5,13 +5,17 @@ module Cryptoexchange::Exchanges
         PAIRS_URL = "#{Cryptoexchange::Exchanges::Gdac::Market::API_URL}/public/marketsummaries"
 
         def fetch
-          output = super()
+          ctx = OpenSSL::SSL::SSLContext.new
+          ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          result = HTTP.get(PAIRS_URL, ssl_context: ctx)
+          output = JSON.parse(result)
+
           market_pairs = []
           pairs = output['result'].map { |r| r["marketName"].split("-") }
           pairs.each do |pair|
             market_pairs << Cryptoexchange::Models::MarketPair.new(
-                              base: pair.first,
-                              target: pair.last,
+                              base: pair.last,
+                              target: pair.first,
                               market: Gdac::Market::NAME
                             )
           end
