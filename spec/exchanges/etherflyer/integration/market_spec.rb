0816da36abad6e86bf3e7c0469589405c1e6653b
Swingcloud <sam@swingcloud.app>
2018-12-28 11:01:34 +0800

    ✅ update test case and readme

--- spec/exchanges/etherflyer/integration/market_spec.rb
+++ spec/exchanges/etherflyer/integration/market_spec.rb
@@ -2,20 +2,20 @@ require 'spec_helper'
 
 RSpec.describe 'Etherflyer integration specs' do
   let(:client) { Cryptoexchange::Client.new }
-  let(:tcash_eth_pair) { Cryptoexchange::Models::MarketPair.new(base: 'TCASH', target: 'ETH', market: 'Etherflyer') }
+  let(:tcash_eth_pair) { Cryptoexchange::Models::MarketPair.new(base: 'TCASH', target: 'ETH', market: 'etherflyer') }
 
   it 'fetch pairs' do
-    pairs = client.pairs('Etherflyer')
+    pairs = client.pairs('etherflyer')
     expect(pairs).not_to be_empty
 
     pair = pairs.first
     expect(pair.base).to eq 'TCASH'
     expect(pair.target).to eq 'ETH'
-    expect(pair.market).to eq 'Etherflyer'
+    expect(pair.market).to eq 'etherflyer'
   end
 
   it 'give trade url' do
-    trade_page_url = client.trade_page_url 'Etherflyer', base: tcash_eth_pair.base, target: tcash_eth_pair.target
+    trade_page_url = client.trade_page_url 'etherflyer', base: tcash_eth_pair.base, target: tcash_eth_pair.target
     expect(trade_page_url).to eq "https://www.etherflyer.com/trade.html?pairs=TCASH-ETH"
   end
 
@@ -24,13 +24,14 @@ RSpec.describe 'Etherflyer integration specs' do
 
     expect(ticker.base).to eq 'TCASH'
     expect(ticker.target).to eq 'ETH'
-    expect(ticker.market).to eq 'Etherflyer'
+    expect(ticker.market).to eq 'etherflyer'
     expect(ticker.last).to be_a Numeric
     expect(ticker.change).to be_a Numeric
     expect(ticker.ask).to be_a Numeric
     expect(ticker.bid).to be_a Numeric
     expect(ticker.volume).to be_a Numeric
-    expect(ticker.timestamp).to be nil
+    expect(ticker.timestamp).to be_a Numeric
+    expect(2000..Date.today.year).to include(Time.at(ticker.timestamp).year)
     expect(ticker.payload).to_not be nil
   end
 end
