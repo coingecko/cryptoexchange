57c10f7579d56057c59de54101997b6364751de9
estherleongym <estherleongym@gmail.com>
2019-01-08 21:50:17 +0000

    Update Pairs, Tickers, and add OrderBook, Trades for P2PB2B

--- lib/cryptoexchange/exchanges/p2pb2b/market.rb
+++ lib/cryptoexchange/exchanges/p2pb2b/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module P2pb2b
     class Market
       NAME = 'p2pb2b'
-      API_URL = 'https://p2pb2b.io/api/v2'
+      API_URL = 'https://p2pb2b.io/api/v1/public'
     end
   end
 end
--- spec/exchanges/p2pb2b/market_spec.rb
+++ spec/exchanges/p2pb2b/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::P2pb2b::Market do
   it { expect(described_class::NAME).to eq 'p2pb2b' }
-  it { expect(described_class::API_URL).to eq 'https://p2pb2b.io/api/v2' }
+  it { expect(described_class::API_URL).to eq 'https://p2pb2b.io/api/v1/public' }
 end
