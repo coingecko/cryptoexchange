da2c629c3c3b124223fe27b178f5ecf3c724f4db
Haiming Nan <haimingnan16@outlook.com>
2019-01-15 02:02:35 +0900

    fixed pairs

--- lib/cryptoexchange/exchanges/tokenmom/services/pairs.rb
+++ lib/cryptoexchange/exchanges/tokenmom/services/pairs.rb
@@ -10,8 +10,8 @@ module Cryptoexchange::Exchanges
         end
 
         def adapt(output)
-          output['markets'].map do |mt|
-            mt['tokens'].map do |pair|
+          output['markets'].each do |mt|
+            mt['tokens'].each do |pair|
               base, target = pair['market_id'].split('-')
               Cryptoexchange::Models::MarketPair.new(
                 base:   base,
