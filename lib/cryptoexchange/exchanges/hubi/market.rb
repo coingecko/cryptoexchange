de0d4da37834350df7863e60b72b9dbe77d6243c
Wenjie Yek <dx3152@hotmail.com>
2019-01-14 14:43:44 +0800

    add trade_page_url

--- lib/cryptoexchange/exchanges/hubi/market.rb
+++ lib/cryptoexchange/exchanges/hubi/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'hubi'
       API_URL = 'https://www.hubi.com/api/v1'
+
+      def self.trade_page_url(args={})
+        "https://www.hubi.com/#/exchange/#{args[:base]}_#{args[:target]}"
+      end
     end
   end
 end
