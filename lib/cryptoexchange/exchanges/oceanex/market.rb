203b054882249d4e819a2c2281cb97a82716b656
Sam Lin <sam@swingcloud.app>
2019-01-22 16:24:33 +0800

    ✨ Implement Oceanex ticker, order book and trades  (#1304)
    * ✨ implement Oceanex pairs
    
    * ✨ implement Oceanex ticker
    
    * ✨ implement Oceanex order book
    
    * ✨ implement Oceanex trades
    
    * ✅ add test case and update readme

---
+++ lib/cryptoexchange/exchanges/oceanex/market.rb
@@ -0,0 +1,14 @@
+module Cryptoexchange::Exchanges
+  module Oceanex
+    class Market < Cryptoexchange::Models::Market
+      NAME    = 'oceanex'
+      API_URL = 'https://api.oceanex.pro/v1'
+
+      def self.trade_page_url(args = {})
+        base   = args[:base].downcase
+        target = args[:target].downcase
+        "https://oceanex.pro/trade/?market=#{base}#{target}"
+      end
+    end
+  end
+end
---
+++ lib/cryptoexchange/exchanges/oceanex/services/market.rb
@@ -0,0 +1,42 @@
+module Cryptoexchange::Exchanges
+  module Oceanex
+    module Services
+      class Market < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            true
+          end
+        end
+
+        def fetch(market_pair)
+          output = super(ticker_url(market_pair))
+          adapt(output, market_pair)
+        end
+
+        def ticker_url(market_pair)
+          base   = market_pair.base.downcase
+          target = market_pair.target.downcase
+          "#{Cryptoexchange::Exchanges::Oceanex::Market::API_URL}/tickers/#{base}#{target}"
+        end
+
+        def adapt(output, market_pair)
+          market = output["data"]['ticker']
+
+          ticker           = Cryptoexchange::Models::Ticker.new
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
+          ticker.market    = Oceanex::Market::NAME
+          ticker.last      = NumericHelper.to_d(market['last'])
+          ticker.bid       = NumericHelper.to_d(market['buy'])
+          ticker.ask       = NumericHelper.to_d(market['sell'])
+          ticker.high      = NumericHelper.to_d(market['high'])
+          ticker.low       = NumericHelper.to_d(market['low'])
+          ticker.volume    = NumericHelper.to_d(market['vol'])
+          ticker.timestamp = NumericHelper.to_d(output['data']['at'])
+          ticker.payload   = output
+          ticker
+        end
+      end
+    end
+  end
+end
