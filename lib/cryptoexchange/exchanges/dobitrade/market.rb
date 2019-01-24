2814ee132aaa3f098064b79520bd6e7bbe14bbb6
Wenjie Yek <dx3152@hotmail.com>
2019-01-24 14:35:27 +0800

    change dobitrade api link

--- lib/cryptoexchange/exchanges/dobitrade/market.rb
+++ lib/cryptoexchange/exchanges/dobitrade/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Dobitrade
     class Market < Cryptoexchange::Models::Market
       NAME = 'dobitrade'
-      API_URL = 'https://api.dobiex.io'
+      API_URL = 'https://api.dobiexchange.com'
 
       def self.trade_page_url(args={})
         "https://www.dobiex.io/zh/trade/#{args[:base].downcase}_#{args[:target].downcase}"
--- spec/exchanges/dobitrade/market_spec.rb
+++ spec/exchanges/dobitrade/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Dobitrade::Market do
   it { expect(described_class::NAME).to eq 'dobitrade' }
-  it { expect(described_class::API_URL).to eq 'https://api.dobiex.io' }
+  it { expect(described_class::API_URL).to eq 'https://api.dobiexchange.com' }
 end
