3518c73887ad2d80ca06aab447ab7f2c96609da1
Wenjie Yek <dx3152@hotmail.com>
2019-01-29 14:02:45 +0800

    edit zgtop endpoint to v2 (#1328)
    * edit zgtop endpoint to v2
    
    * edit zgtop endpoint to v2
    
    * update vcr cassettes
    
    * add trade_page_url

--- lib/cryptoexchange/exchanges/zgtop/market.rb
+++ lib/cryptoexchange/exchanges/zgtop/market.rb
@@ -2,7 +2,11 @@ module Cryptoexchange::Exchanges
   module Zgtop
     class Market < Cryptoexchange::Models::Market
       NAME = 'zgtop'
-      API_URL = 'https://www.zg.top/API/api/v1'
+      API_URL = 'https://www.zg.top/API/api/v2'
+
+      def self.trade_page_url(args={})
+        "https://www.zg.top"
+      end      
     end
   end
 end
