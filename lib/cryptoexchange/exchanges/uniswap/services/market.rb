c750d1067faad30cddaa462c56613e519dcc27e8
TM Lee <tmlee.ltm@gmail.com>
2019-02-03 10:55:57 +0800

    Fix invalid argument

--- lib/cryptoexchange/exchanges/uniswap/services/market.rb
+++ lib/cryptoexchange/exchanges/uniswap/services/market.rb
@@ -14,7 +14,6 @@ module Cryptoexchange::Exchanges
         end
 
         def ticker_url(market_pair)
-          exchangeAddress = market_pair.inst_id
           pairs = Cryptoexchange::Exchanges::Uniswap::Services::Pairs.new.fetch
           pair = pairs.select { |s| s.base == market_pair.base }.first
           exchangeAddress = pair.inst_id
@@ -26,10 +25,10 @@ module Cryptoexchange::Exchanges
           ticker.base      = market_pair.base
           ticker.target    = market_pair.target
           ticker.market    = Uniswap::Market::NAME
-          ticker.last      = NumericHelper.to_d(output['lastTradePrice'])
-          ticker.high      = NumericHelper.to_d(output['highPrice'])
-          ticker.low       = NumericHelper.to_d(output['lowPrice'])
-          ticker.volume    = (NumericHelper.to_d(output['tradeVolume']) / 10**18) * ticker.last
+          ticker.last      = NumericHelper.divide(1, NumericHelper.to_d(output['lastTradePrice']))
+          ticker.high      = NumericHelper.divide(1, NumericHelper.to_d(output['highPrice']))
+          ticker.low       = NumericHelper.divide(1, NumericHelper.to_d(output['lowPrice']))
+          ticker.volume    = (NumericHelper.to_d(output['tradeVolume']) / 10**18) / ticker.last
           ticker.timestamp = nil
           ticker.payload   = output
           ticker
