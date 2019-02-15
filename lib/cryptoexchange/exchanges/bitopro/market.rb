d495296e3273700995583dd7208409ef373c5f51
Wenjie Yek <dx3152@hotmail.com>
2019-02-15 17:19:45 +0800

    implement bitopro

---
+++ lib/cryptoexchange/exchanges/bitopro/market.rb
@@ -0,0 +1,15 @@
+module Cryptoexchange::Exchanges
+  module Bitopro
+    class Market < Cryptoexchange::Models::Market
+      NAME = 'bitopro'
+      API_URL = 'https://api.bitopro.com/v2'
+
+      def self.trade_page_url(args={})
+        base = args[:base].downcase
+        target = args[:target].downcase
+        "https://www.bitopro.com/trading/#{base}" if target == "twd"
+        "https://www.bitopro.com/trading/#{base}#{target}"
+      end
+    end
+  end
+end
