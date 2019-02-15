f5c745465961a40efbff56d5814894b617596d14
Wenjie Yek <dx3152@hotmail.com>
2019-02-15 16:32:28 +0800

    add trade url

--- lib/cryptoexchange/exchanges/bitshares_assets/market.rb
+++ lib/cryptoexchange/exchanges/bitshares_assets/market.rb
@@ -3,6 +3,12 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'bitshares_assets'
       API_URL = 'https://cryptofresh.com/api'
+
+      def self.trade_page_url(args = {})
+        base   = args[:base]
+        target = args[:target]
+        "https://openledger.io/market/#{base}_#{target}"
+      end
     end
   end
 end
