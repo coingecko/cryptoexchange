f29c51b445f186f4dcf29e8aacfbede9241ce396
Wenjie Yek <dx3152@hotmail.com>
2019-02-19 17:15:13 +0800

    fix idax api endpoint

--- lib/cryptoexchange/exchanges/idax/services/pairs.rb
+++ lib/cryptoexchange/exchanges/idax/services/pairs.rb
@@ -2,21 +2,17 @@ module Cryptoexchange::Exchanges
   module Idax
     module Services
       class Pairs < Cryptoexchange::Services::Pairs
-        PAIRS_URL = "#{Cryptoexchange::Exchanges::Idax::Market::API_URL}/GetAllOpenMarkets"
-        HTTP_METHOD = 'POST'
+        PAIRS_URL = "#{Cryptoexchange::Exchanges::Idax::Market::API_URL}/ticker"
 
-        def fetch_via_api
+        def fetch
           output = super
-          adapt(output)
-        end
-
-        def adapt(output)
-          output['data'].map do |market|
-            Cryptoexchange::Models::MarketPair.new({
-              base: market['baseCode'],
-              target: market['quoteCode'],
-              market: Idax::Market::NAME
-            })
+          output['ticker'].map do |output|
+            base, target = output['pair'].split('_')
+            Cryptoexchange::Models::MarketPair.new(
+                              base: base,
+                              target: target,
+                              market: Idax::Market::NAME
+            )
           end
         end
       end
--- spec/exchanges/idax/market_spec.rb
+++ spec/exchanges/idax/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Idax::Market do
   it { expect(described_class::NAME).to eq 'idax' }
-  it { expect(described_class::API_URL).to eq 'https://www.idax.mn/api/Signalr' }
+  it { expect(described_class::API_URL).to eq 'https://openapi.idax.pro/api/v2' }
 end
