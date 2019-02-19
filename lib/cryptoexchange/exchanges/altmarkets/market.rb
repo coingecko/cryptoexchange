5c4dc411930940e868f88f790e32274cf9de8198
elvis <yinchuang94@gmail.com>
2019-02-19 17:10:34 +0800

    wip

--- lib/cryptoexchange/exchanges/altmarkets/market.rb
+++ lib/cryptoexchange/exchanges/altmarkets/market.rb
@@ -2,10 +2,10 @@ module Cryptoexchange::Exchanges
   module Altmarkets
     class Market < Cryptoexchange::Models::Market
       NAME = 'altmarkets'
-      API_URL = 'https://altmarkets.cc/api/v1'
+      API_URL = 'https://altmarkets.io/swagger?url=/api/v2/swagger'
 
       def self.trade_page_url(args = {})
-        "https://altmarkets.cc/market/#{args[:target]}-#{args[:base]}"
+        "https://altmarkets.io/trading/#{args[:target]}#{args[:base]}"
       end
     end
   end
