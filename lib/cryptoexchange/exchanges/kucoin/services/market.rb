b984cbbb0436260d2fb725699b133ec7be6da088
Wenjie Yek <dx3152@hotmail.com>
2019-02-19 10:27:59 +0800

    fix kucoin endpoint

--- lib/cryptoexchange/exchanges/kucoin/services/market.rb
+++ lib/cryptoexchange/exchanges/kucoin/services/market.rb
@@ -4,45 +4,32 @@ module Cryptoexchange::Exchanges
       class Market < Cryptoexchange::Services::Market
         class << self
           def supports_individual_ticker_query?
-            false
+            true
           end
         end
 
-        def fetch
-          output = super ticker_url
-          adapt_all(output)
+        def fetch(market_pair)
+          output = super(ticker_url(market_pair))
+          adapt(output, market_pair)
         end
 
-        def ticker_url
-          "#{Cryptoexchange::Exchanges::Kucoin::Market::API_URL}/open/tick"
-        end
-
-        def adapt_all(output)
-          output['data'].map do |ticker|
-            base, target = ticker['symbol'].split('-')
-            market_pair = Cryptoexchange::Models::MarketPair.new(
-                            base: base,
-                            target: target,
-                            market: Kucoin::Market::NAME
-                          )
-            adapt(ticker, market_pair)
-          end
+        def ticker_url(market_pair)
+          "#{Cryptoexchange::Exchanges::Kucoin::Market::API_URL}/market/stats?symbol=#{market_pair.base.upcase}-#{market_pair.target.upcase}"
         end
 
         def adapt(output, market_pair)
-          market = output
-          ticker = Cryptoexchange::Models::Ticker.new
-          ticker.base = market_pair.base
-          ticker.target = market_pair.target
-          ticker.market = Kucoin::Market::NAME
-          ticker.ask = NumericHelper.to_d(market['sell'])
-          ticker.bid = NumericHelper.to_d(market['buy'])
-          ticker.last = NumericHelper.to_d(market['lastDealPrice'])
-          ticker.high = NumericHelper.to_d(market['high'])
-          ticker.low = NumericHelper.to_d(market['low'])
-          ticker.volume = NumericHelper.to_d(market['vol'])
-          ticker.timestamp = market['datetime'].to_i / 1000
-          ticker.payload = market
+          ticker           = Cryptoexchange::Models::Ticker.new
+          ticker_json      = output['data']
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
+          ticker.market    = Kucoin::Market::NAME
+
+          ticker.last      = NumericHelper.to_d(ticker_json['close'])
+          ticker.high      = NumericHelper.to_d(ticker_json['high'])
+          ticker.low       = NumericHelper.to_d(ticker_json['low'])
+          ticker.volume    = NumericHelper.to_d(ticker_json['vol'])
+          ticker.timestamp = nil
+          ticker.payload   = output
           ticker
         end
       end
