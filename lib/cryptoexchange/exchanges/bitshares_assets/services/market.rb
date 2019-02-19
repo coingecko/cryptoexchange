0e97c79e5719bee3c92aade1a7765e01f7f9b0f0
TM Lee <tmlee.ltm@gmail.com>
2019-02-19 12:16:57 +0800

    Change bitshares assets to user defined pairs due to too many of them

--- lib/cryptoexchange/exchanges/bitshares_assets/services/market.rb
+++ lib/cryptoexchange/exchanges/bitshares_assets/services/market.rb
@@ -4,40 +4,29 @@ module Cryptoexchange::Exchanges
       class Market < Cryptoexchange::Services::Market
         class << self
           def supports_individual_ticker_query?
-            false
+            true
           end
         end
 
-        def fetch
-          output = super(ticker_url)
-          adapt_all(output)
+        def fetch(market_pair)
+          output = super(ticker_url(market_pair))
+          adapt(output, market_pair)
         end
 
-        def ticker_url
-          "#{Cryptoexchange::Exchanges::BitsharesAssets::Market::API_URL}/asset/markets?asset=BTS"
+        def ticker_url(market_pair)
+          target = market_pair.target
+          "#{Cryptoexchange::Exchanges::BitsharesAssets::Market::API_URL}/asset/markets?asset=#{target}"
         end
 
-        def adapt_all(output)
-          output.map do |output|
-            base = output[0]
-            target = "BTS"
-            market_pair = Cryptoexchange::Models::MarketPair.new(
-                            base: base,
-                            target: target,
-                            market: BitsharesAssets::Market::NAME
-                          )
-            adapt(market_pair, output)
-          end
-        end
-
-        def adapt(market_pair, output)
+        def adapt(output, market_pair)
+          output = output[market_pair.base]
           ticker           = Cryptoexchange::Models::Ticker.new
           ticker.base      = market_pair.base
           ticker.target    = market_pair.target
           ticker.market    = BitsharesAssets::Market::NAME
 
-          ticker.last      = NumericHelper.divide(1.0, NumericHelper.to_d(output[1]['price']))
-          ticker.volume    = NumericHelper.to_d(output[1]['volume24'])
+          ticker.last      = NumericHelper.divide(1.0, NumericHelper.to_d(output['price']))
+          ticker.volume    = NumericHelper.to_d(output['volume24'])
           ticker.timestamp = nil
           ticker.payload   = output
           ticker
--- lib/cryptoexchange/exchanges/bitshares_assets/services/pairs.rb
+++ lib/cryptoexchange/exchanges/bitshares_assets/services/pairs.rb
@@ -2,23 +2,21 @@ module Cryptoexchange::Exchanges
   module BitsharesAssets
     module Services
       class Pairs < Cryptoexchange::Services::Pairs
-        PAIRS_URL = "#{Cryptoexchange::Exchanges::BitsharesAssets::Market::API_URL}/asset/markets?asset=BTS"
-
         def fetch
           output = super
           adapt(output)
         end
 
         def adapt(output)
-          output.map do |output|
-            base = output[0]
-            target = "BTS"
-            Cryptoexchange::Models::MarketPair.new(
-              base: base,
-              target: target,
-              market: BitsharesAssets::Market::NAME
-            )
+          market_pairs = []
+          output.each do |pair|
+            market_pairs << Cryptoexchange::Models::MarketPair.new(
+                              base: pair[:base],
+                              target: pair[:target],
+                              market: BitsharesAssets::Market::NAME
+                            )
           end
+          market_pairs
         end
       end
     end
