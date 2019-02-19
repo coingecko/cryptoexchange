3b959c5e20311ce48561e05430943a12772286a9
Wenjie Yek <dx3152@hotmail.com>
2019-02-19 14:53:24 +0800

    edit readme and spec

--- lib/cryptoexchange/exchanges/therocktrading/market.rb
+++ lib/cryptoexchange/exchanges/therocktrading/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'therocktrading'
       API_URL = 'https://api.therocktrading.com/v1'
+
+      def self.trade_page_url(args = {})
+        "https://www.therocktrading.com/en/offers/#{args[:base].upcase}#{args[:target].upcase}"
+      end
     end
   end
 end
