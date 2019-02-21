d25abea652e5236b3a27c8d1b5e6cf833021465d
TM Lee <tmlee.ltm@gmail.com>
2019-02-21 15:14:05 +0800

    Fix kucoin changed to last from close, and use all tickers

--- lib/cryptoexchange/exchanges/kucoin/services/market.rb
+++ lib/cryptoexchange/exchanges/kucoin/services/market.rb
@@ -4,30 +4,41 @@ module Cryptoexchange::Exchanges
       class Market < Cryptoexchange::Services::Market
         class << self
           def supports_individual_ticker_query?
-            true
+            false
           end
         end
 
-        def fetch(market_pair)
-          output = super(ticker_url(market_pair))
-          adapt(output, market_pair)
+        def fetch
+          output = super(ticker_url)
+          adapt_all(output)
         end
 
-        def ticker_url(market_pair)
-          "#{Cryptoexchange::Exchanges::Kucoin::Market::API_URL}/market/stats?symbol=#{market_pair.base.upcase}-#{market_pair.target.upcase}"
+        def ticker_url
+          "#{Cryptoexchange::Exchanges::Kucoin::Market::API_URL}/market/allTickers"
+        end
+
+        def adapt_all(output)
+          output["data"]["ticker"].map do |ticker|
+            base, target = ticker["symbol"].split('-')
+            market_pair = Cryptoexchange::Models::MarketPair.new(
+                            base: base,
+                            target: target,
+                            market: Kucoin::Market::NAME
+                          )
+            adapt(ticker, market_pair)
+          end
         end
 
         def adapt(output, market_pair)
           ticker           = Cryptoexchange::Models::Ticker.new
-          ticker_json      = output['data']
           ticker.base      = market_pair.base
           ticker.target    = market_pair.target
           ticker.market    = Kucoin::Market::NAME
 
-          ticker.last      = NumericHelper.to_d(ticker_json['close'])
-          ticker.high      = NumericHelper.to_d(ticker_json['high'])
-          ticker.low       = NumericHelper.to_d(ticker_json['low'])
-          ticker.volume    = NumericHelper.to_d(ticker_json['vol'])
+          ticker.last      = NumericHelper.to_d(output['last'])
+          ticker.high      = NumericHelper.to_d(output['high'])
+          ticker.low       = NumericHelper.to_d(output['low'])
+          ticker.volume    = NumericHelper.to_d(output['vol'])
           ticker.timestamp = nil
           ticker.payload   = output
           ticker
