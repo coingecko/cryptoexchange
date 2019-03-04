89a24e03642a2469e1a919fedc081409b0c311ee
Kees de Wit <kees@txbit.io>
2019-03-04 11:19:42 +0100

    Set condition for nil timestamps

--- lib/cryptoexchange/exchanges/txbit/services/market.rb
+++ lib/cryptoexchange/exchanges/txbit/services/market.rb
@@ -33,7 +33,7 @@ module Cryptoexchange::Exchanges
           ticker.ask       = NumericHelper.to_d(market['Ask'])
           ticker.bid       = NumericHelper.to_d(market['Bid'])
           ticker.volume    = NumericHelper.to_d(market['BaseVolume'])
-          ticker.timestamp = NumericHelper.to_d(DateTime.parse(market['TimeStamp']).to_time.to_i)
+          ticker.timestamp = market['TimeStamp'] ? NumericHelper.to_d(DateTime.parse(market['TimeStamp']).to_time.to_i) : nil
           ticker.payload   = market
           ticker
         end
