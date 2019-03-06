a7f4494b2f055ce8ea709615a95b8e3f652a03a9
Wenjie Yek <dx3152@hotmail.com>
2019-03-06 16:28:42 +0800

    edit swiftex link

--- spec/exchanges/swiftex/integration/market_spec.rb
+++ spec/exchanges/swiftex/integration/market_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Swiftex integration specs' do
 
   it 'has trade_page_url' do
     trade_page_url = client.trade_page_url mynt_btc_pair.market, base: mynt_btc_pair.base, target: mynt_btc_pair.target
-    expect(trade_page_url).to eq "https://swiftex.co/markets/mynt-btc"
+    expect(trade_page_url).to eq "https://swiftex.co/trading/mynt-btc"
   end
 
   it 'fetch pairs' do
