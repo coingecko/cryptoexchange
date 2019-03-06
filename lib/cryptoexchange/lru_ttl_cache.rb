628b1e56534ce4bbf9369fef289528b147011960
Jack Wong <jackyhwong91@gmail.com>
2019-03-06 12:45:15 +0800

    Fix Rspec

--- lib/cryptoexchange/lru_ttl_cache.rb
+++ lib/cryptoexchange/lru_ttl_cache.rb
@@ -1,7 +1,6 @@
 class LruTtlCache
   class << self
     def ticker_cache
-      Cryptoexchange.configuration.cache_size
       @@ticker_cache ||= LruRedux::TTL::Cache.new(
         Cryptoexchange.configuration.cache_size,
         Cryptoexchange.configuration.ticker_ttl
