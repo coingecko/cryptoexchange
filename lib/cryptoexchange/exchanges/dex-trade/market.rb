0ba1d9b3b570c7f17387b904ff2895e21626729e
Vadim Skrupskiy <vadim.skrupskuy@webporttechnology.com>
2019-03-11 12:09:04 +0200

    Revert "Dex-Trade implementation"
    This reverts commit 51263aee7956ed752f4dd4c440e2a27cad2cc09b.

--- lib/cryptoexchange/exchanges/dex-trade/market.rb
+++
@@ -1,8 +0,0 @@
-module Cryptoexchange::Exchanges
-  module Dextrade
-    class Market < Cryptoexchange::Models::Market
-      NAME = 'Dextrade'
-      API_URL = 'https://api.dex-trade.com/v1/coingecko'
-    end
-  end
-end
--- spec/exchanges/dex-trade/market_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe Cryptoexchange::Exchanges::Dextrade::Market do
-  it { expect(described_class::NAME).to eq 'Dextrade' }
-  it { expect(described_class::API_URL).to eq 'https://api.dex-trade.com/v1/coingecko' }
-end
