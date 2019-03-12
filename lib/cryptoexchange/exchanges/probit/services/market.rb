3f0e5d10d1d5e3a64223844ebc48087cca4865be
Jack Wong <jackyhwong91@gmail.com>
2019-03-12 14:48:09 +0800

    Implement Probit exchange (#1416)

---
+++ lib/cryptoexchange/exchanges/probit/services/market.rb
@@ -0,0 +1,43 @@
+module Cryptoexchange::Exchanges
+  module Probit
+    module Services
+      class Market < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            false
+          end
+        end
+
+        def fetch
+          output = super(ticker_url)
+          adapt_all(output)
+        end
+
+        def ticker_url
+          "#{Cryptoexchange::Exchanges::Probit::Market::API_URL}/ticker"
+        end
+
+        def adapt_all(output)
+          output['data'].map do |ticker|
+            adapt(ticker)
+          end
+        end
+
+        def adapt(output)
+          base, target     = output['market_id'].split('-')
+          ticker           = Cryptoexchange::Models::Ticker.new
+          ticker.base      = base
+          ticker.target    = target
+          ticker.market    = Probit::Market::NAME
+          ticker.last      = NumericHelper.to_d(output['last'])
+          ticker.high      = NumericHelper.to_d(output['high'])
+          ticker.low       = NumericHelper.to_d(output['low'])
+          ticker.volume    = NumericHelper.to_d(output['base_volume'])
+          ticker.timestamp = output['time'] ? DateTime.parse(output['time']).to_time.to_i : nil
+          ticker.payload   = output
+          ticker
+        end
+      end
+    end
+  end
+end
