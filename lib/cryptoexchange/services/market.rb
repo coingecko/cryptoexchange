fe32c43f247668772c52598c02f390b516249ec9
Jack Wong <jackyhwong91@gmail.com>
2019-03-12 16:52:52 +0800

    update httprb gem, increase timeout duration

--- lib/cryptoexchange/services/market.rb
+++ lib/cryptoexchange/services/market.rb
@@ -45,7 +45,7 @@ module Cryptoexchange
       private
 
       def http_get(endpoint)
-        HTTP.timeout(:write => 2, :connect => 10, :read => 12).follow.get(endpoint)
+        HTTP.timeout(:write => 2, :connect => 15, :read => 18).follow.get(endpoint)
       end
 
       def http_post(endpoint, params)
--- lib/cryptoexchange/version.rb
+++ lib/cryptoexchange/version.rb
@@ -1,3 +1,3 @@
 module Cryptoexchange
-  VERSION = "0.3.0"
+  VERSION = "0.31.0"
 end
