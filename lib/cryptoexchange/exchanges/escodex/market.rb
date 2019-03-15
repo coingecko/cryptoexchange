d5fe0c2dcf71e5221289d1c698fafd7c0ce99a72
elvis <yinchuang94@gmail.com>
2019-03-15 14:57:55 +0800

    add trade page url

--- lib/cryptoexchange/exchanges/escodex/market.rb
+++ lib/cryptoexchange/exchanges/escodex/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market
       NAME = 'escodex'
       API_URL = 'http://labs.escodex.com/api'
+
+      def self.trade_page_url(args={})
+        "https://wallet.escodex.com/market/ESCODEX.#{args[:base]}_ESCODEX.#{args[:target]}"
+      end
     end
   end
 end
