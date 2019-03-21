06ba47b07be5afba1970b3e8b2d6e09ea89ae6d9
elvis <yinchuang94@gmail.com>
2019-03-21 13:28:50 +0800

    remove support for trade page url

--- lib/cryptoexchange/exchanges/tron_trade/market.rb
+++ lib/cryptoexchange/exchanges/tron_trade/market.rb
@@ -3,13 +3,6 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'tron_trade'
       API_URL = 'https://trontrade.io/graphql'
-
-      def self.trade_page_url(args={})
-        client = Cryptoexchange::Client.new
-        pair = Cryptoexchange::Models::MarketPair.new(base: args[:base], target: args[:target], market: 'tron_trade')
-        ticker = client.ticker(pair)
-        "https://trontrade.io/exchange/#{ticker.payload["id"]}"
-      end
     end
   end
 end
