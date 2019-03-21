5220c65103c4f121dfd685c6d446f95fcf7f47ac
Jack Wong <jackyhwong91@gmail.com>
2019-03-21 15:34:18 +0800

    update zgtop api endpoint (#1438)

--- spec/exchanges/zgtop/market_spec.rb
+++ spec/exchanges/zgtop/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Zgtop::Market do
   it { expect(described_class::NAME).to eq 'zgtop' }
-  it { expect(described_class::API_URL).to eq 'https://www.zg.top/API/api/v2' }
+  it { expect(described_class::API_URL).to eq 'https://www.zgtop.io/API/api/v1' }
 end
