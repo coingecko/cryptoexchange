6a2fe47eba427d5ee22f08531e68b5aa0780582e
Wenjie Yek <dx3152@hotmail.com>
2019-04-02 12:09:05 +0800

    Wj implement tokenize exchange (#1454)
    * implement tokenize exchange
    
    * add trade_page_url
    
    * update api link
    
    * edit test
    
    * fix base target position
    
    * edit readme

---
+++ spec/exchanges/tokenize/market_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+RSpec.describe Cryptoexchange::Exchanges::Tokenize::Market do
+  it { expect(described_class::NAME).to eq 'tokenize' }
+  it { expect(described_class::API_URL).to eq 'https://api2.tokenize.exchange/public/v1' }
+end
