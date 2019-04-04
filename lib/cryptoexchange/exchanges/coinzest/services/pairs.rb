c14a277a99f4638a2d5a27c4948edf6051e91d01
Wenjie Yek <dx3152@hotmail.com>
2019-04-04 16:52:19 +0800

    fix coinzest (#1459)

--- lib/cryptoexchange/exchanges/coinzest/services/pairs.rb
+++ lib/cryptoexchange/exchanges/coinzest/services/pairs.rb
@@ -2,23 +2,21 @@ module Cryptoexchange::Exchanges
   module Coinzest
     module Services
       class Pairs < Cryptoexchange::Services::Pairs
-        PAIRS_URL = "#{Cryptoexchange::Exchanges::Coinzest::Market::API_URL}/market"
+        PAIRS_URL = "#{Cryptoexchange::Exchanges::Coinzest::Market::API_URL}/market_summary"
 
         def fetch
           ctx = OpenSSL::SSL::SSLContext.new
           ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
           result = HTTP.get(PAIRS_URL, ssl_context: ctx)
           output = JSON.parse(result)
-          market_pairs = []
-          output['list'].each do |pair|
-            market_pairs << Cryptoexchange::Models::MarketPair.new(
-                              base: pair['trdCoinName'],
-                              target: pair['mrkCoinName'],
-                              inst_id: "#{pair['mrkCoin']}-#{pair['trdCoin']}",
-                              market: Coinzest::Market::NAME
-                            )
+          output['result'].map do |pair|
+            target, base = pair['MarketName'].split('-')
+            Cryptoexchange::Models::MarketPair.new(
+              base: base,
+              target: target,
+              market: Coinzest::Market::NAME
+            )
           end
-          market_pairs
         end
       end
     end
