fa73774ad4de6c0c286d3609e1bd6df21c2e543e
wenjieyek <dx3152@hotmail.com>
2019-04-10 15:44:34 +0800

    fix digifinex

--- lib/cryptoexchange/exchanges/digifinex/market.rb
+++ lib/cryptoexchange/exchanges/digifinex/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Digifinex
     class Market
       NAME = 'digifinex'
-      API_URL = 'https://data.block.cc/api/v1'
+      API_URL = 'https://openapi.digifinex.vip/v2'
     end
   end
 end
