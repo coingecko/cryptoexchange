9dd55a2f842d8f02ea424fd21da1cc45e964cdf9
wenjieyek <dx3152@hotmail.com>
2019-04-12 18:00:47 +0800

    add spec and readme

--- lib/cryptoexchange/exchanges/btcexa/market.rb
+++ lib/cryptoexchange/exchanges/btcexa/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'btcexa'
       API_URL = 'https://api.btcexa.com/api'
+
+      def self.trade_page_url(args={})
+        "https://btcexa.com/exchange?tag=#{args[:target]}&name=#{args[:base]}_#{args[:target]}"
+      end
     end
   end
 end
