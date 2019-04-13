b88c4cfff921b83e26225231af39dd18e33644b3
Nikita Datsenko <nikdatsenko@gmail.com>
2019-04-13 13:04:43 +0300

    whitebit integration

--- lib/cryptoexchange/exchanges/whitebit/market.rb
+++ lib/cryptoexchange/exchanges/whitebit/market.rb
@@ -1,8 +1,12 @@
 module Cryptoexchange::Exchanges
   module Whitebit
-    class Market
+    class Market < Cryptoexchange::Models::Market
       NAME = 'whitebit'
-      API_URL = 'https://whitebit.com/api/v2/'
+      API_URL = 'https://whitebit.com/api/v1/public'
+
+      def self.trade_page_url(args={})
+        "https://whitebit.com/trade/#{args[:base]}_#{args[:target]}"
+      end
     end
   end
 end
--- spec/exchanges/whitebit/market_spec.rb
+++ spec/exchanges/whitebit/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Whitebit::Market do
   it { expect(described_class::NAME).to eq 'whitebit' }
-  it { expect(described_class::API_URL).to eq 'https://whitebit.com/api/v2' }
+  it { expect(described_class::API_URL).to eq 'https://whitebit.com/api/v1/public' }
 end
