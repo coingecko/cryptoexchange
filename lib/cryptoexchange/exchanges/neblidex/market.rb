a3870f5a945e04b0a0712390b8b8b1b1ec39bad3
david4neblio <43017430+david4neblio@users.noreply.github.com>
2019-04-22 17:25:39 -0400

    Added trade page url

--- lib/cryptoexchange/exchanges/neblidex/market.rb
+++ lib/cryptoexchange/exchanges/neblidex/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market
       NAME = 'neblidex'
       API_URL = 'https://www.neblidex.xyz/seed/?v=1&api=get_market_ticker'
+      
+      def self.trade_page_url(args={})
+        "https://www.neblidex.xyz"
+      end
     end
   end
 end
