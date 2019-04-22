e216457500e1dea8bba46d2bb87c06554516882e
david4neblio <43017430+david4neblio@users.noreply.github.com>
2019-04-22 17:43:38 -0400

    fixed case

--- spec/exchanges/neblidex/market_spec.rb
+++ spec/exchanges/neblidex/market_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe Cryptoexchange::Exchanges::NebliDex::Market do
+RSpec.describe Cryptoexchange::Exchanges::Neblidex::Market do
   it { expect(described_class::NAME).to eq 'neblidex' }
   it { expect(described_class::API_URL).to eq 'https://www.neblidex.xyz/seed/?v=1&api=get_market_ticker' }
 end
