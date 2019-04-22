05df19c6a6e2c707e37bf6c62f63c91482200110
david4neblio <43017430+david4neblio@users.noreply.github.com>
2019-04-22 15:53:15 -0400

    Added neblidex market.rb

---
+++ lib/cryptoexchange/exchanges/neblidex/market.rb
@@ -0,0 +1,8 @@
+module Cryptoexchange::Exchanges
+  module NebliDex
+    class Market
+      NAME = 'neblidex'
+      API_URL = 'https://www.neblidex.xyz/seed/?v=1&api=get_market_ticker'
+    end
+  end
+end
