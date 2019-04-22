ca9dd85eff527be6f67f2c88096911cb8bef81a8
david4neblio <43017430+david4neblio@users.noreply.github.com>
2019-04-22 17:44:07 -0400

    Fixed case

--- lib/cryptoexchange/exchanges/neblidex/market.rb
+++ lib/cryptoexchange/exchanges/neblidex/market.rb
@@ -1,5 +1,5 @@
 module Cryptoexchange::Exchanges
-  module NebliDex
+  module Neblidex
     class Market
       NAME = 'neblidex'
       API_URL = 'https://www.neblidex.xyz/seed/?v=1&api=get_market_ticker'
