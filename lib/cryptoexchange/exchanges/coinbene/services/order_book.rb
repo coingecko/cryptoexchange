b4cd4d4863e1b9b5385b95b240f9ab1443c3d21c
TM Lee <tmlee.ltm@gmail.com>
2019-04-24 13:51:03 +0800

    [#Orderbook] Verify more exchanges, binance more order book
    - Verify orderbook of bitebtc, bitstamp, cobinhood, coinegg, coinbene, coinone, kryptono, kucoin
    - Binance orderbook to return more data

--- lib/cryptoexchange/exchanges/coinbene/services/order_book.rb
+++ lib/cryptoexchange/exchanges/coinbene/services/order_book.rb
@@ -14,7 +14,7 @@ module Cryptoexchange::Exchanges
         end
 
         def ticker_url(market_pair)
-          "#{Cryptoexchange::Exchanges::Coinbene::Market::API_URL}/market/orderbook?symbol=#{market_pair.base}#{market_pair.target}"
+          "#{Cryptoexchange::Exchanges::Coinbene::Market::API_URL}/market/orderbook?symbol=#{market_pair.base}#{market_pair.target}&depth=500"
         end
 
         def adapt(output, market_pair)
