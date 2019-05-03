97d661d916b1597723e2ded997205a344982c966
elvis <yinchuang94@gmail.com>
2019-05-03 17:10:32 +0800

    deribit exchange intergration

---
+++ lib/cryptoexchange/exchanges/deribit/services/order_book.rb
@@ -0,0 +1,55 @@
+module Cryptoexchange::Exchanges
+  module Deribit
+    module Services
+      class OrderBook < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            true
+          end
+        end
+
+        def fetch(market_pair)
+          output = super(orderbook_url(market_pair))
+          split_orderbook = split(output)
+          adapt(split_orderbook, market_pair)
+        end
+
+        def orderbook_url(market_pair)
+          symbol = "#{market_pair.base}-#{market_pair.contract_interval.upcase}"
+          "#{Cryptoexchange::Exchanges::Deribit::Market::API_URL}/getorderbook?instrument=#{symbol}"
+        end
+
+        def split(output)
+          asks = []
+          bids = []
+          bids = output["result"]["bids"]
+          asks = output["result"]["asks"]
+
+          {asks: asks, bids: bids}
+        end
+
+        def adapt(output, market_pair)
+          order_book = Cryptoexchange::Models::OrderBook.new
+          timestamp = Time.now.to_i
+
+          order_book.base      = market_pair.base
+          order_book.target    = market_pair.target
+          order_book.market    = Deribit::Market::NAME
+          order_book.asks      = adapt_orders(output[:asks])
+          order_book.bids      = adapt_orders(output[:bids])
+          order_book.timestamp = timestamp
+          order_book.payload   = output
+          order_book
+        end
+
+        def adapt_orders(orders)
+          orders.collect do |order_entry|
+            Cryptoexchange::Models::Order.new(price: order_entry['price'],
+                                              amount: order_entry['amount'],
+                                              timestamp: nil)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/exchanges/deribit/market_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+RSpec.describe Cryptoexchange::Exchanges::Deribit::Market do
+  it { expect(described_class::NAME).to eq 'deribit' }
+  it { expect(described_class::API_URL).to eq 'https://www.deribit.com/api/v1/public' }
+end
