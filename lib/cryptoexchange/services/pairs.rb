a7f724244caa9512ebff4e6231227833eb329722
TM Lee <tmlee.ltm@gmail.com>
2019-05-03 11:28:13 +0800

    Workaround Binance using adapt_all tickers, Bibox build fix

--- lib/cryptoexchange/services/pairs.rb
+++ lib/cryptoexchange/services/pairs.rb
@@ -20,23 +20,25 @@ module Cryptoexchange
       end
 
       def fetch_via_api(endpoint = self.class::PAIRS_URL, params = self.class::POST_PARAMS)
-        begin
-          fetch_response = self.class::HTTP_METHOD == 'POST' ? http_post(endpoint, params) : http_get(endpoint)
-          if fetch_response.code == 200
-            fetch_response.parse :json
-          elsif fetch_response.code == 400
-            raise Cryptoexchange::HttpBadRequestError, { response: fetch_response }
-          else
-            raise Cryptoexchange::HttpResponseError, { response: fetch_response }
+        Cryptoexchange::Cache.ticker_cache.fetch(endpoint) do
+          begin
+            fetch_response = self.class::HTTP_METHOD == 'POST' ? http_post(endpoint, params) : http_get(endpoint)
+            if fetch_response.code == 200
+              fetch_response.parse :json
+            elsif fetch_response.code == 400
+              raise Cryptoexchange::HttpBadRequestError, { response: fetch_response }
+            else
+              raise Cryptoexchange::HttpResponseError, { response: fetch_response }
+            end
+          rescue HTTP::ConnectionError => e
+            raise Cryptoexchange::HttpConnectionError, { error: e, response: fetch_response }
+          rescue HTTP::TimeoutError => e
+            raise Cryptoexchange::HttpTimeoutError, { error: e, response: fetch_response }
+          rescue JSON::ParserError => e
+            raise Cryptoexchange::JsonParseError, { error: e, response: fetch_response }
+          rescue TypeError => e
+            raise Cryptoexchange::TypeFormatError, { error: e, response: fetch_response }
           end
-        rescue HTTP::ConnectionError => e
-          raise Cryptoexchange::HttpConnectionError, { error: e, response: fetch_response }
-        rescue HTTP::TimeoutError => e
-          raise Cryptoexchange::HttpTimeoutError, { error: e, response: fetch_response }
-        rescue JSON::ParserError => e
-          raise Cryptoexchange::JsonParseError, { error: e, response: fetch_response }
-        rescue TypeError => e
-          raise Cryptoexchange::TypeFormatError, { error: e, response: fetch_response }
         end
       end
 
