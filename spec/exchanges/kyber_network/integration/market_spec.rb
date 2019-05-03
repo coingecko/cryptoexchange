31a1df0567f31e11d01c41979e271f91bb5f518c
elvis <yinchuang94@gmail.com>
2019-05-03 10:38:23 +0800

    spacing

--- spec/exchanges/kyber_network/integration/market_spec.rb
+++ spec/exchanges/kyber_network/integration/market_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe 'KyberNetwork integration specs' do
     expect(ticker.high).to be_nil
     expect(ticker.volume).to be_a Numeric
     expect(ticker.timestamp).to be nil
-
+    
     expect(ticker.payload).to_not be nil
   end
 end
