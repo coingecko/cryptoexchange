673042d8fc7c34e212bc3ff3aa549064b5907baa
Jack Wong <jackyhwong91@gmail.com>
2019-05-10 13:05:34 +0800

    verify bitinfi and bitinka

--- lib/cryptoexchange/exchanges/bitinfi/services/order_book.rb
+++ lib/cryptoexchange/exchanges/bitinfi/services/order_book.rb
@@ -25,15 +25,17 @@ module Cryptoexchange::Exchanges
           order_book.market    = Bitinfi::Market::NAME
           order_book.asks      = adapt_orders(output['data']['asks'])
           order_book.bids      = adapt_orders(output['data']['bids'])
-          order_book.timestamp = Time.now.to_i
+          order_book.timestamp = nil
           order_book.payload   = output
           order_book
         end
 
         def adapt_orders(orders)
-          orders.collect do |order_entry|
-            Cryptoexchange::Models::Order.new(price: order_entry['price'],
-                                              amount: order_entry['quantity'])
+          orders.map do |order_entry|
+            Cryptoexchange::Models::Order.new(
+              price: order_entry['price'],
+              amount: order_entry['quantity']
+            )
           end
         end
       end
