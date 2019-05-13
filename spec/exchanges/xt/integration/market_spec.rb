5b9b4a4281758b58caed81861ccdbb7f0ff70f1d
wenjieyek <dx3152@hotmail.com>
2019-05-13 14:46:06 +0800

    implement order_book

--- spec/exchanges/xt/integration/market_spec.rb
+++ spec/exchanges/xt/integration/market_spec.rb
@@ -34,38 +34,37 @@ RSpec.describe 'xt integration specs' do
     expect(ticker.payload).to_not be nil
   end
 
-  # it 'fetch order book' do
-  #   order_book = client.order_book(btc_usdt_pair)
+  it 'fetch order book' do
+    order_book = client.order_book(btc_usdt_pair)
 
-  #   expect(order_book.base).to eq 'BTC'
-  #   expect(order_book.target).to eq 'USDT'
-  #   expect(order_book.market).to eq 'xt'
-  #   expect(order_book.asks).to_not be_empty
-  #   expect(order_book.bids).to_not be_empty
-  #   expect(order_book.asks.first.price).to_not be_nil
-  #   expect(order_book.bids.first.amount).to_not be_nil
-  #   expect(order_book.bids.first.timestamp).to be_nil
-  #   expect(order_book.asks.count).to be > 5
-  #   expect(order_book.bids.count).to be > 5
-  #   expect(order_book.timestamp).to be_a Numeric
-  #   expect(2000..Date.today.year).to include(Time.at(order_book.timestamp).year)
-  #   expect(order_book.payload).to_not be nil
-  # end
+    expect(order_book.base).to eq 'BTC'
+    expect(order_book.target).to eq 'USDT'
+    expect(order_book.market).to eq 'xt'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to be_nil
+    expect(order_book.asks.count).to be > 5
+    expect(order_book.bids.count).to be > 5
+    expect(order_book.timestamp).to be_a Numeric
+    expect(2000..Date.today.year).to include(Time.at(order_book.timestamp).year)
+    expect(order_book.payload).to_not be nil
+  end
 
-  # it 'fetch trade' do
-  #   trades = client.trades(btc_usdt_pair)
-  #   trade = trades.sample
+  it 'fetch trade' do
+    trades = client.trades(btc_usdt_pair)
+    trade = trades.sample
 
-  #   expect(trades).to_not be_empty
-  #   expect(trade.base).to eq 'BTC'
-  #   expect(trade.target).to eq 'USDT'
-  #   expect(trade.market).to eq 'xt'
-  #   expect(trade.trade_id).to_not be_nil
-  #   expect(['buy', 'sell']).to include trade.type
-  #   expect(trade.price).to_not be_nil
-  #   expect(trade.amount).to_not be_nil
-  #   expect(trade.timestamp).to be_a Numeric
-  #   expect(2000..Date.today.year).to include(Time.at(trade.timestamp).year)
-  #   expect(trade.payload).to_not be nil
-  # end
+    expect(trades).to_not be_empty
+    expect(trade.base).to eq 'BTC'
+    expect(trade.target).to eq 'USDT'
+    expect(trade.market).to eq 'xt'
+    expect(['bid', 'ask']).to include trade.type
+    expect(trade.price).to_not be_nil
+    expect(trade.amount).to_not be_nil
+    expect(trade.timestamp).to be_a Numeric
+    expect(2000..Date.today.year).to include(Time.at(trade.timestamp).year)
+    expect(trade.payload).to_not be nil
+  end
 end
