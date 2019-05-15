fc56bf60ac4fd45c9045e84e5861d7f5625f3d09
Jack Wong <jackyhwong91@gmail.com>
2019-05-15 10:37:37 +0800

    update qtrade orderbooks

--- lib/cryptoexchange/exchanges/qtrade/services/order_book.rb
+++ lib/cryptoexchange/exchanges/qtrade/services/order_book.rb
@@ -21,14 +21,13 @@ module Cryptoexchange::Exchanges
 
         def adapt(output, market_pair)
           order_book = Cryptoexchange::Models::OrderBook.new
-          timestamp = Time.now.to_i
 
           order_book.base      = market_pair.base
           order_book.target    = market_pair.target
           order_book.market    = Qtrade::Market::NAME
           order_book.asks      = adapt_orders output['data']['sell']
           order_book.bids      = adapt_orders output['data']['buy']
-          order_book.timestamp = timestamp
+          order_book.timestamp = nil
           order_book.payload   = output
           order_book
         end
@@ -38,8 +37,7 @@ module Cryptoexchange::Exchanges
             price, amount = order
             Cryptoexchange::Models::Order.new(
               price:     price,
-              amount:    amount,
-              timestamp: nil
+              amount:    amount
             )
           end
         end
