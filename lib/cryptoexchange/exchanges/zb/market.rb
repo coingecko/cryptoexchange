bda8d478eaaa15046a87f37122b02b2fe3fa9d17
Jack Wong <jackyhwong91@gmail.com>
2019-05-16 14:16:15 +0800

    updates ZB api endpoint.

--- lib/cryptoexchange/exchanges/zb/market.rb
+++ lib/cryptoexchange/exchanges/zb/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Zb
     class Market < Cryptoexchange::Models::Market
       NAME = 'zb'
-      API_URL = 'http://api.zb.com/data/v1'
+      API_URL = 'http://api.zb.cn/data/v1'
 
       def self.trade_page_url(args={})
         "https://trans.zb.com/#{args[:base].downcase}#{args[:target].downcase}"
