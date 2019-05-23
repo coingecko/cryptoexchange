4832adea23724534b7d87fc38f76967f6c4fe3eb
Jack Wong <jackyhwong91@gmail.com>
2019-05-23 16:16:20 +0800

    Jw fix cezex integration (#1557)
    * Implement Cezex Exchange
    
    * fix cezex readme typo, nullify ticker timestamp, update spec
    
    * remove unneccessary !

---
+++ lib/cryptoexchange/exchanges/cezex/services/market.rb
@@ -0,0 +1,40 @@
+module Cryptoexchange::Exchanges
+  module Cezex
+    module Services
+      class Market < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            true
+          end
+        end
+
+        def fetch(market_pair)
+          output = super(ticker_url(market_pair))
+          adapt(output, market_pair)
+        end
+
+        def ticker_url(market_pair)
+          market = "#{market_pair.base}#{market_pair.target}".downcase
+          "#{Cryptoexchange::Exchanges::Cezex::Market::API_URL}/tickers/#{market}.json"
+        end
+
+        def adapt(output, market_pair)
+          ticker           = Cryptoexchange::Models::Ticker.new
+          ticker_json      = output['ticker']
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
+          ticker.market    = Cezex::Market::NAME
+          ticker.bid       = NumericHelper.to_d(ticker_json['buy'])
+          ticker.ask       = NumericHelper.to_d(ticker_json['sell'])
+          ticker.low       = NumericHelper.to_d(ticker_json['low'])
+          ticker.high      = NumericHelper.to_d(ticker_json['high'])
+          ticker.last      = NumericHelper.to_d(ticker_json['last'])
+          ticker.volume    = NumericHelper.to_d(ticker_json['vol'])
+          ticker.timestamp = nil
+          ticker.payload   = output
+          ticker
+        end
+      end
+    end
+  end
+end
