ae49adfdb1179e175a959ac09a792a68f17be7ff
Wenjie Yek <dx3152@hotmail.com>
2019-05-23 15:04:18 +0800

    add graviex orderbook (#1556)

--- spec/exchanges/graviex/integration/market_spec.rb
+++ spec/exchanges/graviex/integration/market_spec.rb
@@ -29,4 +29,19 @@ RSpec.describe 'Graviex integration specs' do
     
     expect(ticker.payload).to_not be nil
   end
+
+  it 'fetch order book' do
+    order_book = client.order_book(gio_btc_pair)
+
+    expect(order_book.base).to eq 'GIO'
+    expect(order_book.target).to eq 'BTC'
+    expect(order_book.market).to eq 'graviex'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to be_nil
+    expect(order_book.timestamp).to be_a Numeric
+    expect(order_book.payload).to_not be nil
+  end  
 end
