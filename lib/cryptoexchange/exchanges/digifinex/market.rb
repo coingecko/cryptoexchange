1f636a823bc5b1278672ed1090156b4b853a5ec2
elvis <yinchuang94@gmail.com>
2019-06-07 11:59:54 +0800

    add order_book and trades to digifinex, add respective test, add trade_url

--- lib/cryptoexchange/exchanges/digifinex/market.rb
+++ lib/cryptoexchange/exchanges/digifinex/market.rb
@@ -3,6 +3,13 @@ module Cryptoexchange::Exchanges
     class Market
       NAME = 'digifinex'
       API_URL = 'https://openapi.digifinex.vip/v2'
+
+      #API_URL_V3 is used for order_book and trades, this url version is newly launched and does not support tickers
+      API_URL_V3 = 'https://openapi.digifinex.vip/v3'
+
+      def self.trade_page_url(args = {})
+        "https://www.digifinex.com/en-ww/trade/#{args[:target]}/#{args[:base]}"
+      end
     end
   end
 end
