3c7602cdfabb801402721f471f319db164db21cf
elvis <yinchuang94@gmail.com>
2019-06-13 16:48:13 +0800

    bcex exchange api url and endpoints changes

--- lib/cryptoexchange/exchanges/bcex/market.rb
+++ lib/cryptoexchange/exchanges/bcex/market.rb
@@ -2,10 +2,10 @@ module Cryptoexchange::Exchanges
   module Bcex
     class Market < Cryptoexchange::Models::Market
       NAME = 'bcex'
-      API_URL = 'https://www.bcex.ca'
+      API_URL = 'https://www.bcex.ca/api'
 
       def self.trade_page_url(args={})
-        "https://www.bcex.ca/trade/#{args[:base]}2#{args[:target]}"
+        "https://www.bcex.ca/trade/#{args[:base].downcase}_#{args[:target].downcase}"
       end
     end
   end
