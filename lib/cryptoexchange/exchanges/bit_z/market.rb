e8fcdd4992b4b6ebf71988c3cc6c96141b042380
Jack Wong <jackyhwong91@gmail.com>
2019-06-19 11:59:14 +0800

    update bit z trade_page_url

--- lib/cryptoexchange/exchanges/bit_z/market.rb
+++ lib/cryptoexchange/exchanges/bit_z/market.rb
@@ -5,7 +5,7 @@ module Cryptoexchange::Exchanges
       API_URL = "https://apiv2.bitz.com".freeze
 
       def self.trade_page_url(args={})
-        "https://bit-z.com/exchange/#{args[:base].downcase}_#{args[:target].downcase}"
+        "https://www.bit-z.com/exchange/#{args[:base].downcase}_#{args[:target].downcase}"
       end
     end
   end
