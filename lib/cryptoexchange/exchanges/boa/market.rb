9c6335c1067d7b37bda32babc2edc484d7309902
elvis <yinchuang94@gmail.com>
2019-06-24 15:47:07 +0800

    add Boa exchange

---
+++ lib/cryptoexchange/exchanges/boa/market.rb
@@ -0,0 +1,12 @@
+module Cryptoexchange::Exchanges
+  module Boa
+    class Market < Cryptoexchange::Models::Market
+      NAME = 'boa'
+      API_URL = 'https://api.boaexchange.com/api/v1/markets'
+
+      def self.trade_page_url(args={})
+        "https://www.boaexchange.com/market/base=#{args[:base]}_#{args[:target]}"
+      end
+    end
+  end
+end
