7960e93682f291c5b7e1d3000918d52622438524
Jack Wong <jackyhwong91@gmail.com>
2019-06-28 12:30:56 +0800

    add trade_page_url coinexchange (#1622)

--- spec/exchanges/coin_exchange/integration/market_spec.rb
+++ spec/exchanges/coin_exchange/integration/market_spec.rb
@@ -1,17 +1,23 @@
 require 'spec_helper'
 
 RSpec.describe 'CoinExchange integration specs' do
+  let(:market) { 'coin_exchange' }
   let(:client) { Cryptoexchange::Client.new }
-  let(:ltc_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'BTBC', target: 'BTC', market: 'coin_exchange') }
+  let(:ltc_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'BTBC', target: 'BTC', market: market) }
 
   it 'fetch pairs' do
-    pairs = client.pairs('coin_exchange')
+    pairs = client.pairs(market)
     expect(pairs).not_to be_empty
 
     pair = pairs.first
     expect(pair.base).not_to be_nil
     expect(pair.target).not_to be_nil
     expect(pair.market).to eq 'coin_exchange'
+  end
+
+  it 'give trade url' do
+    trade_page_url = client.trade_page_url market, base: ltc_btc_pair.base, target: ltc_btc_pair.target
+    expect(trade_page_url).to eq "https://www.coinexchange.io/market/#{ltc_btc_pair.base}/#{ltc_btc_pair.target}"
   end
 
   it 'fetch ticker' do
@@ -26,7 +32,7 @@ RSpec.describe 'CoinExchange integration specs' do
     expect(ticker.high).to be_a Numeric
     expect(ticker.volume).to be_a Numeric
     expect(ticker.timestamp).to be nil
-    
+
     expect(ticker.payload).to_not be nil
   end
 end
