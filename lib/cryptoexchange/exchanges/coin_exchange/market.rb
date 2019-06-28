5f4f182cc1bd55686496c1fc63e5f9ef3afa707b
Jack Wong <jackyhwong91@gmail.com>
2019-06-28 12:29:23 +0800

    add trade_page_url coinexchange

--- lib/cryptoexchange/exchanges/coin_exchange/market.rb
+++ lib/cryptoexchange/exchanges/coin_exchange/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'coin_exchange'
       API_URL = 'https://www.coinexchange.io/api/v1'
+
+      def self.trade_page_url(args = {})
+        "https://www.coinexchange.io/market/#{args[:base]}/#{args[:target]}"
+      end
     end
   end
 end
