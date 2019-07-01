30479b5c4e44cd73b0103a16c89452ea43e9e327
elvis <yinchuang94@gmail.com>
2019-07-01 13:00:00 +0800

    reverse base target pair

--- lib/cryptoexchange/exchanges/synthetix/services/pairs.rb
+++ lib/cryptoexchange/exchanges/synthetix/services/pairs.rb
@@ -12,7 +12,7 @@ module Cryptoexchange::Exchanges
         def adapt(output)
           market_pairs = []
           output.each do |pair|
-              base, target = pair.split('-')
+              target, base = pair.split('-')
               market_pairs << Cryptoexchange::Models::MarketPair.new(
                                 base: base,
                                 target: target,
