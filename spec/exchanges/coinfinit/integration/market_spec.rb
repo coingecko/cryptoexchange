ca5538e72a0c12f4348bc26f33dbea9ec0ce8ba4
Wenjie Yek <dx3152@hotmail.com>
2019-07-08 14:55:42 +0800

    implement order_book (#1636)
    * implement order_book
    
    * edit readme

--- spec/exchanges/coinfinit/integration/market_spec.rb
+++ spec/exchanges/coinfinit/integration/market_spec.rb
@@ -25,12 +25,27 @@ RSpec.describe 'Coinfinit integration specs' do
     expect(ticker.base).to eq btc_krw_pair.base
     expect(ticker.target).to eq btc_krw_pair.target
     expect(ticker.market).to eq btc_krw_pair.market
-    expect(ticker.high).to be_a Numeric
-    expect(ticker.low).to be_a Numeric
+    expect(ticker.ask).to be_a Numeric
+    expect(ticker.bid).to be_a Numeric
     expect(ticker.last).to be_a Numeric
     expect(ticker.volume).to be_a Numeric
     expect(ticker.change).to be_a Numeric    
     expect(ticker.timestamp).to be nil
     expect(ticker.payload).to_not be nil
+  end
+
+  it 'fetch order book' do
+    order_book = client.order_book(btc_krw_pair)
+
+    expect(order_book.base).to eq btc_krw_pair.base
+    expect(order_book.target).to eq btc_krw_pair.target
+    expect(order_book.market).to eq btc_krw_pair.market
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to be_nil
+    expect(order_book.timestamp).to be_nil
+    expect(order_book.payload).to_not be nil
   end
 end
