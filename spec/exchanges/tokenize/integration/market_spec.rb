b4a67a2151490be0981b6a4d48c5efca3ff70834
Wenjie Yek <dx3152@hotmail.com>
2019-07-18 16:22:38 +0800

    add tokenize orderbook (#1656)

--- spec/exchanges/tokenize/integration/market_spec.rb
+++ spec/exchanges/tokenize/integration/market_spec.rb
@@ -35,5 +35,21 @@ RSpec.describe 'Tokenize integration specs' do
   it 'give trade url' do
     trade_page_url = client.trade_page_url 'tokenize', base: btc_eth_pair.base, target: btc_eth_pair.target
     expect(trade_page_url).to eq "https://tokenize.exchange/market/BTC-ETH"
-  end  
+  end
+
+it 'fetch order book' do
+    order_book = client.order_book(btc_eth_pair)
+
+    expect(order_book.base).to eq 'ETH'
+    expect(order_book.target).to eq 'BTC'
+    expect(order_book.market).to eq 'tokenize'
+
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to be_nil
+    expect(order_book.timestamp).to be_nil
+    expect(order_book.payload).to_not be nil
+  end    
 end
