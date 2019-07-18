183ca582d914864d2f2d70f466315686188568e9
TM Lee <tmlee.ltm@gmail.com>
2019-07-18 12:18:10 +0800

    Fix FTX pair input

--- lib/cryptoexchange/exchanges/ftx/services/market.rb
+++ lib/cryptoexchange/exchanges/ftx/services/market.rb
@@ -14,7 +14,7 @@ module Cryptoexchange::Exchanges
         end
 
         def ticker_url(market_pair)
-          "#{Cryptoexchange::Exchanges::Ftx::Market::API_URL}/futures/#{market_pair.inst_id}"
+          "#{Cryptoexchange::Exchanges::Ftx::Market::API_URL}/futures/#{market_pair.base}-PERP"
         end
 
         def adapt(output, market_pair)
