76211da053c55c37f5cdb97a56203772eeb1d985
wenjieyek <dx3152@hotmail.com>
2019-07-18 12:26:08 +0800

    implement bitz orderbook

--- spec/exchanges/bit_z/integration/market_spec.rb
+++ spec/exchanges/bit_z/integration/market_spec.rb
@@ -35,4 +35,19 @@ RSpec.describe 'BitZ integration specs' do
     expect(ticker.timestamp).to be nil
     expect(ticker.payload).to_not be nil
   end
+
+  it 'fetch order book' do
+    order_book = client.order_book(ltc_btc_pair)
+
+    expect(order_book.base).to eq 'LTC'
+    expect(order_book.target).to eq 'BTC'
+    expect(order_book.market).to eq 'bit_z'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to be_nil
+    expect(order_book.timestamp).to be nil
+    expect(order_book.payload).to_not be nil
+  end  
 end
