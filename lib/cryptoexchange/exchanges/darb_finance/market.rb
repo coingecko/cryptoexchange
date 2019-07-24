549927e9708be4473f97f6f1030943869450902a
Wenjie Yek <dx3152@hotmail.com>
2019-07-24 17:09:37 +0800

    add trade url (#1667)

--- lib/cryptoexchange/exchanges/darb_finance/market.rb
+++ lib/cryptoexchange/exchanges/darb_finance/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'darb_finance'
       API_URL = 'https://api.darbfinance.com/api/v1'
+
+      def self.trade_page_url(args={})
+        "https://base.darbfinance.com/trade/#{args[:base].upcase}-#{args[:target].upcase}"
+      end      
     end
   end
 end
