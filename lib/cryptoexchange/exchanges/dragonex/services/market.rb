13d1eff8d0618af068280c37a789cec021573e90
Jack Wong <jackyhwong91@gmail.com>
2019-07-24 14:25:08 +0800

    fix dragonex volume (#1664)

--- lib/cryptoexchange/exchanges/dragonex/services/market.rb
+++ lib/cryptoexchange/exchanges/dragonex/services/market.rb
@@ -3,10 +3,6 @@ module Cryptoexchange::Exchanges
     module Services
       class Market < Cryptoexchange::Services::Market
         # meaning of params please see: https://github.com/Dragonexio/OpenApi/blob/master/docs/English/1.interface_document_v1.md
-        # Query K line part
-        SEARCH_TIMESTAMP = 0
-        COUNT            = 1
-        KLINE_TYPE       = 1
 
         class << self
           def supports_individual_ticker_query?
@@ -31,19 +27,19 @@ module Cryptoexchange::Exchanges
         end
 
         def ticker_url(market_pair)
-          "#{Cryptoexchange::Exchanges::Dragonex::Market::API_URL}/api/v1/market/kline/?symbol_id=#{market_pair.inst_id}&kline_type=#{KLINE_TYPE}&st=#{SEARCH_TIMESTAMP}&count=#{COUNT}"
+          "#{Cryptoexchange::Exchanges::Dragonex::Market::API_URL}/api/v1/market/real?symbol_id=#{market_pair.inst_id}"
         end
 
         def adapt(output, market_pair)
-          market           = HashHelper.dig(output, 'data', 'lists').first
+          market           = HashHelper.dig(output, 'data').first
           ticker           = Cryptoexchange::Models::Ticker.new
           ticker.base      = market_pair.base
           ticker.target    = market_pair.target
           ticker.market    = Dragonex::Market::NAME
-          ticker.high      = NumericHelper.to_d(market[2])
-          ticker.low       = NumericHelper.to_d(market[3])
-          ticker.last      = NumericHelper.to_d(market[1])
-          ticker.volume    = NumericHelper.to_d(market[-1])
+          ticker.high      = NumericHelper.to_d(market['max_price'])
+          ticker.low       = NumericHelper.to_d(market['min_price'])
+          ticker.last      = NumericHelper.to_d(market['close_price'])
+          ticker.volume    = NumericHelper.to_d(market['total_volume'])
           ticker.timestamp = nil
           ticker.payload   = output
           ticker
