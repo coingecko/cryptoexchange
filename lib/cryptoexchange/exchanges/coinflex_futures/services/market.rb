1fc7b514d790d82d2dd600648071b66912661f57
Wenjie Yek <dx3152@hotmail.com>
2019-07-26 15:27:14 +0800

    add coinflex futures inst id (#1672)

--- lib/cryptoexchange/exchanges/coinflex_futures/services/market.rb
+++ lib/cryptoexchange/exchanges/coinflex_futures/services/market.rb
@@ -9,6 +9,7 @@ module Cryptoexchange::Exchanges
         end
 
         def fetch(market_pair)
+          market_pair = Cryptoexchange::Exchanges::CoinflexFutures::Market.assign_inst_id(market_pair) if market_pair.inst_id.nil?
           output = super(ticker_url(market_pair))
           adapt(output, market_pair)
         end
