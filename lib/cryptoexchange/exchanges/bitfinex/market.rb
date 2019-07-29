e06a6115aa5eb3d116bed9941cc91e576b353ff3
wenjieyek <dx3152@hotmail.com>
2019-07-29 11:53:13 +0800

    update bitfinex api endpoint

--- lib/cryptoexchange/exchanges/bitfinex/market.rb
+++ lib/cryptoexchange/exchanges/bitfinex/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Bitfinex
     class Market < Cryptoexchange::Models::Market
       NAME = 'bitfinex'
-      API_URL = 'https://api.bitfinex.com/v1'
+      API_URL = 'https://api.bitfinex.com/v2'
       WS_URL = 'wss://api.bitfinex.com/ws/'
     end
   end
--- lib/cryptoexchange/exchanges/bitfinex/services/pairs.rb
+++ lib/cryptoexchange/exchanges/bitfinex/services/pairs.rb
@@ -2,8 +2,7 @@ module Cryptoexchange::Exchanges
   module Bitfinex
     module Services
       class Pairs < Cryptoexchange::Services::Pairs
-        PAIRS_URL = "#{Cryptoexchange::Exchanges::Bitfinex::Market::API_URL}/symbols"
-
+        PAIRS_URL = "#{Cryptoexchange::Exchanges::Bitfinex::Market::API_URL}/tickers?symbols=ALL"
 
         def fetch
           output = super
@@ -12,18 +11,23 @@ module Cryptoexchange::Exchanges
 
         def adapt(output)
           output.map do |pair|
+            pair = pair[0]
+
+            next if pair[0] != 't'
+
             if pair.include? ":"
               base, target = pair.split(":")
             else
-              base = pair[0..pair.length - 4]
+              base = pair[1..pair.length - 4]
               target = pair[-3..-1]
             end
+
             Cryptoexchange::Models::MarketPair.new(
               base: base,
               target: target,
               market: Bitfinex::Market::NAME
             )
-          end
+          end.compact
         end
       end
     end
