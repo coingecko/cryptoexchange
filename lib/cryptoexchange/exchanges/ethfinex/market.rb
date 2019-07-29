f244c157831bf36254b59e32cfe4e2041f68111e
Jack Wong <jackyhwong91@gmail.com>
2019-07-29 11:56:56 +0800

    update ethfinex endpoint (v2) (#1674)
    * update ethfinex endpoint (v2)
    
    * add compact for nil pairs

--- lib/cryptoexchange/exchanges/ethfinex/market.rb
+++ lib/cryptoexchange/exchanges/ethfinex/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Ethfinex
     class Market < Cryptoexchange::Models::Market
       NAME = 'ethfinex'
-      API_URL = 'https://api.ethfinex.com'
+      API_URL = 'https://api-pub.ethfinex.com'
 
       def self.trade_page_url(args={})
         "https://ethfinex.com"
--- lib/cryptoexchange/exchanges/ethfinex/services/pairs.rb
+++ lib/cryptoexchange/exchanges/ethfinex/services/pairs.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Ethfinex
     module Services
       class Pairs < Cryptoexchange::Services::Pairs
-        PAIRS_URL = "#{Cryptoexchange::Exchanges::Ethfinex::Market::API_URL}/v1/symbols"
+        PAIRS_URL = "#{Cryptoexchange::Exchanges::Ethfinex::Market::API_URL}/v2/tickers?symbols=ALL"
 
         def fetch
           output = super
@@ -11,18 +11,23 @@ module Cryptoexchange::Exchanges
 
         def adapt(output)
           output.map do |pair|
+            pair = pair[0]
+
+            next if pair[0] != 't'
+
             if pair.include? ":"
               base, target = pair.split(":")
             else
-              base = pair[0..pair.length - 4]
+              base = pair[1..pair.length - 4]
               target = pair[-3..-1]
             end
+
             Cryptoexchange::Models::MarketPair.new(
               base: base,
               target: target,
               market: Ethfinex::Market::NAME
             )
-          end
+          end.compact
         end
       end
     end
