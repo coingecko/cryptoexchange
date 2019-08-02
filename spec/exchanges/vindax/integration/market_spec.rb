a19d3f7fc3631a6717168fd8461dea578e938b3d
elvis <yinchuang94@gmail.com>
2019-08-02 17:32:38 +0800

    add orderbook for qbtc, probit, vindax, oceanex and tidex

--- spec/exchanges/vindax/integration/market_spec.rb
+++ spec/exchanges/vindax/integration/market_spec.rb
@@ -34,4 +34,22 @@ RSpec.describe 'Vindax integration specs' do
     expect(ticker.timestamp).to be nil
     expect(ticker.payload).to_not be nil
   end
+
+  it 'fetch order book' do
+    order_book = client.order_book(bnb_eth_pair)
+
+    expect(order_book.base).to eq 'BNB'
+    expect(order_book.target).to eq 'ETH'
+    expect(order_book.market).to eq 'vindax'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to be_nil
+    expect(order_book.asks.count).to be > 0
+    expect(order_book.bids.count).to be > 0
+    expect(order_book.timestamp).to be_a Numeric
+    expect(2000..Date.today.year).to include(Time.at(order_book.timestamp).year)
+    expect(order_book.payload).to_not be nil
+  end
 end
