1b92f2c03b1261b0425ffbfe6700cb5d48cb6cea
elvis <yinchuang94@gmail.com>
2019-08-06 13:04:35 +0800

    fix spec

--- spec/exchanges/citex/market_spec.rb
+++ spec/exchanges/citex/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Citex::Market do
   it { expect(described_class::NAME).to eq 'citex' }
-  it { expect(described_class::API_URL).to eq 'https://api.citex.co.kr/v1/markets/common/ticker' }
+  it { expect(described_class::API_URL).to eq 'https://open.citex.co.kr/api/v1/common' }
 end
