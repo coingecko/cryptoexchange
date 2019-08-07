a8ff623db5a64f7c26841cadb824f2e0e47f1320
Alterdice <43209450+Alterdice@users.noreply.github.com>
2019-08-07 09:39:34 +0300

    Implement order book and trades (#1659)

--- spec/exchanges/dex-trade/integration/market_spec.rb
+++ spec/exchanges/dex-trade/integration/market_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe 'Dextrade integration specs' do
   client = Cryptoexchange::Client.new
-  let(:btc_adc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'btc', target: 'usd', market: 'Dextrade') }
+  let(:btc_usd_pair) { Cryptoexchange::Models::MarketPair.new(base: 'btc', target: 'usd', market: 'Dextrade') }
 
   it 'fetch pairs' do
     pairs = client.pairs('Dextrade')
@@ -15,7 +15,7 @@ RSpec.describe 'Dextrade integration specs' do
   end
 
   it 'fetch ticker' do
-    ticker = client.ticker(btc_adc_pair)
+    ticker = client.ticker(btc_usd_pair)
 
     expect(ticker.base).to eq 'BTC'
     expect(ticker.target).to eq 'USD'
@@ -27,5 +27,39 @@ RSpec.describe 'Dextrade integration specs' do
     expect(ticker.volume).to be_a Numeric
     expect(ticker.timestamp).to be nil
     expect(ticker.payload).to_not be nil
+  end
+
+  it 'fetch order book' do
+    order_book = client.order_book(btc_usd_pair)
+
+    expect(order_book.base).to eq 'BTC'
+    expect(order_book.target).to eq 'USD'
+    expect(order_book.market).to eq 'Dextrade'
+
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to be_nil
+    expect(order_book.asks.count).to be > 4
+    expect(order_book.bids.count).to be > 4
+    expect(order_book.timestamp).to be_nil
+    expect(order_book.payload).to_not be nil
+  end
+
+  it 'fetch trade' do
+    trades = client.trades(btc_usd_pair)
+    trade = trades.sample
+
+    expect(trades).to_not be_empty
+    expect(trade.base).to eq 'BTC'
+    expect(trade.target).to eq 'USD'
+    expect(trade.market).to eq 'Dextrade'
+
+    expect(['BUY', 'SELL']).to include trade.type
+    expect(trade.price).to_not be_nil
+    expect(trade.amount).to_not be_nil
+    expect(trade.timestamp).to be_a Numeric
+    expect(trade.payload).to_not be nil
   end
 end
