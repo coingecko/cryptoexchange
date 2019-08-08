2d7dd285cb6a102725c923dd7682313179bf6fd1
elvis <yinchuang94@gmail.com>
2019-08-08 17:05:44 +0800

    add GDAC orderbook

--- lib/cryptoexchange/exchanges/gdac/market.rb
+++ lib/cryptoexchange/exchanges/gdac/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME    = 'gdac'
       API_URL = 'https://marketapi.gdac.co.kr/v1'
+
+      def self.trade_page_url(args={})
+        "https://www.gdac.com/exchange/#{args[:base]}/#{args[:target]}"
+      end
     end
   end
 end
