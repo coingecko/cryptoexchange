2ee6754ce1d0e929c57b7e0ff5033cd72e60818d
elvis <yinchuang94@gmail.com>
2019-08-09 16:20:16 +0800

    orderbook for wazirx, mercadorbitcoin, coinexchange

--- lib/cryptoexchange/exchanges/mercado_bitcoin/services/order_book.rb
+++ lib/cryptoexchange/exchanges/mercado_bitcoin/services/order_book.rb
@@ -1,5 +1,5 @@
 module Cryptoexchange::Exchanges
-  module Wazirx
+  module MercadoBitcoin
     module Services
       class OrderBook < Cryptoexchange::Services::Market
         class << self
@@ -14,7 +14,7 @@ module Cryptoexchange::Exchanges
         end
 
         def ticker_url(market_pair)
-          "#{Cryptoexchange::Exchanges::Wazirx::Market::API_URL}/depth?market=#{market_pair.base.downcase}#{market_pair.target.downcase}"
+          "#{Cryptoexchange::Exchanges::MercadoBitcoin::Market::API_URL}/#{market_pair.base}/orderbook/"
         end
 
         def adapt(output, market_pair)
@@ -22,18 +22,20 @@ module Cryptoexchange::Exchanges
 
           order_book.base      = market_pair.base
           order_book.target    = market_pair.target
-          order_book.market    = Wazirx::Market::NAME
-          order_book.asks      = adapt_orders(output['asks'])
-          order_book.bids      = adapt_orders(output['bids'])
-          order_book.timestamp = output['timestamp']
+          order_book.market    = MercadoBitcoin::Market::NAME
+
+          order_book.asks      = adapt_orders output['asks']
+          order_book.bids      = adapt_orders output['bids']
+          order_book.timestamp = nil
           order_book.payload   = output
           order_book
         end
 
         def adapt_orders(orders)
           orders.collect do |order_entry|
-            Cryptoexchange::Models::Order.new(price: order_entry[0],
-                                              amount: order_entry[1])
+            Cryptoexchange::Models::Order.new(price: order_entry[0].to_f,
+                                              amount: order_entry[1].to_f,
+                                              timestamp: nil)
           end
         end
       end
--- spec/exchanges/mercado_bitcoin/integration/market_spec.rb
+++ spec/exchanges/mercado_bitcoin/integration/market_spec.rb
@@ -29,4 +29,22 @@ RSpec.describe 'MercadoBitcoin integration specs' do
     expect(ticker.timestamp).to be nil
     expect(ticker.payload).to_not be nil
   end
+
+  it 'fetch order book' do
+    order_book = client.order_book(btc_brl_pair)
+
+    expect(order_book.base).to eq 'BTC'
+    expect(order_book.target).to eq 'BRL'
+    expect(order_book.market).to eq 'mercado_bitcoin'
+
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to be_nil
+    expect(order_book.asks.count).to be >= 1
+    expect(order_book.bids.count).to be >= 1
+    expect(order_book.timestamp).to be_nil
+    expect(order_book.payload).to_not be nil
+  end
 end
