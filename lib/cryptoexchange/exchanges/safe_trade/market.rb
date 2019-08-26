889da3b11d90eb7123d88682aa9c594e85c5d4d6
streetcrypto7 <streetcrypto7@protonmail.com>
2019-08-26 15:32:08 +0900

    Add SafeTrade exchange trading page (#1715)

--- lib/cryptoexchange/exchanges/safe_trade/market.rb
+++ lib/cryptoexchange/exchanges/safe_trade/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME    = 'safe_trade'
       API_URL = 'https://safe.trade/api'
+      
+      def self.trade_page_url(args={})
+        "https://safe.trade/trading/#{args[:base]}#{args[:target]}"
+      end
     end
   end
 end
