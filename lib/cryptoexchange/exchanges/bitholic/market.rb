40ef065912a36aaa1a642fa24b9209216173de40
TM Lee <tmlee.ltm@gmail.com>
2019-08-28 17:42:03 +0800

    Update bitholic url path to bitthumbsg

--- lib/cryptoexchange/exchanges/bitholic/market.rb
+++ lib/cryptoexchange/exchanges/bitholic/market.rb
@@ -5,7 +5,7 @@ module Cryptoexchange::Exchanges
       API_URL = 'https://api.bitholic.com/public'
 
       def self.trade_page_url(args={})
-        "https://www.bitholic.com/exchange/#{args[:base]}-#{args[:target]}"
+        "https://www.bithumbsg.com/exchange/#{args[:base]}-#{args[:target]}"
       end
     end
   end
