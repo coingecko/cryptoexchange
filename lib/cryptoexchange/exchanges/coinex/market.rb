afc5565400ab97392339087d55a9fd78d8281f79
Jack Wong <jackyhwong91@gmail.com>
2019-09-11 11:55:14 +0800

    add CET as target separator in CoinEx

--- lib/cryptoexchange/exchanges/coinex/market.rb
+++ lib/cryptoexchange/exchanges/coinex/market.rb
@@ -3,7 +3,7 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'coinex'
       API_URL = 'https://api.coinex.com/v1'
-      SEPARATOR_REGEX = /(USDT|BTC|BCH|ETH|USDC|GUSD|PAX|TUSD)\z/
+      SEPARATOR_REGEX = /(USDT|BTC|BCH|ETH|USDC|GUSD|PAX|TUSD|CET)\z/
 
       def self.trade_page_url(args={})
         "https://www.coinex.com/trading?currency=#{args[:target]}&dest=#{args[:base]}#limit"
