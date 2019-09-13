dfe2a3087d600a16bc58a06d43918e73dce0bd80
elvis <yinchuang94@gmail.com>
2019-09-14 03:52:21 +0800

    add ftx's contract_stat and spec

---
+++ lib/cryptoexchange/exchanges/ftx/services/contract_stat.rb
@@ -0,0 +1,39 @@
+module Cryptoexchange::Exchanges
+  module Ftx
+    module Services
+      class ContractStat < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            true
+          end
+        end
+
+        def fetch(market_pair)
+          open_interest = super(open_interest_url(market_pair))
+          index = super(index_url(market_pair))
+
+          adapt(open_interest, index, market_pair)
+        end
+
+        def open_interest_url(market_pair)
+          "#{Cryptoexchange::Exchanges::Ftx::Market::API_URL}/futures/#{market_pair.inst_id}/stats"
+        end
+
+        def index_url(market_pair)
+          "#{Cryptoexchange::Exchanges::Ftx::Market::API_URL}/futures/#{market_pair.inst_id}"
+        end
+
+        def adapt(open_interest, index, market_pair)
+          contract_stat = Cryptoexchange::Models::ContractStat.new
+          contract_stat.base      = market_pair.base
+          contract_stat.target    = market_pair.target
+          contract_stat.market    = Ftx::Market::NAME
+          contract_stat.open_interest = open_interest['result']['openInterest'].to_f
+          contract_stat.index     = index['result']['index'].to_f
+          contract_stat.payload   = { "open_interest" => open_interest, "index" => index }
+          contract_stat
+        end
+      end
+    end
+  end
+end
