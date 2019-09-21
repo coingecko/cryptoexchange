2de5fe9b9edaad6ebbeae27ea63f6d8d3e0a4007
Brian Gaston <brian@mandalaex.com>
2019-09-21 18:38:54 -0400

    test fix

--- spec/exchanges/mandala/market_spec.rb
+++ spec/exchanges/mandala/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Mandala::Market do
   it { expect(described_class::NAME).to eq 'mandala' }
-  it { expect(described_class::API_URL).to eq 'https://zapi.mandalaex.com/api' }
+  it { expect(described_class::API_URL).to eq 'https://zapi.mandalaex.com' }
 end
