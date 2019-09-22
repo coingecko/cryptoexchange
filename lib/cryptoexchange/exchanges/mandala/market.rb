d9ba26ddd0697ebfb5931daccd9c92a7911599ea
Brian Gaston <btgaston@yahoo.com>
2019-09-22 17:26:45 -0400

    wip

--- lib/cryptoexchange/exchanges/mandala/market.rb
+++ lib/cryptoexchange/exchanges/mandala/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'mandala'
       API_URL = 'https://zapi.mandalaex.com'
+
+      def self.trade_page_url(args={})
+      	"https://trade.mandalaex.com/trade/#{args[:base]}-#{args[:target]}"
+      end
     end
   end
 end
