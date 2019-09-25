44a347324648df1c1ed0c5af66b078d73ec6ee2b
elvis <yinchuang94@gmail.com>
2019-09-25 16:54:29 +0800

    fix trade  urls

--- spec/exchanges/bybit/integration/market_spec.rb
+++ spec/exchanges/bybit/integration/market_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe 'Bybit integration specs' do
   end
 
   it 'give trade url' do
-    trade_page_url = client.trade_page_url 'bybit', base: btc_usd_pair.base, target: btc_usd_pair.target
+    trade_page_url = client.trade_page_url 'bybit', base: btc_usd_pair.base, target: btc_usd_pair.target, inst_id: btc_usd_pair.inst_id
     expect(trade_page_url).to eq "https://www.bybit.com/app/exchange/BTCUSD"
   end
 
