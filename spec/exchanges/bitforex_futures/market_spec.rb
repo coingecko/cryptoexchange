295842534e7a8f22cfd8cec345b06d929172ee9c
TM Lee <tmlee.ltm@gmail.com>
2019-09-25 18:06:40 +0800

    [#Derivatives] Add inst_id and contract stat to bitforex_futures

--- spec/exchanges/bitforex_futures/market_spec.rb
+++ spec/exchanges/bitforex_futures/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::BitforexFutures::Market do
   it { expect(described_class::NAME).to eq 'bitforex_futures' }
-  it { expect(described_class::API_URL).to eq 'https://bitforex.com/contract/mkapi' }
+  it { expect(described_class::API_URL).to eq 'https://www.bitforex.com/contract/mkapi/v2/' }
 end
