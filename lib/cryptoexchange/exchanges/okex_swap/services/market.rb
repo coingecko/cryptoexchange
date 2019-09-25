ff154ba1094a8c856c01cbe32ff376f425abea38
TM Lee <tmlee.ltm@gmail.com>
2019-09-25 18:54:06 +0800

    [#Derivatives] Add inst_id and fix contract stat for OKEX Swaps

--- lib/cryptoexchange/exchanges/okex_swap/services/market.rb
+++ lib/cryptoexchange/exchanges/okex_swap/services/market.rb
@@ -4,36 +4,17 @@ module Cryptoexchange::Exchanges
       class Market < Cryptoexchange::Services::Market
         class << self
           def supports_individual_ticker_query?
-            false
+            true
           end
         end
 
-        def fetch
-          output_swaps = super(ticker_url)
-          swaps = adapt_all(output_swaps)
-
-          output_futures_pairs = super("https://www.okex.com/api/futures/v3/instruments") # For mapping the interval via instrument_id, since ticker does not return instrument_id
-          output_futures = super("https://www.okex.com/api/futures/v3/instruments/ticker")
-          futures = adapt_all_futures(output_futures, output_futures_pairs)
-
-          swaps + futures
-        end
-
-        def ticker_url
-          "#{Cryptoexchange::Exchanges::OkexSwap::Market::API_URL}/instruments/ticker"
-        end
-
-        def adapt_all(output)
-          output.map do |ticker|
-            base, target, expire_at = ticker['instrument_id'].split('-')
-            market_pair = Cryptoexchange::Models::MarketPair.new(
-              base: base,
-              target: target,
-              market: OkexSwap::Market::NAME,
-              contract_interval: "perpetual",
-            )
-            adapt(ticker, market_pair)
+        def fetch(market_pair)
+          output = if market_pair.contract_interval == "perpetual"
+            super("https://www.okex.com/api/swap/v3/instruments/#{market_pair.inst_id}/ticker")
+          else
+            super("https://www.okex.com/api/futures/v3/instruments/#{market_pair.inst_id}/ticker")
           end
+          adapt(output, market_pair)
         end
 
         def get_volume_in_target(volume, market_pair)
@@ -51,58 +32,16 @@ module Cryptoexchange::Exchanges
           ticker.base      = market_pair.base
           ticker.target    = market_pair.target
           ticker.market    = OkexSwap::Market::NAME
-          ticker.ask       = NumericHelper.to_d(output['best_ask'])
-          ticker.bid       = NumericHelper.to_d(output['best_bid'])
-          ticker.last      = NumericHelper.to_d(output['last'])
-          ticker.high      = NumericHelper.to_d(output['high_24h'])
-          ticker.low       = NumericHelper.to_d(output['low_24h'])
-          ticker.volume    = NumericHelper.divide(volume_in_target, ticker.last)
-          ticker.timestamp = nil
-          ticker.payload   = output
-          ticker.contract_interval = "perpetual"
-          ticker
-        end
-
-        ### Adapt for Futures data
-
-        def adapt_all_futures(output, pairs)
-          output.map do |ticker|
-            base, target, expire_at = ticker['instrument_id'].split('-')
-
-            pair = pairs.select { |p| p["instrument_id"] == ticker["instrument_id"] }.first
-            interval = if pair["alias"] == "this_week"
-              "weekly"
-            elsif pair["alias"] == "next_week"
-              "biweekly"
-            elsif pair["alias"] == "quarter"
-              "quarterly"
-            end
-
-            market_pair = Cryptoexchange::Models::MarketPair.new(
-              base: base,
-              target: target,
-              market: OkexSwap::Market::NAME,
-              contract_interval: interval,
-            )
-            adapt_futures(ticker, market_pair)
-          end
-        end
-
-        def adapt_futures(output, market_pair)
-          volume_in_target = get_volume_in_target(NumericHelper.to_d(output['volume_24h']), market_pair)
-          ticker           = Cryptoexchange::Models::Ticker.new
-          ticker.base      = market_pair.base
-          ticker.target    = market_pair.target
-          ticker.market    = OkexSwap::Market::NAME
-          ticker.ask       = NumericHelper.to_d(output['best_ask'])
-          ticker.bid       = NumericHelper.to_d(output['best_bid'])
-          ticker.last      = NumericHelper.to_d(output['last'])
-          ticker.high      = NumericHelper.to_d(output['high_24h'])
-          ticker.low       = NumericHelper.to_d(output['low_24h'])
-          ticker.volume    = NumericHelper.divide(volume_in_target, ticker.last)
-          ticker.timestamp = nil
-          ticker.payload   = output
+          ticker.inst_id   = market_pair.inst_id
           ticker.contract_interval = market_pair.contract_interval
+          ticker.ask       = NumericHelper.to_d(output['best_ask'])
+          ticker.bid       = NumericHelper.to_d(output['best_bid'])
+          ticker.last      = NumericHelper.to_d(output['last'])
+          ticker.high      = NumericHelper.to_d(output['high_24h'])
+          ticker.low       = NumericHelper.to_d(output['low_24h'])
+          ticker.volume    = NumericHelper.divide(volume_in_target, ticker.last)
+          ticker.timestamp = nil
+          ticker.payload   = output
           ticker
         end
       end
