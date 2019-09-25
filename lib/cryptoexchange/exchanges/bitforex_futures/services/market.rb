a2bba43931ed0c6ac2ef9e1909a367661787bad2
TM Lee <tmlee.ltm@gmail.com>
2019-09-25 18:06:40 +0800

    [#Derivatives] Add inst_id and contract stat to bitforex_futures

--- lib/cryptoexchange/exchanges/bitforex_futures/services/market.rb
+++ lib/cryptoexchange/exchanges/bitforex_futures/services/market.rb
@@ -4,24 +4,39 @@ module Cryptoexchange::Exchanges
       class Market < Cryptoexchange::Services::Market
         class << self
           def supports_individual_ticker_query?
-            true
+            false
           end
         end
 
-        def fetch(market_pair)
-          output = super(ticker_url(market_pair))
-          adapt(output, market_pair)
+        def fetch
+          output = super(ticker_url)
+          adapt_all(output["data"])
         end
 
-        def ticker_url(market_pair)
-          "#{Cryptoexchange::Exchanges::BitforexFutures::Market::API_URL}/getH24Ticker?businessType=swap-#{market_pair.target.downcase}-#{market_pair.base.downcase}"
+        def ticker_url
+          "#{Cryptoexchange::Exchanges::BitforexFutures::Market::API_URL}/tickers"
+        end
+
+        def adapt_all(output)
+          output.map do |inst_id, ticker|
+            type, target, base = inst_id.split('-')
+            market_pair = Cryptoexchange::Models::MarketPair.new(
+                            base: base.upcase,
+                            target: target.upcase,
+                            inst_id: inst_id,
+                            contract_interval: 'perpetual',
+                            market: GateFutures::Market::NAME
+                          )
+            adapt(ticker, market_pair)
+          end.compact
         end
 
         def adapt(output, market_pair)
-          output           = output["data"]
           ticker           = Cryptoexchange::Models::Ticker.new
           ticker.base      = market_pair.base
           ticker.target    = market_pair.target
+          ticker.inst_id   = market_pair.inst_id
+          ticker.contract_interval = market_pair.contract_interval
           ticker.market    = BitforexFutures::Market::NAME
           ticker.last      = NumericHelper.to_d(output['last'])
           ticker.volume    = NumericHelper.divide(NumericHelper.to_d(output['productvol']), ticker.last)
