0fad3dc685e019e803f5661b58d201ae3544147f
wenjieyek <dx3152@hotmail.com>
2019-09-25 18:20:21 +0800

    use inst_id in cme futures

--- spec/exchanges/cme_futures/integration/market_spec.rb
+++ spec/exchanges/cme_futures/integration/market_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe 'CME Futures integration specs' do
   let(:client) { Cryptoexchange::Client.new }
-  let(:btc_usd_pair) { Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'USD', market: 'cme_futures', contract_interval: "SEP 2019") }
+  let(:btc_usd_pair) { Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'USD', market: 'cme_futures', contract_interval: "AUG 2019", inst_id: "BTCQ9") }
 
   it 'fetch pairs' do
     pairs = client.pairs('cme_futures')
@@ -11,6 +11,7 @@ RSpec.describe 'CME Futures integration specs' do
     pair = pairs.first
     expect(pair.base).to_not be nil
     expect(pair.target).to_not be nil
+    expect(pair.inst_id).to_not be nil
     expect(pair.market).to eq 'cme_futures'
     expect(pair.contract_interval).to eq "AUG 2019"
   end
@@ -21,12 +22,13 @@ RSpec.describe 'CME Futures integration specs' do
     expect(ticker.base).to eq 'BTC'
     expect(ticker.target).to eq 'USD'
     expect(ticker.market).to eq 'cme_futures'
+    expect(ticker.inst_id).to eq 'BTCQ9'
     expect(ticker.last).to be_a Numeric
     expect(ticker.ask).to be nil
     expect(ticker.bid).to be nil
     expect(ticker.volume).to be_a Numeric
     expect(ticker.timestamp).to be nil
     expect(ticker.payload).to_not be nil
-    expect(ticker.contract_interval).to eq "SEP 2019"
+    expect(ticker.contract_interval).to eq "AUG 2019"
   end
 end
