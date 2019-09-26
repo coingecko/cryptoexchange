a0a98bbed1af17db67b57526cee5ffd18b5946d1
Wenjie Yek <dx3152@hotmail.com>
2019-09-26 14:00:00 +0800

    Binance JEX using inst_id
    * use inst_id in jex_futures
    
    * add jex readme

--- lib/cryptoexchange/exchanges/jex_futures/services/order_book.rb
+++ lib/cryptoexchange/exchanges/jex_futures/services/order_book.rb
@@ -14,7 +14,7 @@ module Cryptoexchange::Exchanges
         end
 
         def ticker_url(market_pair)
-          "#{Cryptoexchange::Exchanges::JexFutures::Market::API_URL}/contract/depth?symbol=#{market_pair.base}#{market_pair.target}"
+          "#{Cryptoexchange::Exchanges::JexFutures::Market::API_URL}/contract/depth?symbol=#{market_pair.inst_id}"
         end
 
         def adapt(output, market_pair)
