b35aabe171f9c78355f1e6403e31c10ab2b87a8e
Wenjie Yek <dx3152@hotmail.com>
2019-10-02 16:31:11 +0800

    Wj fix limit to 20 bitopro orderbook (#1805)
    * fix limit to 20 bitopro orderbook
    
    * update vcr

--- lib/cryptoexchange/exchanges/bitopro/services/order_book.rb
+++ lib/cryptoexchange/exchanges/bitopro/services/order_book.rb
@@ -14,7 +14,7 @@ module Cryptoexchange::Exchanges
         end
 
         def ticker_url(market_pair)
-          "#{Cryptoexchange::Exchanges::Bitopro::Market::API_URL}/order-book/#{market_pair.base}_#{market_pair.target}"
+          "#{Cryptoexchange::Exchanges::Bitopro::Market::API_URL}/order-book/#{market_pair.base}_#{market_pair.target}?limit=20"
         end
 
         def adapt(output, market_pair)
