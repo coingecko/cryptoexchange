e7d6d4e01c6bb5324684e303dc36143bc2ef2d2b
elvis <yinchuang94@gmail.com>
2019-10-03 16:44:12 +0800

    implement delta_futures

--- lib/cryptoexchange/exchanges/delta_futures/services/contract_stat.rb
+++ lib/cryptoexchange/exchanges/delta_futures/services/contract_stat.rb
@@ -1,51 +1,42 @@
-# module Cryptoexchange::Exchanges
-#   module DeltaFutures
-#     module Services
-#       class ContractStat < Cryptoexchange::Services::Market
-#         class << self
-#           def supports_individual_ticker_query?
-#             false
-#           end
-#         end
-#
-#         def fetch
-#           output = super(ticker_url)
-#           adapt_all(output)
-#         end
-#
-#         def ticker_url
-#           "#{Cryptoexchange::Exchanges::DeltaFutures::Market::API_URL}/tickers"
-#         end
-#
-#         def adapt_all(output)
-#           output["result"].map do |pair|
-#             separator = /(BTC|ETH|EOS|XRP|USD)\z/ =~ pair['symbol']
-#             next if separator.nil?
-#
-#             base   = pair['symbol'][0..separator - 1]
-#             target = pair['symbol'][separator..-1]
-#             market_pair = Cryptoexchange::Models::MarketPair.new(
-#               base: base,
-#               target: target,
-#               market: DeltaFutures::Market::NAME
-#             )
-#
-#             adapt(market_pair, pair)
-#           end.compact
-#         end
-#
-#         def adapt(market_pair, pair)
-#           contract_stat = Cryptoexchange::Models::ContractStat.new
-#
-#           contract_stat.base      = market_pair.base
-#           contract_stat.target    = market_pair.target
-#           contract_stat.market    = DeltaFutures::Market::NAME
-#           contract_stat.open_interest = pair['open_interest'].to_f
-#           contract_stat.index     = pair['index_price'].to_f
-#           contract_stat.payload   = pair
-#           contract_stat
-#         end
-#       end
-#     end
-#   end
-# end
+module Cryptoexchange::Exchanges
+  module DeltaFutures
+    module Services
+      class ContractStat < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            true
+          end
+        end
+
+        def fetch(market_pair)
+          output = super(ticker_url(market_pair))
+          adapt(output, market_pair)
+        end
+
+        def ticker_url(market_pair)
+          product_id = get_product_id(market_pair)
+          "#{Cryptoexchange::Exchanges::DeltaFutures::Market::API_URL}/orderbook/#{product_id}/l2"
+        end
+
+        def get_product_id(market_pair)
+          url = "#{Cryptoexchange::Exchanges::DeltaFutures::Market::API_URL}/products"
+          output = HTTP.get(url).parse(:json)
+          pair = output.find { |i| i['symbol'] == market_pair.inst_id }
+          pair['id']
+        end
+
+        def adapt(output, market_pair)
+          contract_stat = Cryptoexchange::Models::ContractStat.new
+
+          contract_stat.base      = market_pair.base
+          contract_stat.target    = market_pair.target
+          contract_stat.market    = DeltaFutures::Market::NAME
+          # contract_stat.funding_rate_percentage = output['funding_rate'].to_f
+          contract_stat.index     = output['mark_price'].to_f
+          contract_stat.payload   = output
+          contract_stat
+        end
+      end
+    end
+  end
+end
---
+++ spec/exchanges/delta_futures/market_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+RSpec.describe Cryptoexchange::Exchanges::BinanceFutures::Market do
+  it { expect(described_class::NAME).to eq 'delta_futures' }
+  it { expect(described_class::API_URL).to eq 'https://api.delta.exchange' }
+end
