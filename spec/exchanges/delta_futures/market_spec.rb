5b007ac83e6ac21f3a56c9201a1c4b1fbb7c123a
elvis <yinchuang94@gmail.com>
2019-10-03 16:50:36 +0800

    fix spec

--- spec/exchanges/delta_futures/market_spec.rb
+++ spec/exchanges/delta_futures/market_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe Cryptoexchange::Exchanges::BinanceFutures::Market do
+RSpec.describe Cryptoexchange::Exchanges::DeltaFutures::Market do
   it { expect(described_class::NAME).to eq 'delta_futures' }
   it { expect(described_class::API_URL).to eq 'https://api.delta.exchange' }
 end
