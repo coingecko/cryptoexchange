31c2c252551c15f32fc0656f0d1227b44f475fca
Wenjie Yek <dx3152@hotmail.com>
2019-10-08 15:34:20 +0800

    fix bitexlive endpoint (#1817)

--- lib/cryptoexchange/exchanges/bitexlive/market.rb
+++ lib/cryptoexchange/exchanges/bitexlive/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Bitexlive
     class Market < Cryptoexchange::Models::Market
       NAME = 'bitexlive'
-      API_URL = 'https://bitexlive.com/api'
+      API_URL = 'https://bitexlive.com/api/public'
 
       def self.trade_page_url(args={})
         "https://bitexlive.com/exchange/#{args[:target]}-#{args[:base]}"
