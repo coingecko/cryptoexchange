f9cdee8cbb830f1c44f686e7222372b7ed1177a9
wenjieyek <dx3152@hotmail.com>
2019-10-10 11:52:14 +0800

    fix cbx api endpoint

--- lib/cryptoexchange/exchanges/cbx/services/trades.rb
+++ lib/cryptoexchange/exchanges/cbx/services/trades.rb
@@ -8,22 +8,22 @@ module Cryptoexchange::Exchanges
         end
 
         def ticker_url(market_pair)
-          "#{Cryptoexchange::Exchanges::Cbx::Market::API_URL}/markets/#{market_pair.base}-#{market_pair.target}/trades"
+          "#{Cryptoexchange::Exchanges::Cbx::Market::API_URL}/asset_pairs/#{market_pair.base}-#{market_pair.target}/trades"
         end
 
         def adapt(output, market_pair)
-          output["data"]["edges"].collect do |trade|
+          output["data"].collect do |trade|
             tr = Cryptoexchange::Models::Trade.new
-            tr.trade_id  = trade['node']['id']
+            tr.trade_id  = trade['id']
             tr.base      = market_pair.base
             tr.target    = market_pair.target
             tr.market    = Cbx::Market::NAME
 
-            tr.price     = trade['node']['price']
-            tr.amount    = trade['node']['amount']
-            tr.type      = trade['node']['taker_side']
-            tr.timestamp = Time.parse(trade['node']['inserted_at']).to_i
-            tr.payload   = trade['node']
+            tr.price     = trade['price']
+            tr.amount    = trade['amount']
+            tr.type      = trade['taker_side']
+            tr.timestamp = Time.parse(trade['inserted_at']).to_i
+            tr.payload   = trade
             tr
           end
         end
--- spec/exchanges/cbx/market_spec.rb
+++ spec/exchanges/cbx/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Cbx::Market do
   it { expect(Cryptoexchange::Exchanges::Cbx::Market::NAME).to eq 'cbx' }
-  it { expect(Cryptoexchange::Exchanges::Cbx::Market::API_URL).to eq 'https://www.cbx.one/api/v2' }
+  it { expect(Cryptoexchange::Exchanges::Cbx::Market::API_URL).to eq 'https://www.cbx.one/api/v3' }
 end
