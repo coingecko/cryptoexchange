1f6c62366a77c9f510f0d72909f68d42977eda7e
wenjieyek <dx3152@hotmail.com>
2019-10-10 15:04:56 +0800

    edit bigone endpoint

--- spec/exchanges/bigone/market_spec.rb
+++ spec/exchanges/bigone/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Bigone::Market do
   it { expect(described_class::NAME).to eq 'bigone' }
-  it { expect(described_class::API_URL).to eq 'https://big.one/api/v2' }
+  it { expect(described_class::API_URL).to eq 'https://big.one/api/v3' }
 end
