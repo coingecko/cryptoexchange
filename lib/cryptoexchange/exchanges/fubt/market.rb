419f7ed4ff17014a291426f12ee9d37b9440ea53
elvis <yinchuang94@gmail.com>
2019-10-16 17:54:12 +0800

    hotfix fubt access bug

--- lib/cryptoexchange/exchanges/fubt/market.rb
+++ lib/cryptoexchange/exchanges/fubt/market.rb
@@ -3,7 +3,7 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'fubt'
       API_URL = 'https://api.fubt.co/v1'
-      ACCESS_KEY = "8KdBkefrMLSP0EURre4SRTYVDZnWhl3hvzi21A2YxTo%3d"
+      ACCESS_KEY = "koiyqwih5Ll8Fz9KvnK6561H0wctqGEu%2bMR49VIlEnk%3d"
       SEPARATOR_REGEX = /(BTC|ETH|USDT|FBT|)\z/
 
       def self.trade_page_url(args={})
