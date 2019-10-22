a95dcd0f6431b01d17217e5ae04c504ae13bdd77
wenjieyek <dx3152@hotmail.com>
2019-10-22 13:39:01 +0800

    add pair url

--- lib/cryptoexchange/exchanges/raidofinance/market.rb
+++ lib/cryptoexchange/exchanges/raidofinance/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'raidofinance'
       API_URL = 'https://datacenter.raidofinance.eu/v1'
+
+      def self.trade_page_url(args={})
+        "https://raidofinance.eu/exchange/#{args[:base]}#{args[:target]}"
+      end
     end
   end
 end
