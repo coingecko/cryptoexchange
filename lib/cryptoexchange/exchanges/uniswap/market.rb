19a74d7e3638f839a42046e39196ab5db40c53ff
Wenjie Yek <dx3152@hotmail.com>
2019-10-23 11:59:10 +0800

    Wj edit uniswap api (#1837)
    * edit uniswap api
    
    * fix uniswap

--- lib/cryptoexchange/exchanges/uniswap/market.rb
+++ lib/cryptoexchange/exchanges/uniswap/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Uniswap
     class Market < Cryptoexchange::Models::Market
       NAME = 'uniswap'
-      API_URL = "https://api-test-238309.appspot.com/v0/exchanges"
+      API_URL = "https://api.blocklytics.org/uniswap/v1"
 
       def self.api_key
         authentication = Cryptoexchange::Exchanges::Uniswap::Authentication.new(
