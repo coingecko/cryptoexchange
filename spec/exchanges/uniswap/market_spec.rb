a45c83e9191e09d47af23227dcc09b7fef742e06
wenjieyek <dx3152@hotmail.com>
2019-10-23 11:57:25 +0800

    fix uniswap

--- spec/exchanges/uniswap/market_spec.rb
+++ spec/exchanges/uniswap/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Uniswap::Market do
   it { expect(described_class::NAME).to eq 'uniswap' }
-  it { expect(described_class::API_URL).to eq 'https://api-test-238309.appspot.com/v0/exchanges' }
+  it { expect(described_class::API_URL).to eq 'https://api.blocklytics.org/uniswap/v1' }
 end
