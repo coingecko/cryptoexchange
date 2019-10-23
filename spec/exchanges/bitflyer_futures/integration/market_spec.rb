13ddb9fe9d1b81d9bbbb42c31c63503875552b33
TM Lee <tmlee.ltm@gmail.com>
2019-10-24 00:55:56 +0800

    [#hotfix] Bitflyer future inst_id missing in Ticker

--- spec/exchanges/bitflyer_futures/integration/market_spec.rb
+++ spec/exchanges/bitflyer_futures/integration/market_spec.rb
@@ -31,6 +31,7 @@ RSpec.describe 'BitflyerFutures integration specs' do
     expect(ticker.ask).to be_a Numeric
     expect(ticker.bid).to be_a Numeric
     expect(ticker.volume).to be_a Numeric
+    expect(ticker.inst_id).to eq 'FX_BTC_JPY'
     expect(ticker.timestamp).to be nil
 
     expect(ticker.payload).to_not be nil
