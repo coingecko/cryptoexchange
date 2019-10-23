7202c47472c4361016c548d87abb3afec955fb06
TM Lee <tmlee.ltm@gmail.com>
2019-10-23 23:31:34 +0800

    Add contract type support for Delta Exchange

--- lib/cryptoexchange/exchanges/delta_futures/services/contract_stat.rb
+++ lib/cryptoexchange/exchanges/delta_futures/services/contract_stat.rb
@@ -9,23 +9,20 @@ module Cryptoexchange::Exchanges
         end
 
         def fetch(market_pair)
-          output = super(ticker_url(market_pair))
-          adapt(output, market_pair)
+          product = super(products_url).find { |i| i['symbol'] == market_pair.inst_id }
+          output = super(ticker_url(product['id']))
+          adapt(output, product, market_pair)
         end
 
-        def ticker_url(market_pair)
-          product_id = get_product_id(market_pair)
-          "#{Cryptoexchange::Exchanges::DeltaFutures::Market::API_URL}/orderbook/#{product_id}/l2"
+        def ticker_url(id)
+          "#{Cryptoexchange::Exchanges::DeltaFutures::Market::API_URL}/orderbook/#{id}/l2"
         end
 
-        def get_product_id(market_pair)
-          url = "#{Cryptoexchange::Exchanges::DeltaFutures::Market::API_URL}/products"
-          output = HTTP.get(url).parse(:json)
-          pair = output.find { |i| i['symbol'] == market_pair.inst_id }
-          pair['id']
+        def products_url
+          "#{Cryptoexchange::Exchanges::DeltaFutures::Market::API_URL}/products"
         end
 
-        def adapt(output, market_pair)
+        def adapt(output, product, market_pair)
           contract_stat = Cryptoexchange::Models::ContractStat.new
 
           contract_stat.base      = market_pair.base
@@ -33,8 +30,17 @@ module Cryptoexchange::Exchanges
           contract_stat.market    = DeltaFutures::Market::NAME
           contract_stat.funding_rate_percentage = output['funding_rate'].to_f
           contract_stat.index     = output['mark_price'].to_f
+          contract_stat.contract_type = contract_type(product['contract_type'])
           contract_stat.payload   = output
           contract_stat
+        end
+
+        def contract_type(instrument_type)
+          if instrument_type == "perpetual_futures"
+            "perpetual"
+          elsif instrument_type == "futures"
+            "futures"
+          end
         end
       end
     end
--- spec/exchanges/delta_futures/integration/market_spec.rb
+++ spec/exchanges/delta_futures/integration/market_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'Delta Futures integration specs' do
   let(:client) { Cryptoexchange::Client.new }
   let(:market) { 'delta_futures' }
   let(:btc_usd_pair) { Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'USD', market: market, inst_id: 'BTCUSD', contract_interval: "perpetual") }
+  let(:btc_usd_pair_futures) { Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'USD', market: market, inst_id: 'BTCUSD_27Dec', contract_interval: "") }
 
   it 'fetch pairs' do
     pairs = client.pairs(market)
@@ -51,16 +52,28 @@ RSpec.describe 'Delta Futures integration specs' do
     expect(order_book.payload).to_not be nil
   end
 
-  it 'fetch contract stat' do
-    contract_stat = client.contract_stat(btc_usd_pair)
+  context 'fetch contract stat' do
+    it 'fetch contract stat' do
+      contract_stat = client.contract_stat(btc_usd_pair)
 
-    expect(contract_stat.base).to eq 'BTC'
-    expect(contract_stat.target).to eq 'USD'
-    expect(contract_stat.market).to eq 'delta_futures'
-    expect(contract_stat.index).to be_a Numeric
-    expect(contract_stat.funding_rate_percentage).to be_a Numeric
-    expect(contract_stat.timestamp).to be nil
+      expect(contract_stat.base).to eq 'BTC'
+      expect(contract_stat.target).to eq 'USD'
+      expect(contract_stat.market).to eq 'delta_futures'
+      expect(contract_stat.index).to be_a Numeric
+      expect(contract_stat.funding_rate_percentage).to be_a Numeric
+      expect(contract_stat.timestamp).to be nil
 
-    expect(contract_stat.payload).to_not be nil
+      expect(contract_stat.payload).to_not be nil
+    end
+
+    it 'fetch perpetual details' do
+      contract_stat = client.contract_stat(btc_usd_pair)
+      expect(contract_stat.contract_type).to eq 'perpetual'
+    end
+
+    it 'fetch futures details' do
+      contract_stat = client.contract_stat(btc_usd_pair_futures)
+      expect(contract_stat.contract_type).to eq 'futures'
+    end
   end
 end
