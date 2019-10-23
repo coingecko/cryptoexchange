387fa5c9757b34b43b84c37c05291160b34286b2
Wenjie Yek <dx3152@hotmail.com>
2019-10-23 15:51:04 +0800

    Wj fix hpx (#1838)
    * wip
    
    * wip

--- spec/exchanges/hpx/market_spec.rb
+++ spec/exchanges/hpx/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Hpx::Market do
   it { expect(described_class::NAME).to eq 'hpx' }
-  it { expect(described_class::API_URL).to eq 'https://api.hpx.com/data/v2' }
+  it { expect(described_class::API_URL).to eq 'http://api.hpx.world/data/v2' }
 end
