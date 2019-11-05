514b62173c739e48e43918b0a44c63ec615bdd07
Wenjie Yek <dx3152@hotmail.com>
2019-11-05 16:16:29 +0800

    add coss order_book (#1861)

--- spec/exchanges/coss/integration/market_spec.rb
+++ spec/exchanges/coss/integration/market_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe 'Coss integration specs' do
   let(:client) { Cryptoexchange::Client.new }
-  let(:coss_eth_pair) { Cryptoexchange::Models::MarketPair.new(base: 'COSS', target: 'ETH', market: 'coss') }
+  let(:btc_usdt_pair) { Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'USDT', market: 'coss') }
 
   it 'fetch pairs' do
     pairs = client.pairs('coss')
@@ -15,10 +15,10 @@ RSpec.describe 'Coss integration specs' do
   end
 
   it 'fetch ticker' do
-    ticker = client.ticker(coss_eth_pair)
+    ticker = client.ticker(btc_usdt_pair)
 
-    expect(ticker.base).to eq 'COSS'
-    expect(ticker.target).to eq 'ETH'
+    expect(ticker.base).to eq 'BTC'
+    expect(ticker.target).to eq 'USDT'
     expect(ticker.market).to eq 'coss'
     expect(ticker.last).to be_a Numeric
     expect(ticker.bid).to be nil
@@ -32,7 +32,24 @@ RSpec.describe 'Coss integration specs' do
   end
 
   it 'give trade url' do
-    trade_page_url = client.trade_page_url 'coss', base: coss_eth_pair.base, target: coss_eth_pair.target
-    expect(trade_page_url).to eq "https://coss.io/c/trade?s=COSS_ETH"
+    trade_page_url = client.trade_page_url 'coss', base: btc_usdt_pair.base, target: btc_usdt_pair.target
+    expect(trade_page_url).to eq "https://coss.io/c/trade?s=BTC_USDT"
+  end
+
+  it 'fetch order book' do
+    order_book = client.order_book(btc_usdt_pair)
+
+    expect(order_book.base).to eq 'BTC'
+    expect(order_book.target).to eq 'USDT'
+    expect(order_book.market).to eq 'coss'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to be_nil
+    expect(order_book.asks.count).to be > 5
+    expect(order_book.bids.count).to be > 5
+    expect(order_book.timestamp).to be nil
+    expect(order_book.payload).to_not be nil
   end
 end
