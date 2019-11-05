1df9b46c2b7a37deea556fdd0f78864692798da3
Wenjie Yek <dx3152@hotmail.com>
2019-11-05 14:54:10 +0800

    implement indoex (#1858)

---
+++ lib/cryptoexchange/exchanges/indoex/services/order_book.rb
@@ -0,0 +1,41 @@
+module Cryptoexchange::Exchanges
+  module Indoex
+    module Services
+      class OrderBook < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            true
+          end
+        end
+
+        def fetch(market_pair)
+          output = super(ticker_url(market_pair))
+          adapt(output, market_pair)
+        end
+
+        def ticker_url(market_pair)
+          "#{Cryptoexchange::Exchanges::Indoex::Market::API_URL}/depth/#{market_pair.base}_#{market_pair.target}"
+        end
+
+        def adapt(output, market_pair)
+          order_book = Cryptoexchange::Models::OrderBook.new
+          order_book.base      = market_pair.base
+          order_book.target    = market_pair.target
+          order_book.market    = Indoex::Market::NAME
+          order_book.asks      = adapt_orders(output['asks'])
+          order_book.bids      = adapt_orders(output['bids'])
+          order_book.timestamp = nil
+          order_book.payload   = output
+          order_book
+        end
+
+        def adapt_orders(orders)
+          orders.collect do |order_entry|
+            Cryptoexchange::Models::Order.new(price: order_entry["price"],
+                                              amount: order_entry["quantity"])
+          end
+        end
+      end
+    end
+  end
+end
