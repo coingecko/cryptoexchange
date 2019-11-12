70970fa3fc8bfde3d35be32923a44671e9a9985f
wenjieyek <dx3152@hotmail.com>
2019-11-12 14:05:48 +0800

    change zg endpoint

--- lib/cryptoexchange/exchanges/zg/market.rb
+++ lib/cryptoexchange/exchanges/zg/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Zg
     class Market < Cryptoexchange::Models::Market
       NAME = 'zg'
-      API_URL = 'https://www.zg.com/api/v1'
+      API_URL = 'https://api1.zg.com'
 
       def self.trade_page_url(args={})
         "https://www.zg.com/exchange?coin=#{args[:base]}_#{args[:target]}&tab=all"
--- spec/exchanges/zg/market_spec.rb
+++ spec/exchanges/zg/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Zg::Market do
   it { expect(described_class::NAME).to eq 'zg' }
-  it { expect(described_class::API_URL).to eq 'https://www.zg.com/api/v1' }
+  it { expect(described_class::API_URL).to eq 'https://api1.zg.com' }
 end
