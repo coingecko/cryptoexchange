e2fd4cd7d40f43f1a6002b22c3ec3a3d9abff32a
Jack Wong <jackyhwong91@gmail.com>
2019-11-14 16:50:21 +0800

    update api endpoint (existing API no longer working) (#1874)

--- lib/cryptoexchange/exchanges/vebitcoin/market.rb
+++ lib/cryptoexchange/exchanges/vebitcoin/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Vebitcoin
     class Market < Cryptoexchange::Models::Market
       NAME = 'vebitcoin'
-      API_URL = 'https://www.vebitcoin.com'
+      API_URL = 'https://us-central1-vebitcoin-market.cloudfunctions.net'
     end
   end
 end
