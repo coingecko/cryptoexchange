0d4d967c4b76056d969faefba576df5a5dd70236
JoaoCampos89 <jcampos893@gmail.com>
2019-11-13 22:14:35 -0300

    change backend url

--- lib/cryptoexchange/exchanges/veridex/market.rb
+++ lib/cryptoexchange/exchanges/veridex/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Veridex
     class Market < Cryptoexchange::Models::Market
       NAME = 'veridex'
-      API_URL = 'https://veridex.herokuapp.com/v2'
+      API_URL = 'https://dex-backend.verisafe.io/v2'
 
       def self.trade_page_url(args={})
         "https://dex.verisafe.io/#/erc20/?base=#{args[:base]}&quote=#{args[:target]}"
