9b32e30cc40da9b30324609e6bfc02187915072a
elvis <yinchuang94@gmail.com>
2019-11-15 17:50:17 +0800

    WIP: btse_futures index

--- spec/exchanges/btse_futures/integration/market_spec.rb
+++ spec/exchanges/btse_futures/integration/market_spec.rb
@@ -80,11 +80,11 @@ RSpec.describe 'btse_futures integration specs' do
       expect(contract_stat.base).to eq 'BTC'
       expect(contract_stat.target).to eq 'USD'
       expect(contract_stat.market).to eq 'btse_futures'
-      expect(contract_stat.index).to be nil
+      expect(contract_stat.index).to be_a Numeric
       expect(contract_stat.open_interest).to be_a Numeric
       expect(contract_stat.timestamp).to be nil
-      expect(contract_stat.index_identifier).to eq 'BtseFutures-BTC'
-      expect(contract_stat.index_name).to eq 'BTSE BTC'
+      expect(contract_stat.index_identifier).to eq 'BTCZ19'
+      expect(contract_stat.index_name).to eq 'BTCZ19'
 
       expect(contract_stat.payload).to_not be nil
     end
