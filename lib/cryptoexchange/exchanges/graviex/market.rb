97c792a67e9f149c4875e6dff0a75423ae9873f1
Jack Wong <jackyhwong91@gmail.com>
2019-11-18 16:50:04 +0800

    add market_url for graviex (#1880)

--- lib/cryptoexchange/exchanges/graviex/market.rb
+++ lib/cryptoexchange/exchanges/graviex/market.rb
@@ -3,7 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'graviex'
       API_URL = 'https://graviex.net/api/v2'
+
+      def self.trade_page_url(args={})
+        "https://graviex.net/markets/#{args[:base].downcase}#{args[:target].downcase}"
+      end
     end
   end
 end
-
--- spec/exchanges/graviex/integration/market_spec.rb
+++ spec/exchanges/graviex/integration/market_spec.rb
@@ -1,17 +1,23 @@
 require 'spec_helper'
 
 RSpec.describe 'Graviex integration specs' do
+  let(:market) { 'graviex' }
   let(:client) { Cryptoexchange::Client.new }
   let(:gio_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'GIO', target: 'BTC', market: 'graviex') }
 
   it 'fetch pairs' do
-    pairs = client.pairs('graviex')
+    pairs = client.pairs market
     expect(pairs).not_to be_empty
 
     pair = pairs.first
     expect(pair.base).not_to be_nil
     expect(pair.target).not_to be_nil
-    expect(pair.market).to eq 'graviex'
+    expect(pair.market).to eq market
+  end
+
+  it 'give trade url' do
+    trade_page_url = client.trade_page_url market, base: gio_btc_pair.base, target: gio_btc_pair.target
+    expect(trade_page_url).to eq "https://graviex.net/markets/#{gio_btc_pair.base.downcase}#{gio_btc_pair.target.downcase}"
   end
 
   it 'fetch ticker' do
@@ -19,14 +25,14 @@ RSpec.describe 'Graviex integration specs' do
 
     expect(ticker.base).to eq 'GIO'
     expect(ticker.target).to eq 'BTC'
-    expect(ticker.market).to eq 'graviex'
+    expect(ticker.market).to eq market
     expect(ticker.last).to be_a Numeric
     expect(ticker.bid).to be_a Numeric
     expect(ticker.ask).to be_a Numeric
     expect(ticker.high).to be_a Numeric
     expect(ticker.low).to be_a Numeric
     expect(ticker.volume).to be_a Numeric
-    
+
     expect(ticker.payload).to_not be nil
   end
 
@@ -35,7 +41,7 @@ RSpec.describe 'Graviex integration specs' do
 
     expect(order_book.base).to eq 'GIO'
     expect(order_book.target).to eq 'BTC'
-    expect(order_book.market).to eq 'graviex'
+    expect(order_book.market).to eq market
     expect(order_book.asks).to_not be_empty
     expect(order_book.bids).to_not be_empty
     expect(order_book.asks.first.price).to_not be_nil
@@ -43,5 +49,5 @@ RSpec.describe 'Graviex integration specs' do
     expect(order_book.bids.first.timestamp).to be_nil
     expect(order_book.timestamp).to be_a Numeric
     expect(order_book.payload).to_not be nil
-  end  
+  end
 end
