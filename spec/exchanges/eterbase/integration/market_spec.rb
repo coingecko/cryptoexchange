38159e2ef73d5d1e003dfe0198001bd75ffcf6f8
Wenjie Yek <dx3152@hotmail.com>
2019-11-20 14:21:59 +0800

    add eterbase orderbook (#1886)

--- spec/exchanges/eterbase/integration/market_spec.rb
+++ spec/exchanges/eterbase/integration/market_spec.rb
@@ -49,4 +49,19 @@ RSpec.describe 'Eterbase integration specs' do
     expect(trade.payload).to_not be nil
     expect(trade.market).to eq 'eterbase'
   end
+
+  it 'fetch order book' do
+    order_book = client.order_book(eth_usdt_pair)
+
+    expect(order_book.base).to eq 'ETH'
+    expect(order_book.target).to eq 'USDT'
+    expect(order_book.market).to eq 'eterbase'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.asks.count).to be > 10
+    expect(order_book.bids.count).to be > 10
+    expect(order_book.payload).to_not be nil
+  end  
 end
