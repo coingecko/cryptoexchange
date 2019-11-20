e83ff7b8994874b4ae1cfd83b4fa210f8e9c4480
Wenjie Yek <dx3152@hotmail.com>
2019-11-20 15:46:47 +0800

    add paribu orderbook and trades (#1888)

---
+++ lib/cryptoexchange/exchanges/paribu/services/order_book.rb
@@ -0,0 +1,43 @@
+module Cryptoexchange::Exchanges
+  module Paribu
+    module Services
+      class OrderBook < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            true
+          end
+        end
+
+        def fetch(market_pair)
+          output = super(ticker_url(market_pair))
+          adapt(output, market_pair)
+        end
+
+        def ticker_url(market_pair)
+          "#{Cryptoexchange::Exchanges::Paribu::Market::API_URL}/api/v1/orderbook/#{market_pair.base.downcase}_#{market_pair.target.downcase}"
+        end
+
+        def adapt(output, market_pair)
+          order_book = Cryptoexchange::Models::OrderBook.new
+          timestamp = Time.now.to_i
+
+          order_book.base      = market_pair.base
+          order_book.target    = market_pair.target
+          order_book.market    = Paribu::Market::NAME
+          order_book.asks      = adapt_orders(output['sell'])
+          order_book.bids      = adapt_orders(output['buy'])
+          order_book.timestamp = timestamp
+          order_book.payload   = output
+          order_book
+        end
+
+        def adapt_orders(orders)
+          orders.collect do |order_entry|
+            Cryptoexchange::Models::Order.new(price: order_entry[0],
+                                              amount: order_entry[0][1])
+          end
+        end
+      end
+    end
+  end
+end
--- spec/exchanges/paribu/integration/market_spec.rb
+++ spec/exchanges/paribu/integration/market_spec.rb
@@ -30,4 +30,36 @@ RSpec.describe 'Paribu integration specs' do
     
     expect(ticker.payload).to_not be nil
   end
+
+  it 'fetch order book' do
+    order_book = client.order_book(btc_tl_pair)
+  
+    expect(order_book.base).to eq 'BTC'
+    expect(order_book.target).to eq 'TL'
+    expect(order_book.market).to eq 'paribu'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to be_nil
+    expect(order_book.asks.count).to be > 10
+    expect(order_book.bids.count).to be > 10
+    expect(order_book.timestamp).to be_a Numeric
+    expect(order_book.payload).to_not be nil
+  end
+  
+  it 'fetch trade' do
+    trades = client.trades(btc_tl_pair)
+    trade = trades.sample
+  
+    expect(trades).to_not be_empty
+    expect(trade.base).to eq 'BTC'
+    expect(trade.target).to eq 'TL'
+    expect(trade.market).to eq 'paribu'
+    expect(['buy', 'sell']).to include trade.type
+    expect(trade.price).to_not be_nil
+    expect(trade.amount).to_not be_nil
+    expect(trade.timestamp).to be_a Numeric
+    expect(trade.payload).to_not be nil
+  end  
 end
