0f4d8cc3d8ce74dc1e204699360e9a856c35ce35
TM Lee <tmlee.ltm@gmail.com>
2019-11-30 14:40:03 +0800

    Okex swap exclude USDT because inconsistent response data

--- lib/cryptoexchange/exchanges/okex_swap/services/pairs.rb
+++ lib/cryptoexchange/exchanges/okex_swap/services/pairs.rb
@@ -16,6 +16,8 @@ module Cryptoexchange::Exchanges
           output.map do |pair|
             base, target, expired_at = pair['instrument_id'].split "-"
 
+            next if target == "USDT"
+
             Cryptoexchange::Models::MarketPair.new(
               base:   base,
               target: target,
@@ -23,13 +25,15 @@ module Cryptoexchange::Exchanges
               contract_interval: "perpetual",
               inst_id: pair['instrument_id']
             )
-          end
+          end.compact
         end
 
         def adapt_futures(output)
           output.map do |pair|
             base, target, expired_at = pair['instrument_id'].split "-"
             inst_id = pair['instrument_id']
+
+            next if target == "USDT"
 
             Cryptoexchange::Models::MarketPair.new(
               base:   base,
@@ -38,7 +42,7 @@ module Cryptoexchange::Exchanges
               contract_interval: "futures",
               inst_id: inst_id
             )
-          end
+          end.compact
         end
       end
     end
