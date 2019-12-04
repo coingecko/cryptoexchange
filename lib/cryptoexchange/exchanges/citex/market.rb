3797a5798c0a1ee3db38226438f064c05fcdfc8a
wenjieyek <dx3152@hotmail.com>
2019-12-04 11:57:19 +0800

    add citex trade url

--- lib/cryptoexchange/exchanges/citex/market.rb
+++ lib/cryptoexchange/exchanges/citex/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME='citex'
       API_URL = 'https://open.citex.co.kr/api/v1/common'
+
+      def self.trade_page_url(args={})
+        "https://trade.citex.co.kr/trade/#{args[:base].upcase}_#{args[:target].upcase}"
+      end
     end
   end
 end
