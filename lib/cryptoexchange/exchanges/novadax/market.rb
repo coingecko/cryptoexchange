b55aeb4b40d10bd8e50e29a05c3ae63f6817121f
Wenjie Yek <dx3152@hotmail.com>
2019-12-04 13:51:27 +0800

    add novadax orderbook (#1906)
    * add novadax orderbook
    
    * edit readme

--- lib/cryptoexchange/exchanges/novadax/market.rb
+++ lib/cryptoexchange/exchanges/novadax/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'novadax'
       API_URL = 'https://api.novadax.com/v1'
+
+      def self.trade_page_url(args={})
+        "https://www.novadax.com/product/pro-crypto-exchange?pair=#{args[:base].upcase}_#{args[:target].upcase}"
+      end
     end
   end
 end
