d3aa914fda3879af12e669038ca97d50a266aa9e
elvis <yinchuang94@gmail.com>
2019-12-13 14:10:52 +0800

    coinfalcon volume fix and add unwritten spec

--- lib/cryptoexchange/exchanges/coinfalcon/market.rb
+++ lib/cryptoexchange/exchanges/coinfalcon/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'coinfalcon'
       API_URL = 'https://coinfalcon.com/api/v1/'
+
+      def self.trade_page_url(args={})
+        "https://coinfalcon.com/en/"
+      end
     end
   end
 end
