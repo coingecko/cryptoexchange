72108beef429d2fae600bb394826e36a201a2433
ElvisLYC <34902933+ElvisLYC@users.noreply.github.com>
2019-12-31 14:43:35 +0800

    Revert "Implement Utorg exchange"

--- spec/exchanges/utorg/market_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe Cryptoexchange::Exchanges::Utorg::Market do
-  it { expect(described_class::NAME).to eq 'utorg' }
-  it { expect(described_class::API_URL).to eq 'https://public-api.utorg.io/api/v1' }
-end
