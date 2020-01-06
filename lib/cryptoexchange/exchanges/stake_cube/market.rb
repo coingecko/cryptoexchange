636b561a3f50d97da73b987ca6bf183ecf5aaf37
wenjieyek <dx3152@hotmail.com>
2020-01-06 15:51:27 +0800

    update stake_cube api

--- lib/cryptoexchange/exchanges/stake_cube/market.rb
+++ lib/cryptoexchange/exchanges/stake_cube/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module StakeCube
     class Market < Cryptoexchange::Models::Market
       NAME = 'stake_cube'
-      API_URL = 'https://stakecube.net/api/v1'
+      API_URL = 'https://stakecube.net/app/api/v1'
 
       def self.trade_page_url(args={})
         "https://stakecube.net/exchange/#{args[:target]}-#{args[:base]}"
