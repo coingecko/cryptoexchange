a43e286f247ba875d90798d5465037be043b9c31
JoaoCampos89 <jcampos893@gmail.com>
2020-01-08 22:07:24 -0300

    fix tests

--- spec/exchanges/veridex/market_spec.rb
+++ spec/exchanges/veridex/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Veridex::Market do
   it { expect(described_class::NAME).to eq 'veridex' }
-  it { expect(described_class::API_URL).to eq 'https://dex-backend.verisafe.io/v2' }
+  it { expect(described_class::API_URL).to eq 'https://dex-backend.verisafe.io/v3' }
 end
