fa2c00b917ffb4bba212c8b5784d0434a2c48a8a
Wenjie Yek <dx3152@hotmail.com>
2020-01-14 16:18:38 +0800

    add bitexbook orderbook (#1940)

--- spec/exchanges/bitexbook/integration/market_spec.rb
+++ spec/exchanges/bitexbook/integration/market_spec.rb
@@ -33,4 +33,20 @@ RSpec.describe 'Bitexbook integration specs' do
     expect(ticker.timestamp).to be nil
     expect(ticker.payload).to_not be nil
   end
+
+  it 'fetch order book' do
+    order_book = client.order_book(btc_usd_pair)
+
+    expect(order_book.base).to eq 'BTC'
+    expect(order_book.target).to eq 'USD'
+    expect(order_book.market).to eq 'bitexbook'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be nil
+    expect(order_book.bids.first.amount).to_not be nil
+    expect(order_book.asks.count).to be > 10
+    expect(order_book.bids.count).to be > 10
+    expect(order_book.timestamp).to be nil
+    expect(order_book.payload).to_not be nil
+  end
 end
