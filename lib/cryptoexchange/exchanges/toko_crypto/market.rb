53ae07f46d8f921a78b1920b01032f4743c17286
Wenjie Yek <dx3152@hotmail.com>
2020-01-30 10:59:54 +0800

    edit toko crypto api (#1959)

--- lib/cryptoexchange/exchanges/toko_crypto/market.rb
+++ lib/cryptoexchange/exchanges/toko_crypto/market.rb
@@ -7,6 +7,15 @@ module Cryptoexchange::Exchanges
       def self.trade_page_url(args={})
         "https://www.tokocrypto.com/id/dashboard/#{args[:base]}#{args[:target]}"
       end
+
+      def self.separate_symbol(pair)
+          separator = /(IDR)\z/i =~ pair
+          base      = pair[0..separator - 1]
+          target    = pair[separator..-1]
+          [base, target]
+        rescue NoMethodError
+          nil
+      end
     end
   end
 end
