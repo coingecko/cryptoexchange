7f333e5afdf553a21907c763a5f2e2e756b6879d
wenjieyek <dx3152@hotmail.com>
2020-02-25 11:28:40 +0800

    update secondbtc api

--- lib/cryptoexchange/exchanges/secondbtc/market.rb
+++ lib/cryptoexchange/exchanges/secondbtc/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Secondbtc
     class Market < Cryptoexchange::Models::Market
       NAME = 'secondbtc'
-      API_URL = 'https://secondbtc.com/api'
+      API_URL = 'https://secondbtc.com/api/public'
 
       def self.trade_page_url(args={})
         "https://secondbtc.com/exchange/#{args[:target]}-#{args[:base]}"
