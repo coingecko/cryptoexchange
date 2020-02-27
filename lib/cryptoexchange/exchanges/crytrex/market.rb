f9b78629f72c3a32adb0f5b5b98f806282c3ba7f
Wenjie Yek <dx3152@hotmail.com>
2020-02-27 14:22:58 +0800

    update crytrex api endpoint (#1991)

--- lib/cryptoexchange/exchanges/crytrex/market.rb
+++ lib/cryptoexchange/exchanges/crytrex/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Crytrex
     class Market < Cryptoexchange::Models::Market
       NAME    = 'crytrex'
-      API_URL = 'https://crytrex.com/public_api'
+      API_URL = 'https://cryt.crytrex.com/public_api'
     end
   end
 end
