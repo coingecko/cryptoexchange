677dfd24ab3c90ea6e881c863851b082a99da95c
Currency.com <44190117+Currencycom@users.noreply.github.com>
2020-03-10 05:28:57 +0300

    Implement currency exchange (#1993)
    * Implement Currency Exchange
    
    * fix
    
    * add order_book.rb in Currency
    
    * add order_book.rb in Currency
    
    * add vcr
    
    * Added new tools
    
    * Added new tools
    
    * Added new tools (fix)
    
    Co-authored-by: andrey_kurliandchik <andrei.kurlyandchik@expcapital.comandrei.kurlyandchik@expcapital.comandrei.kurlyandchik@expcapital.comandrei.kurlyandchik@expcapital.comandrei.kurlyandchik@expcapital.comandrei.kurlyandchik@expcapital.comandrei.kurlyandchik@expcapital.comandrei.kurlyandchik@expcapital.com>
    Co-authored-by: andrey_kurliandchik <andrei.kurlyandchik@expcapital.com>
    Co-authored-by: wenjieyek <dx3152@hotmail.com>

--- lib/cryptoexchange/exchanges/currency/services/market.rb
+++ lib/cryptoexchange/exchanges/currency/services/market.rb
@@ -9,36 +9,40 @@ module Cryptoexchange::Exchanges
         end
 
         def fetch
-          output = super ticker_url
+          output = super(ticker_url)
           adapt_all(output)
         end
 
         def ticker_url
-          "#{Cryptoexchange::Exchanges::Currency::Market::API_URL}/api/v1/ticker"
+          "#{Cryptoexchange::Exchanges::Currency::Market::API_URL}/api/v1/token_crypto/ticker"
         end
 
         def adapt_all(output)
           output.map do |pair, ticker|
-            adapt(pair, ticker)
-          end
+            market_pair = Cryptoexchange::Models::MarketPair.new({
+              base: ticker['base_currency'],
+              target: ticker['quote_currency'],
+              market: Currency::Market::NAME
+            })
+            adapt(market_pair, ticker)
+          end.compact
         end
 
-        def adapt(pair, output)
-            base, target     = pair.split('/')
-            ticker           = Cryptoexchange::Models::Ticker.new
-            ticker.base      = base
-            ticker.target    = target
-            ticker.market    = Currency::Market::NAME
-            ticker.ask       = NumericHelper.to_d(output['lowest_ask_price'])
-            ticker.bid       = NumericHelper.to_d(output['highest_bid_price'])
-            ticker.last      = NumericHelper.to_d(output['last_price'])
-            ticker.high      = NumericHelper.to_d(output['past_24hrs_high_price'])
-            ticker.low       = NumericHelper.to_d(output['past_24hrs_low_price'])
-            ticker.change    = NumericHelper.to_d(output['past_24hrs_price_change'].to_f)
-            ticker.volume    = NumericHelper.to_d(output['base_volume'])
-            ticker.timestamp = nil
-            ticker.payload   = output
-            ticker
+        def adapt(market_pair, output)
+          ticker           = Cryptoexchange::Models::Ticker.new
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
+          ticker.market    = Currency::Market::NAME
+          ticker.ask       = NumericHelper.to_d(output['lowest_ask_price'])
+          ticker.bid       = NumericHelper.to_d(output['highest_bid_price'])
+          ticker.last      = NumericHelper.to_d(output['last_price'])
+          ticker.high      = NumericHelper.to_d(output['past_24hrs_high_price'])
+          ticker.low       = NumericHelper.to_d(output['past_24hrs_low_price'])
+          ticker.change    = NumericHelper.to_d(output['past_24hrs_price_change'].to_f)
+          ticker.volume    = NumericHelper.to_d(output['base_volume'])
+          ticker.timestamp = nil
+          ticker.payload   = output
+          ticker
         end
       end
     end
