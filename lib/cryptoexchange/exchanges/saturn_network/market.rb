8028d7e2a9d2a2fa51bb5834d0cea87e8c35c186
Saturn Network <44673408+saturn-network@users.noreply.github.com>
2020-03-12 14:22:52 +0000

    Update saturn trade url
    Yesterday's fix did not work due to weird renaming. Visually it looked like token's address was appended after a `-` symbol, but it sounds like you've appended `OX` instead of `0x`?
    
    Going through token's ticker instead is a more robust approach. Confirmed it works for [NCOV](https://www.saturn.network/exchange/ETH/order-book/NCOV)

--- lib/cryptoexchange/exchanges/saturn_network/market.rb
+++ lib/cryptoexchange/exchanges/saturn_network/market.rb
@@ -5,7 +5,7 @@ module Cryptoexchange::Exchanges
       API_URL = 'https://ticker.saturn.network'
 
       def self.trade_page_url(args={})
-        "https://www.saturn.network/exchange/#{args[:target]}/order-book/#{args[:base].split('-').last}"
+        "https://www.saturn.network/exchange/#{args[:target]}/order-book/#{args[:base].split('-').first}"
       end
     end
   end
