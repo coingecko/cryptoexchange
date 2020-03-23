86a35071d4aaa999ea69e2ba689a1e2103781595
TM Lee <tmlee.ltm@gmail.com>
2020-03-23 22:24:57 +0800

    Bancor update api to https://api.bancor.network/0.1 (#2023)
    * Update Bancor API
    
    * Bancor: Change API endpoint

--- lib/cryptoexchange/exchanges/bancor/services/market.rb
+++ lib/cryptoexchange/exchanges/bancor/services/market.rb
@@ -4,41 +4,29 @@ module Cryptoexchange::Exchanges
       class Market < Cryptoexchange::Services::Market
         class << self
           def supports_individual_ticker_query?
-            false
+            true
           end
         end
 
-        def fetch
-          output = super ticker_url
-          adapt_all(output)
+        def fetch(market_pair)
+          output = super(ticker_url(market_pair))
+          adapt(output, market_pair)
         end
 
-        def ticker_url
-          "#{Cryptoexchange::Exchanges::Bancor::Market::API_URL}/exchanges?platform=bancor&key=#{Cryptoexchange::Exchanges::Bancor::Market.api_key}"
-        end
-
-        def adapt_all(output)
-          output['results'].map do |ticker|
-            next if ticker["tokenSymbol"] == nil || ticker["baseSymbol"] == nil
-            market_pair = Cryptoexchange::Models::MarketPair.new(
-                            base: ticker["tokenSymbol"],
-                            target: ticker["baseSymbol"],
-                            market: Bancor::Market::NAME
-                          )
-
-            adapt(ticker, market_pair)
-          end.compact
+        def ticker_url(market_pair)
+          "#{Cryptoexchange::Exchanges::Bancor::Market::API_URL}/currencies/#{market_pair.base}/ticker?fromCurrencyCode=#{market_pair.target}&displayCurrencyCode=#{market_pair.target}"
         end
 
         def adapt(output, market_pair)
-          ticker = Cryptoexchange::Models::Ticker.new
-          ticker.base = market_pair.base
-          ticker.target = market_pair.target
-          ticker.market = Bancor::Market::NAME
-          ticker.last = NumericHelper.to_d(output['basePrice'])
-          ticker.volume = NumericHelper.flip_volume(NumericHelper.to_d(output['baseVolume']), ticker.last)
+          output = output["data"]
+          ticker           = Cryptoexchange::Models::Ticker.new
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
+          ticker.market    = Bancor::Market::NAME
+          ticker.last      = NumericHelper.to_d(output['price'])
+          ticker.volume    = NumericHelper.flip_volume(NumericHelper.to_d(output['volume24hD']), NumericHelper.to_d(output['price']))
           ticker.timestamp = nil
-          ticker.payload = output
+          ticker.payload   = output
           ticker
         end
       end
