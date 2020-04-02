55d8a637c814159b9b571b8154139a3a8e52c203
wenjieyek <dx3152@hotmail.com>
2020-04-02 14:25:46 +0800

    add liquid_perpetual

---
+++ lib/cryptoexchange/exchanges/liquid_perpetual/services/contract_stat.rb
@@ -0,0 +1,52 @@
+module Cryptoexchange::Exchanges
+  module LiquidPerpetual
+    module Services
+      class ContractStat < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            false
+          end
+        end
+
+        def fetch
+          output = super ticker_url
+          adapt_all(output)
+        end
+
+        def ticker_url
+          "#{Cryptoexchange::Exchanges::LiquidPerpetual::Market::API_URL}/products?perpetual=1"
+        end
+
+        def adapt_all(output)
+          output.map do |output|
+            base = output["base_currency"].split("-")[1]
+            target = output["quoted_currency"]
+
+            market_pair = Cryptoexchange::Models::MarketPair.new(
+                            base: base,
+                            target: target,
+                            inst_id: output["id"],
+                            contract_interval: output["product_type"].downcase,
+                            market: LiquidPerpetual::Market::NAME
+                          )
+
+            adapt(output, market_pair)
+          end
+        end
+
+        def adapt(output, market_pair)
+          contract_stat = Cryptoexchange::Models::ContractStat.new
+          contract_stat.base = market_pair.base
+          contract_stat.target = market_pair.target
+          contract_stat.market = LiquidPerpetual::Market::NAME
+          contract_stat.contract_type = market_pair.contract_interval
+
+          contract_stat.index = output['index_price'].to_f
+          contract_stat.funding_rate_percentage = output["funding_rate"].to_f * 100
+          contract_stat.payload = output
+          contract_stat
+        end
+      end
+    end
+  end
+end
---
+++ lib/cryptoexchange/exchanges/liquid_perpetual/services/market.rb
@@ -0,0 +1,56 @@
+module Cryptoexchange::Exchanges
+  module LiquidPerpetual
+    module Services
+      class Market < Cryptoexchange::Services::Market
+        class << self
+          def supports_individual_ticker_query?
+            false
+          end
+        end
+
+        def fetch
+          output = super ticker_url
+          adapt_all(output)
+        end
+
+        def ticker_url
+          "#{Cryptoexchange::Exchanges::LiquidPerpetual::Market::API_URL}/products?perpetual=1"
+        end
+
+        def adapt_all(output)
+          output.map do |output|
+            base = output["base_currency"].split("-")[1]
+            target = output["quoted_currency"]
+
+            market_pair = Cryptoexchange::Models::MarketPair.new(
+                            base: base,
+                            target: target,
+                            inst_id: output["id"],
+                            contract_interval: output["product_type"].downcase,
+                            market: LiquidPerpetual::Market::NAME
+                          )
+
+            adapt(output, market_pair)
+          end
+        end
+
+        def adapt(output, market_pair)
+          ticker           = Cryptoexchange::Models::Ticker.new
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
+          ticker.inst_id   = market_pair.inst_id
+          ticker.contract_interval = market_pair.contract_interval
+          ticker.market    = LiquidPerpetual::Market::NAME
+
+          ticker.bid       = NumericHelper.to_d(output['low_market_bid'])
+          ticker.ask       = NumericHelper.to_d(output['high_market_ask'])
+          ticker.last      = NumericHelper.to_d(output['last_traded_price'])
+          ticker.volume    = NumericHelper.to_d(output['volume_24h'])
+          ticker.timestamp = nil
+          ticker.payload   = output
+          ticker
+        end
+      end
+    end
+  end
+end
