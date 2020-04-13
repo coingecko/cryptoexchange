4a0ffcb63bd2c7ec224affa5e39be21876089407
Wenjie Yek <dx3152@hotmail.com>
2020-04-13 11:59:26 +0800

    use v3 api in radar_relay (#2049)

--- lib/cryptoexchange/exchanges/radar_relay/market.rb
+++ lib/cryptoexchange/exchanges/radar_relay/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module RadarRelay
     class Market < Cryptoexchange::Models::Market
       NAME = 'radar_relay'
-      API_URL = 'https://api.radarrelay.com/v2'
+      API_URL = 'https://api.radarrelay.com/v3'
 
       def self.trade_page_url(args={})
         "https://app.radarrelay.com/#{args[:base]}/#{args[:target]}"
--- spec/exchanges/radar_relay/market_spec.rb
+++ spec/exchanges/radar_relay/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::RadarRelay::Market do
   it { expect(described_class::NAME).to eq 'radar_relay' }
-  it { expect(described_class::API_URL).to eq 'https://api.radarrelay.com/v2' }
+  it { expect(described_class::API_URL).to eq 'https://api.radarrelay.com/v3' }
 end
