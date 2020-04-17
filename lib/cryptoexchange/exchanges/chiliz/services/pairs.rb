766ca378ee28b984f888c0d09cd16826a7d6150e
TM Lee <tmlee.ltm@gmail.com>
2020-04-17 11:45:33 +0800

    Chiliz pairs - manual manage (#2053)
    * Chiliz pairs - manual manage
    
    * Set graphql client version

--- lib/cryptoexchange/exchanges/chiliz/services/pairs.rb
+++ lib/cryptoexchange/exchanges/chiliz/services/pairs.rb
@@ -2,24 +2,21 @@ module Cryptoexchange::Exchanges
   module Chiliz
     module Services
       class Pairs < Cryptoexchange::Services::Pairs
-        PAIRS_URL = "#{Cryptoexchange::Exchanges::Chiliz::Market::API_URL}/ticker/24hr"
-
         def fetch
           output = super
           adapt(output)
         end
 
         def adapt(output)
-          output.map do |output|
-            base, target = Chiliz::Market.separate_symbol(output["symbol"])
-            next if target.nil? || base.nil?
-
-            Cryptoexchange::Models::MarketPair.new(
-              base: base,
-              target: target,
-              market: Chiliz::Market::NAME,
-            )    
-          end.compact
+          market_pairs = []
+          output.each do |pair|
+            market_pairs << Cryptoexchange::Models::MarketPair.new(
+                              base: pair[:base],
+                              target: pair[:target],
+                              market: Chiliz::Market::NAME
+                            )
+          end
+          market_pairs
         end
       end
     end
