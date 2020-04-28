9b7b917137267ff4b35caf8f4f7b0e772c9075a8
Wenjie Yek <dx3152@hotmail.com>
2020-04-28 17:36:25 +0800

    independent reserve volume divided by 3 (#2071)

--- lib/cryptoexchange/exchanges/independent_reserve/services/market.rb
+++ lib/cryptoexchange/exchanges/independent_reserve/services/market.rb
@@ -2,6 +2,9 @@ module Cryptoexchange::Exchanges
   module IndependentReserve
     module Services
       class Market < Cryptoexchange::Services::Market
+        # this exchange share the volume across 3 fiat, need divide the volume by 3
+        FIAT_SHARED_VOLUME = 3
+
         class << self
           def supports_individual_ticker_query?
             true
@@ -30,7 +33,7 @@ module Cryptoexchange::Exchanges
           ticker.last      = NumericHelper.to_d(output['LastPrice'])
           ticker.high      = NumericHelper.to_d(output['DayHighestPrice'])
           ticker.low       = NumericHelper.to_d(output['DayLowestPrice'])
-          ticker.volume    = NumericHelper.to_d(output["DayVolumeXbt"])
+          ticker.volume    = NumericHelper.to_d(output["DayVolumeXbt"]) / FIAT_SHARED_VOLUME
           ticker.timestamp = nil
           ticker.payload   = output
           ticker
