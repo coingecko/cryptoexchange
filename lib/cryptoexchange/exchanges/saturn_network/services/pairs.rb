66b5be3c3b24bff325233b28d940134b7fe02d31
Wenjie Yek <dx3152@hotmail.com>
2020-04-29 14:52:10 +0800

    skip old dogecoin gold classic old address (#2073)

--- lib/cryptoexchange/exchanges/saturn_network/services/pairs.rb
+++ lib/cryptoexchange/exchanges/saturn_network/services/pairs.rb
@@ -13,6 +13,10 @@ module Cryptoexchange::Exchanges
           output.map do |pair|
             base = pair[1]["symbol"]
             target, contract_address = pair[0].split('_')
+
+            # skip if it fetch old DGC coin
+            next if contract_address == "0x7be3e4849aa2658c91b2d6fcf16ea88cfcb8b41e"
+
             if base == "NCOV"
               base = "#{base}-#{contract_address}"
             end
@@ -22,7 +26,7 @@ module Cryptoexchange::Exchanges
               inst_id: contract_address,
               market: SaturnNetwork::Market::NAME,
             )
-          end
+          end.compact
         end
       end
     end
