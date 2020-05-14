c929207b87642079a08d5e7c905e7bbfc31a2b0d
wenjieyek <dx3152@hotmail.com>
2020-05-14 14:02:13 +0800

    edit citex trade link

--- lib/cryptoexchange/exchanges/citex/market.rb
+++ lib/cryptoexchange/exchanges/citex/market.rb
@@ -5,7 +5,7 @@ module Cryptoexchange::Exchanges
       API_URL = 'https://open.citex.co.kr/api/v1/common'
 
       def self.trade_page_url(args={})
-        "https://trade.citex.co.kr/trade/#{args[:base].upcase}_#{args[:target].upcase}"
+        "https://trade.citex.me/trade/#{args[:base].upcase}_#{args[:target].upcase}"
       end
     end
   end
