b83c3535606f5cf29e660f7c2576c1b407d409bf
wenjieyek <dx3152@hotmail.com>
2020-05-14 14:20:33 +0800

    add nash orderbook

--- spec/exchanges/nash/integration/market_spec.rb
+++ spec/exchanges/nash/integration/market_spec.rb
@@ -28,4 +28,16 @@ RSpec.describe 'Nash integration specs' do
     expect(ticker.timestamp).to be nil
     expect(ticker.payload).to_not be nil
   end
+
+  it 'fetch order book' do
+    order_book = client.order_book(gas_eth_pair)
+
+    expect(order_book.base).to eq 'GAS'
+    expect(order_book.target).to eq 'ETH'
+    expect(order_book.market).to eq 'nash'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.timestamp).to be_a Numeric
+    expect(order_book.payload).to_not be nil
+  end  
 end
