2344e7ac716a96bb4109bd064001855683824064
elvis <yinchuang94@gmail.com>
2020-05-14 16:32:38 +0800

    fix broken bitsonic spec

--- spec/exchanges/bitsonic/market_spec.rb
+++ spec/exchanges/bitsonic/market_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe Cryptoexchange::Exchanges::Bitstorage::Market do
-  it { expect(described_class::NAME).to eq 'bitstorage' }
-  it { expect(described_class::API_URL).to eq 'https://bitstorage.finance/api' }
+RSpec.describe Cryptoexchange::Exchanges::Bitsonic::Market do
+  it { expect(described_class::NAME).to eq 'bitsonic' }
+  it { expect(described_class::API_URL).to eq 'https://open-api.bitsonic.co.kr/api' }
 end
