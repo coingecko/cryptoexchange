e5b2611fdae0420813df414777f9fdbe1845cbc2
elvis <yinchuang94@gmail.com>
2020-05-18 16:33:38 +0800

    add btcturk trade url

--- lib/cryptoexchange/exchanges/btcturk/market.rb
+++ lib/cryptoexchange/exchanges/btcturk/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'btcturk'
       API_URL = 'https://api.btcturk.com/api/v2'
+
+      def self.trade_page_url(args={})
+        "https://pro.btcturk.com/pro/al-sat/#{args[:base]}_#{args[:target]}"
+      end
     end
   end
 end
