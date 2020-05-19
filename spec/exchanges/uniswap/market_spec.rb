9a6fd0894a966c345f27c7e55ac8dda1b3aace60
Wenjie Yek <dx3152@hotmail.com>
2020-05-19 09:53:40 +0800

    Revert "edit uniswap endpoint (#2094)" (#2098)
    This reverts commit 921153cb237e5a913441c83587d52d850eabd255.

--- spec/exchanges/uniswap/market_spec.rb
+++ spec/exchanges/uniswap/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::Uniswap::Market do
   it { expect(described_class::NAME).to eq 'uniswap' }
-  it { expect(described_class::API_URL).to eq 'https://api.blocklytics.org/pools/v0/exchanges?platform=uniswap' }
+  it { expect(described_class::API_URL).to eq 'https://api.blocklytics.org/uniswap/v1' }
 end
