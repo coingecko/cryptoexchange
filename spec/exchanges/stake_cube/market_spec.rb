45a02514567cab13342ce4ac33c057fccf2dbd6c
Jack Wong <jackyhwong91@gmail.com>
2020-05-29 11:23:44 +0800

    update stakecube api

--- spec/exchanges/stake_cube/market_spec.rb
+++ spec/exchanges/stake_cube/market_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Cryptoexchange::Exchanges::StakeCube::Market do
   it { expect(described_class::NAME).to eq 'stake_cube' }
-  it { expect(described_class::API_URL).to eq 'https://stakecube.net/app/api/v1' }
+  it { expect(described_class::API_URL).to eq 'https://api.stakecube.net/v1' }
 end
