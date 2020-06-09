dd6e0f9501381ff4108a6de465bf049a3cf5896c
Wenjie Yek <dx3152@hotmail.com>
2020-06-09 17:21:39 +0800

    fix deversifi volume (#2120)

--- lib/cryptoexchange/exchanges/deversifi/services/market.rb
+++ lib/cryptoexchange/exchanges/deversifi/services/market.rb
@@ -9,8 +9,10 @@ module Cryptoexchange::Exchanges
         end
 
         def fetch(market_pair)
+          volume_output = super(volume_url)
+          volume = get_volume(volume_output, market_pair)
           output = super(ticker_url(market_pair))
-          adapt(output[0], market_pair)
+          adapt(output[0], market_pair, volume)
         end
 
         def ticker_url(market_pair)
@@ -19,7 +21,7 @@ module Cryptoexchange::Exchanges
           "#{Cryptoexchange::Exchanges::Deversifi::Market::API_URL}/bfx/v2/tickers?symbols=t#{base}#{target}"
         end
 
-        def adapt(output, market_pair)
+        def adapt(output, market_pair, volume)
           # [
           #   0 SYMBOL,
           #   1 BID,
@@ -43,10 +45,20 @@ module Cryptoexchange::Exchanges
           ticker.ask       = NumericHelper.to_d(output[3])
           ticker.high      = NumericHelper.to_d(output[9])
           ticker.low       = NumericHelper.to_d(output[10])
-          ticker.volume    = NumericHelper.to_d(output[8])
+          ticker.volume    = NumericHelper.to_d(volume)
           ticker.timestamp = nil
           ticker.payload   = output
           ticker
+        end
+
+        def volume_url
+          "#{Cryptoexchange::Exchanges::Deversifi::Market::API_URL}/v1/trading/r/last24HoursVolume"
+        end
+
+        def get_volume(output, market_pair)
+          target = market_pair.target
+          target = "USDT" if market_pair.target == "USD"
+          output["symbols"]["#{market_pair.base}:#{target}"]["tokenAmount"]
         end
       end
     end
