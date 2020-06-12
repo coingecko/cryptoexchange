2ad9b27013bbe8b2259564e5758e7302e6a11294
TM Lee <tmlee.ltm@gmail.com>
2020-06-12 15:34:29 +0800

    Add support for quarterly futures for Binance Futures

--- lib/cryptoexchange/exchanges/binance_futures/services/pairs.rb
+++ lib/cryptoexchange/exchanges/binance_futures/services/pairs.rb
@@ -2,16 +2,23 @@ module Cryptoexchange::Exchanges
   module BinanceFutures
     module Services
       class Pairs < Cryptoexchange::Services::Pairs
-        PAIRS_URL = "#{Cryptoexchange::Exchanges::BinanceFutures::Market::API_URL}/exchangeInfo"
+        PERPETUAL_PAIRS_URL = "#{Cryptoexchange::Exchanges::BinanceFutures::Market::API_URL}/exchangeInfo"
+        FUTURES_PAIRS_URL = "#{Cryptoexchange::Exchanges::BinanceFutures::Market::FUTURES_API_URL}/exchangeInfo"
 
         def fetch
-          output = super
-          adapt(output)
+          perpetual_output = fetch_via_api(PERPETUAL_PAIRS_URL)
+          futures_output = fetch_via_api(FUTURES_PAIRS_URL)
+          adapt(perpetual_output, :perpetual) + adapt(futures_output, :futures)
         end
 
-        def adapt(output)
+        def adapt(output, type)
           output["symbols"].map do |pair|
-            next if pair["status"] != "TRADING"
+
+            if type == :perpetual
+              next if pair["status"] != "TRADING"
+            elsif type == :futures
+              next if pair["contractStatus"] != "TRADING"
+            end
 
             base = pair["baseAsset"]
             target = pair["quoteAsset"]
@@ -21,7 +28,7 @@ module Cryptoexchange::Exchanges
               base: base,
               target: target,
               market: BinanceFutures::Market::NAME,
-              contract_interval: "perpetual",
+              contract_interval: type.to_s,
               inst_id: pair["symbol"]
             )
           end.compact
